467151e1b785b3899f63806005df29d29d91c9dd
Charanpal Dhanjal <charanpal@gmail.com>
2012-04-30 18:32:42 +0100

    Finish stochastic search for tree.

--- exp/sandbox/predictors/PenaltyDecisionTree.py
+++ exp/sandbox/predictors/PenaltyDecisionTree.py
@@ -7,35 +7,110 @@ from exp.sandbox.predictors.DecisionNode import DecisionNode
 from exp.sandbox.predictors.TreeCriterionPy import findBestSplit2, findBestSplitRand
 
 class PenaltyDecisionTree(DecisionTreeLearner): 
-    def __init__(self, criterion="mse", maxDepth=10, minSplit=30, type="reg"):
+    def __init__(self, criterion="mse", maxDepth=10, minSplit=30, type="reg", gamma=0.1):
         """
-        Need a minSplit for the internal nodes and one for leaves. 
+        Need a minSplit for the internal nodes and one for leaves. A PenaltyDecisionTree
+        is one created such that the penalty on the empirical risk is proportional 
+        to the root of the size of the tree as in Nobel 2002. 
+        
+        :param gamma: The weight on the penalty factor.  
         """
         super(PenaltyDecisionTree, self).__init__()
         self.maxDepth = maxDepth
         self.minSplit = minSplit
         self.criterion = criterion
         self.type = type
+        self.gamma = gamma 
+        self.sampleSize = 100 
                 
     def learnModel(self, X, y):
-        nodeId = (0, )         
-        self.tree = DictTree()
-        rootNode = DecisionNode(numpy.arange(X.shape[0]), y.mean())
-        self.tree.setVertex(nodeId, rootNode)
-
+        if numpy.unique(y).shape[0] != 2: 
+            raise ValueError("Must provide binary labels")
+        
+        self.shapeX = X.shape
+        (n, d) = X.shape
+        rootId = (0, )         
+        
         argsortX = numpy.zeros(X.shape, numpy.int)
         for i in range(X.shape[1]): 
             argsortX[:, i] = numpy.argsort(X[:, i])
             argsortX[:, i] = numpy.argsort(argsortX[:, i])
         
-        self.recursiveSplit(X, y, argsortX, nodeId)
+        bestError = float("inf")        
+        
+        for i in range(self.sampleSize): 
+            self.tree = DictTree()
+            rootNode = DecisionNode(numpy.arange(X.shape[0]), y.mean())
+            self.tree.setVertex(rootId, rootNode)
+            self.recursiveSplit(X, y, argsortX, rootId)
+            
+            #self.prune(X, y)
+            T = self.tree.getNumVertices()
+            #print(X.shape)
+            predY = self.predict(X)
+            error = numpy.sum(predY!=y)/float(X.shape[0]) + self.gamma*numpy.sqrt(32*(T*d*numpy.log(n) + T*numpy.log(2) + 2*numpy.log(T))/n)
+            
+            if error < bestError: 
+                print(error)
+                bestError = error
+                bestTree = self.tree.copy()
+        
+        self.tree = bestTree 
 
-        #Next add empirical risk plus penalty 
+    def prune(self, X, y): 
+        """
+        Do some post pruning greedily. 
+        """
+        rootId = (0,)
+        self.tree.getVertex(rootId).setTestInds(numpy.arange(X.shape[0]))
+        self.recursiveSetPrune(X, y, rootId)  
+        self.computeAlphas()
+        self.recursivePrune(rootId)
+        
+    def vertexTestError(self, trueY, predY):
+        """
+        This is the error used for pruning. We compute it at each node. 
+        """
+        return numpy.sum(trueY != predY)
+        
+    def computeAlphas(self): 
+        n = self.shapeX[0]
+        d = self.shapeX[1]        
+        
+        for vertexId in self.tree.getAllVertexIds(): 
+            currentNode = self.tree.getVertex(vertexId)            
+            subtreeLeaves = self.tree.leaves(vertexId)
+    
+            testErrorSum = 0 
+            for leaf in subtreeLeaves: 
+                testErrorSum += self.tree.getVertex(leaf).getTestError()
+        
+            T = self.tree.getNumVertices()
+            T2 = T - len(self.tree.subtreeIds(vertexId)) + 1 
+            currentNode.alpha = (testErrorSum - currentNode.getTestError())
+            currentNode.alpha += self.gamma * numpy.sqrt(32*n*(T*d*numpy.log(n) + T*numpy.log(2) + 2*numpy.log(T)))
+            currentNode.alpha -= self.gamma * numpy.sqrt(32*n*(T2*d*numpy.log(n) + T2*numpy.log(2) + 2*numpy.log(T2)))
+            currentNode.alpha /= n
+
+    def recursivePrune(self, nodeId): 
+        """
+        We prune as early as possible and recompute the alphas after each pruning.   
+        """
+        node = self.tree.getVertex(nodeId)
+
+        if node.alpha > self.alphaThreshold: 
+            self.tree.pruneVertex(nodeId)
+            self.computeAlphas()
+        else: 
+            for childId in [self.getLeftChildId(nodeId), self.getRightChildId(nodeId)]: 
+                if self.tree.vertexExists(childId):
+                    self.recursivePrune(childId)
 
     def recursiveSplit(self, X, y, argsortX, nodeId): 
         """
         Give a sample of data and a node index, we find the best split and 
-        add children to the tree accordingly. 
+        add children to the tree accordingly. We perform a pre-pruning 
+        based on the penalty. 
         """
         if len(nodeId)-1 >= self.maxDepth: 
             return 
@@ -60,14 +135,14 @@ class PenaltyDecisionTree(DecisionTreeLearner):
             node.setThreshold(bestThreshold)
             
             leftChildId = self.getLeftChildId(nodeId)
-            leftChild = DecisionNode(bestLeftInds, y[bestLeftInds].mean())
+            leftChild = DecisionNode(bestLeftInds, Util.mode(y[bestLeftInds]))
             self.tree.addChild(nodeId, leftChildId, leftChild)
             
             if leftChild.getTrainInds().shape[0] >= self.minSplit: 
                 self.recursiveSplit(X, y, argsortX, leftChildId)
             
             rightChildId = self.getRightChildId(nodeId)
-            rightChild = DecisionNode(bestRightInds, y[bestRightInds].mean())
+            rightChild = DecisionNode(bestRightInds, Util.mode(y[bestRightInds]))
             self.tree.addChild(nodeId, rightChildId, rightChild)
             
             if rightChild.getTrainInds().shape[0] >= self.minSplit: 
--- exp/sandbox/predictors/test/PenaltyDecisionTreeTest.py
+++ exp/sandbox/predictors/test/PenaltyDecisionTreeTest.py
@@ -7,7 +7,7 @@ from apgl.data.Standardiser import Standardiser
 import sklearn.datasets as data 
 from apgl.util.Evaluator import Evaluator
 
-class DecisionTreeLearnerTest(unittest.TestCase):
+class PenaltyDecisionTreeLearnerTest(unittest.TestCase):
     def setUp(self):
         numpy.random.seed(21)
         numpy.seterr("raise")
@@ -20,21 +20,40 @@ class DecisionTreeLearnerTest(unittest.TestCase):
     def testLearnModel(self): 
         generator = ExamplesGenerator()         
         
-        numExamples = numpy.random.randint(1, 200)
-        numFeatures = numpy.random.randint(1, 10)
-        minSplit = numpy.random.randint(1, 50)
-        maxDepth = numpy.random.randint(0, 10)
+        numExamples = 200
+        numFeatures = 10
+        minSplit = 20
+        maxDepth = 3
             
         X, y = generator.generateBinaryExamples(numExamples, numFeatures)
-        y+=1 
+        y+=1        
         
-        learner = PenaltyDecisionTree(minSplit=minSplit, maxDepth=maxDepth) 
+        testX = X[100:, :]
+        testY = y[100:]
+        X = X[0:100, :]
+        y = y[0:100]
+         
         
-        for i in range(10):         
-            learner.learnModel(X, y)        
-            tree = learner.getTree() 
+        gamma = 0.1
+        learner = PenaltyDecisionTree(minSplit=minSplit, maxDepth=maxDepth, gamma=gamma) 
+        learner.learnModel(X, y)   
+                  
+        tree = learner.getTree() 
         
-            print(tree)        
+        #Work out penalty cost 
+        predY = learner.predict(X)
+        predTestY = learner.predict(testX)
+        
+        n = float(X.shape[0])
+        d = X.shape[1]
+        T = tree.getNumVertices()
+        error = numpy.sum(predY!=y)/n
+        testError = numpy.sum(predTestY!=testY)/float(testY.shape[0])
+        error += gamma*numpy.sqrt(32*(T*d*numpy.log(n) + T*numpy.log(2) + 2*numpy.log(T))/n)
+        
+        print("error=" + str(error))
+        print("testError=" + str(testError))            
+        print(tree)        
         
 if __name__ == '__main__':
     unittest.main()
