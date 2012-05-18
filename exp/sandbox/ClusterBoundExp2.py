941ba9af9d1450f1693faf598f4a5853b4658ed2
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-18 09:26:07 +0200

    Bound in terms of sigma.

--- exp/sandbox/ClusterBoundExp2.py
+++ exp/sandbox/ClusterBoundExp2.py
@@ -1,12 +1,28 @@
 
-from sympy import Symbol, simplify, collect, latex, pprint 
+from sympy import Symbol, simplify, collect, latex, pprint, solve 
+
+
 rho = Symbol("rho")
 s = Symbol("s")
+p = Symbol("p")
 q = Symbol("q")
 r = Symbol("r")
 eps = Symbol("epsilon")
-pprint(collect(simplify((q*(rho**2)*(s+1)*(rho-1)**2).expand() - 
+sigma = Symbol("sigma")
+
+
+print(collect(simplify( ((p - sigma)**2*(sigma**2 * (s+1) - 2*p*sigma)).expand() 
+    + r*sigma**2*s**2 - (2*r*s*sigma*(p-sigma)).expand() - ((p - sigma)**2*eps).expand()), sigma))
+
+"""
+print(solve(collect(simplify((q*(rho**2)*(s+1)*(rho-1)**2).expand() - 
     (2*q*rho*((rho-1)**2) * (rho*(s+1)-s) ).expand() + 
     (r*rho**2*(rho*(s+1)-s)**2).expand() - 
     (2*r*rho*(rho-1)*(rho*(s+1)-s)**2).expand() -
-    (eps*((rho*(s+1)-s)**2) *(rho-1)**2).expand()), rho))
+    (eps*((rho*(s+1)-s)**2) *(rho-1)**2).expand()), rho), rho))"""
+
+"""pprint(collect(simplify((q*(rho**2)*(s+1)*(rho-1)**2).expand() - 
+    (2*q*rho*((rho-1)**2) * (rho*(s+1)-s) ).expand() + 
+    (r*rho**2*(rho*(s+1)-s)**2).expand() - 
+    (2*r*rho*(rho-1)*(rho*(s+1)-s)**2).expand() -
+    (eps*((rho*(s+1)-s)**2) *(rho-1)**2).expand()), rho))"""
