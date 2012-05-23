dbd5881bb70fe1f1db38bfef06109fc1cf556f01
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-23 20:34:39 +0200

    New bound form.

--- exp/sandbox/ClusterBoundExp.py
+++ exp/sandbox/ClusterBoundExp.py
@@ -78,11 +78,92 @@ def computeBound(U, delta):
         
     return bestObj
 
+def computeBound2(U, delta): 
+    """
+    Find the worse lower bound for a matrix U given a purtubation delta for a 
+    two cluster problem. We use a simpler formulation and solve for y = rho/(rho-1). 
+    """
+    X, a, Y = numpy.linalg.svd(U)
+    a = numpy.flipud(numpy.sort(a))
+    epsilon = delta - numpy.trace(U.T.dot(U))
+    logging.debug("a=" + str(a))
+    logging.debug("epsilon=" + str(epsilon)) 
+    tol = 10**-3
+    
+    s = 0    
+    bestSigma = numpy.zeros(a.shape[0])
+    bestSigma[-1] = 1
+    
+    while numpy.argmax(bestSigma) != 0 and s!=a.shape[0]-1: 
+        q = (a[0:s+1]).sum()
+        r = ((a[s+1:])**2).sum()
+        logging.debug("q=" + str(q))
+        logging.debug("r=" + str(r))
+        logging.debug("s=" + str(s))
+        
+        b = numpy.zeros(5)
+        b[0] = r
+        b[1] = 2*r*s - 2*r
+        b[2] = -epsilon + q**2*s - q**2 + r*s**2 - 4*r*s
+        b[3] = -2*epsilon*s - 2*q**2*s - 2*r*s**2
+        b[4] = -epsilon*s**2
 
+        logging.debug("b=" + str(b))
+        
+        ys = numpy.roots(b)
+        logging.debug("ys=" + str(ys))
+        ys = ys[numpy.isreal(ys)]
+        ys = ys[ys>0]
+        logging.debug("ys=" + str(ys))
+        
+        #Compute epsilon from roots 
+        for y in ys: 
+            zero = (q**2*y**2*(s+1)) - (2*y*q**2*(y+s)) + (r*y**2*(y+s)**2) - (2*r*y*(y+s)**2) - (epsilon*(y+s)**2)
+            assert abs(zero) <= tol, "%f" % (zero)
+        
+        #This is the objective when sigma[i] = a[i]
+        bestObj = (a[1:]**2).sum() 
+
+        for i in range(ys.shape[0]): 
+            sigma = numpy.zeros(a.shape[0])
+            y = ys[i] 
+            
+            sigma[0:s+1] = y*q/(y+s)
+            sigma[s+1:] = a[s+1:]*y
+    
+            obj = s*y**2*q**2/(y+2)**2 + y**2 * r
+            obj2 = (sigma[1:]**2).sum()
+            assert abs(obj - obj2) <= tol, "%f %f" % (obj, obj2)
+            
+            logging.debug("sigma=" + str(sigma))
+            logging.debug("obj=" + str(obj))
+            
+            #Check if expansion of polynomial is correct 
+            #t1 = (rho/(rho*s - s + rho))
+            #t2 = rho/(rho-1)
+            #val1 = (sigma*(sigma-2*a))
+            
+            #poly = q*((t1**2)*(s+1) - t1*2) + r*(t2**2-2*t2)
+            
+            #assert abs((q*((t1**2)*(s+1) - t1*2))- val1[0:s+1].sum()) < 10**-3, "%s, %s" % (str((q*((t1**2)*(s+1) - t1*2))), str(val1[0:s+1].sum()))
+            #assert abs((r*(t2**2-2*t2)) - val1[s+1:].sum()) <= 10**-3, "%s" % str(abs((r*(t2**2-2*t2)) - val1[s+1:].sum()))
+            #logging.debug("poly=" + str(poly))
+            
+            #Check bound holds 
+            val = (sigma*(sigma-2*a)).sum() 
+            assert - val +  epsilon < 0.1, "%f" % (abs(val - epsilon))
+            logging.debug("bound=" + str(val))
+            
+            if bestObj < obj: 
+                bestObj = obj
+                bestSigma = sigma.copy() 
+        s += 1 
+        
+    return bestObj
 
 numpy.random.seed(21)
 numpy.set_printoptions(suppress=True, precision=3)
-#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 numExamples = 100 
 numFeatures = 2
@@ -120,7 +201,7 @@ upperBounds = numpy.zeros(deltas.shape[0])
 realError = numpy.zeros(deltas.shape[0])
 
 for i in range(deltas.shape[0]): 
-    worstLowerBounds[i] = computeBound(U, deltas[i])
+    worstLowerBounds[i] = computeBound2(U, deltas[i])
     
     #Now add random matrix to U 
     E = numpy.random.randn(numExamples, numFeatures)
--- exp/sandbox/ClusterBoundExp2.py
+++ exp/sandbox/ClusterBoundExp2.py
@@ -7,11 +7,15 @@ s = Symbol("s")
 q = Symbol("q")
 r = Symbol("r")
 eps = Symbol("epsilon")
-sigma = Symbol("sigma")
+y = Symbol("y")
 
 
-print(collect(simplify( ((q - sigma)**2*(sigma**2 * (s+1) - 2*q*sigma)).expand() 
-    + r*sigma**2*s**2 - (2*r*s*sigma*(q-sigma)).expand() - ((q - sigma)**2*eps).expand()), sigma))
+"""print(collect(simplify( ((q - sigma)**2 * ((s+1)*sigma**2 - 2*q*sigma)).expand() 
+    + r* s**2 * sigma**2 - (2*r*s*sigma*(q-sigma)).expand() - (eps*(q - sigma)**2).expand()), sigma))
+
+"""
+print(collect(simplify( (q**2*y**2*(s+1)).expand() - (2*y*q**2*(y+s)).expand() + (r*y**2*(y+s)**2).expand() - (2*r*y*(y+s)**2).expand() - (eps*(y+s)**2).expand()   ), y))
+  
 
 """
 print(solve(collect(simplify((q*(rho**2)*(s+1)*(rho-1)**2).expand() - 
@@ -25,3 +29,5 @@ print(solve(collect(simplify((q*(rho**2)*(s+1)*(rho-1)**2).expand() -
     (r*rho**2*(rho*(s+1)-s)**2).expand() - 
     (2*r*rho*(rho-1)*(rho*(s+1)-s)**2).expand() -
     (eps*((rho*(s+1)-s)**2) *(rho-1)**2).expand()), rho))"""
+    
+    #-epsilon*q**2 + sigma**4*(s + 1) + sigma**3*(-2*q*s - 4*q) + sigma**2*(-epsilon + q**2*s + 5*q**2 + r*s**2 + 2*r*s) + sigma*(2*epsilon*q - 2*q**3 - 2*q*r*s)
