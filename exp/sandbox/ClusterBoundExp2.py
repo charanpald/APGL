e409f7f622cbc7015236a98b5af1cd566068ac57
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-24 16:50:33 +0200

    Solve for multi cluster case.

--- exp/sandbox/ClusterBoundExp2.py
+++ exp/sandbox/ClusterBoundExp2.py
@@ -21,17 +21,5 @@ sigma = Symbol("sigma")
 
 #Multi cluster bound 
 
-print(collect(simplify( ((-b - eps)*(1+t+s*y)**2).expand() + (1+s+t)*y**2*q**2 - (2*y*q**2*(1+t+s*y)).expand() + (c*(y**2 - 2*y)*(1+t+s*y)**2).expand()  ), y))
+print(collect(simplify( ((-r - eps)*(1+t+s*y)**2).expand() + (1+s+t)*y**2*q**2 - (2*y*q**2*(1+t+s*y)).expand() + (c*(y**2 - 2*y)*(1+t+s*y)**2).expand()  ), y))
 
-"""
-print(solve(collect(simplify((q*(rho**2)*(s+1)*(rho-1)**2).expand() - 
-    (2*q*rho*((rho-1)**2) * (rho*(s+1)-s) ).expand() + 
-    (r*rho**2*(rho*(s+1)-s)**2).expand() - 
-    (2*r*rho*(rho-1)*(rho*(s+1)-s)**2).expand() -
-    (eps*((rho*(s+1)-s)**2) *(rho-1)**2).expand()), rho), rho))"""
-
-"""pprint(collect(simplify((q*(rho**2)*(s+1)*(rho-1)**2).expand() - 
-    (2*q*rho*((rho-1)**2) * (rho*(s+1)-s) ).expand() + 
-    (r*rho**2*(rho*(s+1)-s)**2).expand() - 
-    (2*r*rho*(rho-1)*(rho*(s+1)-s)**2).expand() -
-    (eps*((rho*(s+1)-s)**2) *(rho-1)**2).expand()), rho))"""
