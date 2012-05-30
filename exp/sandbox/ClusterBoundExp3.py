b12f98eb798482e3c5c0b03ee3add46506896582
Charanpal Dhanjal <charanpal@gmail.com>
2012-05-30 16:25:42 +0200

    Test if eigenvalues increase on addition of psd matrix.

--- exp/sandbox/ClusterBoundExp3.py
+++ exp/sandbox/ClusterBoundExp3.py
@@ -3,17 +3,17 @@ We test the cluster bound on the spectral clustering approach
 """ 
 import numpy 
 
-numpy.random.seed(21)
+#numpy.random.seed(21)
 numpy.set_printoptions(suppress=True, precision=3)
 
 nrows = 10 
-AA = numpy.random.rand(nrows, 5)
+AA = numpy.random.randn(nrows, 5)
 AA = AA.dot(AA.T)
 
-U = numpy.random.rand(nrows, 2)
+U = numpy.random.randn(nrows, 2)
 U = U.dot(U.T)
 
-k = 3 
+k = 3
 
 lmbda, Q = numpy.linalg.eigh(AA)
 sigma, P = numpy.linalg.eigh(U)
@@ -26,24 +26,14 @@ lmbdak = lmbda[indsk]
 
 AAk = (Qk*lmbdak).dot(Qk.T)
 
-#print(AAk - AA)
-#Find component of U in space of A 
-alpha = numpy.zeros(nrows) 
-beta = numpy.zeros(nrows) 
+lmbda2, Q2 = numpy.linalg.eigh(AA + U)
+lmbda3, Q3 = numpy.linalg.eigh(AAk + U)
 
-R = (numpy.eye(nrows) - Q.dot(Q.T)).dot(P)
-print(Q)
-print(R)
+print(Q2.T.dot(Q3))
 
-for i in range(nrows): 
-    #alpha[i] = lmbda[i] + Q[:, i].T.dot(U).dot(Q[:, i])
-    alpha[i] = Q[:, i].T.dot(U).dot(Q[:, i])
-    beta[i] = R[:, i].T.dot(U).dot(R[:, i])
+lmbda2 = numpy.sort(lmbda2)
+lmbda3 = numpy.sort(lmbda3)
 
-print(lmbda)
-print(alpha)
-print(beta)
-    
-print((Q*alpha).dot(Q.T))  
-print(U)
 
+print(lmbda2)
+print(lmbda3)
