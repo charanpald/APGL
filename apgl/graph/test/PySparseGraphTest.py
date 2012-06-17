2f1a03d25794486a035b6904763ceb3eb15d43c4
Charanpal Dhanjal <charanpal@gmail.com>
2012-06-17 17:30:28 +0200

    Test of setting using sparse matrix.

--- apgl/graph/test/PySparseGraphTest.py
+++ apgl/graph/test/PySparseGraphTest.py
@@ -4,7 +4,7 @@ from apgl.graph.VertexList import VertexList
 from apgl.util import *
 import unittest
 import apgl
-import pickle
+import scipy.sparse as sparse 
 
 try: 
     from apgl.graph.PySparseGraph import PySparseGraph
@@ -32,7 +32,30 @@ class PySparseGraphTest(unittest.TestCase, MatrixGraphTest):
 
         self.assertRaises(ValueError, PySparseGraph, [])
         
+    def testSetWeightMatrixSparse(self): 
+        numVertices = 10
+        numFeatures = 0
 
+        vList = VertexList(numVertices, numFeatures)
+        graph = self.GraphType(vList)
+        graph[0, 1] = 1
+        
+        W = sparse.lil_matrix((numVertices, numVertices))
+        
+        W[2, 1] = 1 
+        W[1, 2] = 1 
+        W[3, 8] = 0.2 
+        W[8, 3] = 0.2 
+        
+        self.assertEquals(graph[0, 1], 1)   
+        
+        graph.setWeightMatrixSparse(W)
+        
+        self.assertEquals(graph[0, 1], 0)
+        self.assertEquals(graph[2, 1], 1)
+        self.assertEquals(graph[3, 8], 0.2)
+        self.assertEquals(graph.getNumEdges(), 2)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
