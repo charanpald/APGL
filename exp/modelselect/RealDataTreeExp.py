5f171f0d4bee82a823d0753458f043c54bdf5208
Charanpal Dhanjal <charanpal@gmail.com>
2012-06-18 17:18:36 +0200

    Test gamma param in trees.

--- exp/modelselect/RealDataTreeExp.py
+++ exp/modelselect/RealDataTreeExp.py
@@ -1,46 +1,118 @@
-
-
 """
 Get some results and compare on a real dataset using Penalty Trees, Decision Trees. 
 """
 import logging 
 import numpy 
 import sys 
+import multiprocessing 
 from apgl.util.PathDefaults import PathDefaults 
 from exp.modelselect.ModelSelectUtils import ModelSelectUtils 
 from apgl.util.Sampling import Sampling
-from apgl.util.Util import Util
-from exp.sandbox.predictors.PenaltyDecisionTree import PenaltyDecisionTree
+from exp.sandbox.predictors.DecisionTreeLearner import DecisionTreeLearner
+import matplotlib.pyplot as plt 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.seterr(all="raise")
 numpy.random.seed(21)
 dataDir = PathDefaults.getDataDir() 
-dataDir += "modelPenalisation/benchmark/"
+dataDir += "modelPenalisation/regression/"
 
-loadMethod = ModelSelectUtils.loadRatschDataset
-datasets = ModelSelectUtils.getRatschDatasets(True)
+loadMethod = ModelSelectUtils.loadRegressDataset
+datasets = ModelSelectUtils.getRegressionDatasets(True)
 datasetName, numRealisations = datasets[0]
+
+logging.debug("Dataset " + datasetName)
 
 errors = numpy.zeros(numRealisations)
 sampleMethod = Sampling.crossValidation
 
-learner = PenaltyDecisionTree()
+numProcesses = multiprocessing.cpu_count()
+learner = DecisionTreeLearner(criterion="mse", maxDepth=50, minSplit=2, pruneType="CART", processes=numProcesses)
+learner.setChunkSize(3)
 
 paramDict = {} 
-paramDict["setAlphaThreshold"] = numpy.linspace(0, 0.1, 20)
+paramDict["setGamma"] = numpy.linspace(0.0, 1.0, 20) 
 
+alpha = 0.6
 folds = 5
-numRealisations = 5
+numRealisations = 10
+numMethods = 3
+sampleSize = 200 
+Cvs = numpy.array([folds-1])*alpha
 
-for j in range(numRealisations): 
-    Util.printIteration(j, 1, numRealisations, "Realisation: ")
+meanCvGrid = numpy.zeros((numMethods, paramDict["setGamma"].shape[0]))
+meanPenalties = numpy.zeros(paramDict["setGamma"].shape[0])
+meanTrainError = numpy.zeros(paramDict["setGamma"].shape[0])
+meanErrors = numpy.zeros(numMethods)
+meanDepths = numpy.zeros(numMethods)
+meanSizes = numpy.zeros(numMethods)
+
+for j in range(numRealisations):
+    print("")
+    logging.debug("j=" + str(j))
     trainX, trainY, testX, testY = loadMethod(dataDir, datasetName, j)
+    logging.debug("Loaded dataset")
+    
+    trainInds = numpy.random.permutation(trainX.shape[0])[0:sampleSize]
+    trainX = trainX[trainInds,:]
+    trainY = trainY[trainInds]
+    
+    #logging.debug("Training set size: " + str(trainX.shape))
     
     idx = sampleMethod(folds, trainX.shape[0])
     bestLearner, cvGrid = learner.parallelModelSelect(trainX, trainY, idx, paramDict)
-    
     predY = bestLearner.predict(testX)
-    errors[j] = bestLearner.getMetricMethod()(testY, predY)
+    meanCvGrid[0, :] += cvGrid     
+    meanErrors[0] += bestLearner.getMetricMethod()(testY, predY)
+    meanDepths[0] += bestLearner.tree.depth()
+    meanSizes[0] += bestLearner.tree.getNumVertices()
+
+    #Now try penalisation
+    resultsList = learner.parallelPen(trainX, trainY, idx, paramDict, Cvs)
+    bestLearner, trainErrors, currentPenalties = resultsList[0]
+    meanCvGrid[1, :] += trainErrors + currentPenalties
+    meanPenalties += currentPenalties
+    meanTrainError += trainErrors
+    predY = bestLearner.predict(testX)
+    meanErrors[1] += bestLearner.getMetricMethod()(testY, predY)
+    meanDepths[1] += bestLearner.tree.depth()
+    meanSizes[1] += bestLearner.tree.getNumVertices()
     
-print(numpy.mean(errors))
+    #Compute true error grid 
+    cvGrid  = learner.parallelSplitGrid(trainX, trainY, testX, testY, paramDict)    
+    meanCvGrid[2, :] += cvGrid
+    bestLearner.setGamma(paramDict["setGamma"][numpy.argmin(cvGrid)])
+    bestLearner.learnModel(trainX, trainY)
+    predY = bestLearner.predict(testX)
+    meanErrors[2] += bestLearner.getMetricMethod()(testY, predY)
+    meanDepths[2] += bestLearner.tree.depth()
+    meanSizes[2] += bestLearner.tree.getNumVertices()
+    
+meanCvGrid /=  numRealisations   
+meanPenalties /=  numRealisations   
+meanTrainError /=  numRealisations   
+meanErrors /=  numRealisations 
+meanDepths /= numRealisations
+meanSizes /= numRealisations
+
+print("\n")
+print("meanErrors=" + str(meanErrors))
+print("meanDepths=" + str(meanDepths))
+print("meanSizes=" + str(meanSizes))
+
+plt.figure(0)
+plt.plot(paramDict["setGamma"], meanCvGrid[0, :], label="CV")
+plt.plot(paramDict["setGamma"], meanCvGrid[1, :], label="Pen")
+plt.plot(paramDict["setGamma"], meanCvGrid[2, :], label="Test")
+plt.xlabel("Gamma")
+plt.ylabel("Error/Penalty")
+plt.legend()
+
+plt.figure(1)
+plt.plot(paramDict["setGamma"], meanPenalties, label="Penalty")
+plt.plot(paramDict["setGamma"], meanTrainError, label="Train Error")
+plt.xlabel("Gamma")
+plt.ylabel("Error/Penalty")
+plt.legend()
+    
+plt.show()
