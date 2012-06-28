0344c2e382d3456b7300181cd7b7324a6c4eeb07
Charanpal Dhanjal <charanpal@gmail.com>
2012-06-28 12:14:26 +0200

    Change error to mean squared.

--- exp/sandbox/predictors/DecisionTreeLearner.py
+++ exp/sandbox/predictors/DecisionTreeLearner.py
@@ -357,7 +357,9 @@ class DecisionTreeLearner(AbstractPredictor):
         
     def getMetricMethod(self): 
         if self.type == "reg": 
-            return Evaluator.rootMeanSqError
+            #return Evaluator.rootMeanSqError
+            #return Evaluator.meanAbsError
+            return Evaluator.meanSqError
         else:
             return Evaluator.binaryError      
             
