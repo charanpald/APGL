de0dfab79a6ead5e3d0dc51d03ead830e884ca82
Charanpal Dhanjal <charanpal@gmail.com>
2012-07-07 16:52:47 +0200

    Save betas for all datasets.

--- exp/modelselect/RealDataTreeExp4.py
+++ exp/modelselect/RealDataTreeExp4.py
@@ -1,11 +1,11 @@
 """
 Try to figure out the learning rate 
 """
-
 import logging 
 import numpy 
 import sys 
 import multiprocessing 
+import scipy.stats 
 from apgl.util.PathDefaults import PathDefaults 
 from exp.modelselect.ModelSelectUtils import ModelSelectUtils 
 from apgl.util.Sampling import Sampling
@@ -23,116 +23,105 @@ figInd = 0
 loadMethod = ModelSelectUtils.loadRegressDataset
 datasets = ModelSelectUtils.getRegressionDatasets(True)
 
-sampleSize = 100
-foldSizes = numpy.arange(2, 9, 1)
+sampleSizes = numpy.array([50, 100, 200])
+foldSizes = numpy.arange(2, 13, 2)
+alpha = 1.0
 
-#datasets = [datasets[1]]
+gammas = numpy.array(numpy.round(2**numpy.arange(1, 7.5, 0.5)-1), dtype=numpy.int)
+
+paramDict = {} 
+paramDict["setGamma"] = numpy.array([3], dtype=numpy.int)
+numParams = paramDict["setGamma"].shape[0]
+
+sampleMethod = Sampling.crossValidation
+numProcesses = multiprocessing.cpu_count()
+Cvs = numpy.array([1])
+
+outputDir = PathDefaults.getOutputDir() + "modelPenalisation/regression/CART/"
+
+#datasets = [datasets[0]]
 
 for datasetName, numRealisations in datasets:
     logging.debug("Dataset " + datasetName)
-    meanErrors = numpy.zeros(foldSizes.shape[0])   
-    meanPenalties = numpy.zeros(foldSizes.shape[0])
-    meanIdealPenalities = numpy.zeros(foldSizes.shape[0])
+    betas = numpy.zeros((gammas.shape[0], sampleSizes.shape[0]))
     
-    k = 0 
-    for folds in foldSizes: 
-        logging.debug("Folds " + str(folds))
-        errors = numpy.zeros(numRealisations)
+    learner = DecisionTreeLearner(criterion="mse", maxDepth=100, minSplit=1, pruneType="CART", processes=numProcesses)
+    learner.setChunkSize(3)    
+    
+    for m in range(sampleSizes.shape[0]): 
+        sampleSize = sampleSizes[m]
+        logging.debug("Sample size " + str(sampleSize))
+    
+        for i in range(gammas.shape[0]): 
+            gamma = gammas[i]
+            logging.debug("Gamma " + str(gamma))
+            paramDict["setGamma"] = numpy.array([gamma], dtype=numpy.int)
+            meanErrors = numpy.zeros(foldSizes.shape[0])   
+            meanPenalties = numpy.zeros(foldSizes.shape[0])
         
-        sampleMethod = Sampling.crossValidation
-        
-        #Setting maxDepth = 50 and minSplit = 5 doesn't effect results 
-        numProcesses = multiprocessing.cpu_count()
-        learner = DecisionTreeLearner(criterion="mse", maxDepth=100, minSplit=1, pruneType="CART", processes=numProcesses)
-        learner.setChunkSize(3)
-        
-        paramDict = {} 
-        paramDict["setGamma"] = numpy.array([3], dtype=numpy.int)
-        numParams = paramDict["setGamma"].shape[0]
-        
-        alpha = 1.0
-        numRealisations = 20
-        
-        #Cvs = numpy.array([folds-1])*alpha
-        Cvs = numpy.array([1])
-        
-        meanAllErrors = numpy.zeros(numParams) 
-        meanTrainError = numpy.zeros(numParams)
-        
-
-        
-        treeSizes = numpy.zeros(numParams)
-        treeDepths = numpy.zeros(numParams)
-        treeLeaveSizes = numpy.zeros(numParams)
-        
-        for j in range(numRealisations):
-            print("")
-            logging.debug("j=" + str(j))
-            trainX, trainY, testX, testY = loadMethod(dataDir, datasetName, j)
-            logging.debug("Loaded dataset with " + str(trainX.shape) +  " train and " + str(testX.shape) + " test examples")
+            numRealisations = 20
             
-            trainInds = numpy.random.permutation(trainX.shape[0])[0:sampleSize]
-            trainX = trainX[trainInds,:]
-            trainY = trainY[trainInds]
-            
-            idx = sampleMethod(folds, trainX.shape[0])        
-            
-            #Now try penalisation
-            resultsList = learner.parallelPen(trainX, trainY, idx, paramDict, Cvs)
-            bestLearner, trainErrors, currentPenalties = resultsList[0]
-            meanPenalties[k] += currentPenalties
-            meanTrainError += trainErrors
-            predY = bestLearner.predict(testX)
-            meanErrors[k] += bestLearner.getMetricMethod()(testY, predY)
-    
-            
-            #Compute ideal penalties and error on training data 
-            meanIdealPenalities[k] += learner.parallelPenaltyGrid(trainX, trainY, testX, testY, paramDict)
-            for i in range(len(paramDict["setGamma"])):
-                allError = 0    
-                learner.setGamma(paramDict["setGamma"][i])
-                for trainInds, testInds in idx: 
-                    validX = trainX[trainInds, :]
-                    validY = trainY[trainInds]
-                    learner.learnModel(validX, validY)
-                    predY = learner.predict(trainX)
-                    allError += learner.getMetricMethod()(predY, trainY)
-                meanAllErrors[i] += allError/float(len(idx))
-            
-        k+= 1
+            k = 0 
+            for folds in foldSizes: 
+                logging.debug("Folds " + str(folds))
+                errors = numpy.zeros(numRealisations)
+                
+                meanAllErrors = numpy.zeros(numParams) 
+                meanTrainError = numpy.zeros(numParams)
+                        
+                for j in range(numRealisations):
+                    print("")
+                    logging.debug("Realisation: " + str(j))
+                    trainX, trainY, testX, testY = loadMethod(dataDir, datasetName, j)
+                    
+                    trainInds = numpy.random.permutation(trainX.shape[0])[0:sampleSize]
+                    trainX = trainX[trainInds,:]
+                    trainY = trainY[trainInds]
+                    
+                    idx = sampleMethod(folds, trainX.shape[0])        
+                    
+                    #Now try penalisation
+                    resultsList = learner.parallelPen(trainX, trainY, idx, paramDict, Cvs)
+                    bestLearner, trainErrors, currentPenalties = resultsList[0]
+                    meanPenalties[k] += currentPenalties
+                    meanTrainError += trainErrors
+                    predY = bestLearner.predict(testX)
+                    meanErrors[k] += bestLearner.getMetricMethod()(testY, predY)
         
+                k+= 1
+                
+            numRealisations = float(numRealisations)
+            meanErrors /=  numRealisations 
+            meanPenalties /=  numRealisations 
         
-    numRealisations = float(numRealisations)
-    meanErrors /=  numRealisations 
-    meanPenalties /=  numRealisations 
-    meanIdealPenalities /=  numRealisations 
-
-    print(meanErrors)
-    """
-    plt.figure(0)
-    plt.plot(foldSizes, meanPenalties, label="Penalty")
-    plt.plot(foldSizes, meanIdealPenalities, label="Ideal penalty")
-    plt.xlabel("Folds")
-    plt.ylabel("Penalty")   
-    plt.legend()
-    """
-    tempFoldSizes = numpy.array(foldSizes, numpy.float)    
-    
-    print(meanIdealPenalities)
-    print(meanPenalties)
-    print(numpy.log(meanPenalties))
-    print(numpy.log((tempFoldSizes-1)/tempFoldSizes*sampleSize))
-    print(numpy.log(meanPenalties)+numpy.log(foldSizes))
-    
-    y = numpy.log(meanPenalties)+numpy.log(foldSizes)    
-    x = numpy.log((tempFoldSizes-1)/tempFoldSizes*sampleSize)
-    
-    print((y[-1]-y[0])/(x[-1]-x[0]))    
-    
-    plt.figure(1)
-    plt.plot(numpy.log((tempFoldSizes-1)/tempFoldSizes*sampleSize), numpy.log(meanPenalties)+numpy.log(foldSizes), label="Penalty")
-    plt.xlabel("log(sampleSize*folds)")
-    plt.ylabel("log(meanPenalties)")   
-    plt.legend()
+            inds = numpy.logical_and(numpy.isfinite(meanPenalties), meanPenalties>0)
+            meanErrors = meanErrors[inds]
+            meanPenalties = meanPenalties[inds]
+            tempFoldSizes = numpy.array(foldSizes, numpy.float)[inds]    
+            
+            print(meanPenalties)        
+            
+            if meanPenalties.shape[0] != 0: 
+                x = numpy.log((tempFoldSizes-1)/tempFoldSizes*sampleSize)
+                y = numpy.log(meanPenalties)+numpy.log(tempFoldSizes)    
+            
+                print(x, y)    
+            
+                betas[i, m], intercept, r, p, err = scipy.stats.linregress(x, y)
+                print(gamma, betas)        
+            
+            """
+            plt.figure(1)
+            plt.plot(x, y, label="Penalty")
+            plt.xlabel("log(sampleSize*folds)")
+            plt.ylabel("log(meanPenalties)")   
+            plt.legend()
+                
+            plt.show()
+            """
         
-    plt.show()
+    print(-betas) 
+    
+    outfileName = outputDir + datasetName + "Beta"
+    numpy.savez(outfileName, -betas)
+    logging.debug("Saved results as file " + outfileName + ".npz")
