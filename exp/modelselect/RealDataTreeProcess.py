34eb02464343c3c387d101898dbc3d9fba100c3e
Charanpal Dhanjal <charanpal@gmail.com>
2012-07-10 17:24:34 +0200

    Clip and find min of beta.

--- exp/modelselect/RealDataTreeProcess.py
+++ exp/modelselect/RealDataTreeProcess.py
@@ -1,4 +1,5 @@
 from apgl.util.PathDefaults import PathDefaults 
+from apgl.util import Util 
 from exp.modelselect.ModelSelectUtils import ModelSelectUtils 
 import matplotlib.pyplot as plt 
 import logging 
@@ -11,6 +12,7 @@ gammas = numpy.unique(numpy.array(numpy.round(2**numpy.arange(1, 7.25, 0.25)-1),
 
 
 print(gammas)
+#To use the betas in practice, pick the lowest value so far 
 
 for datasetName, numRealisations in datasets:
     try: 
@@ -19,11 +21,13 @@ for datasetName, numRealisations in datasets:
         inds = gammas>10
         tempGamma = numpy.sqrt(gammas[inds])
         tempA = A[inds, :]
+        
+        tempA = numpy.clip(tempA, 0, 1)
             
         plt.figure(0)
-        plt.plot(tempGamma, tempA[:, 0], label="50")
-        plt.plot(tempGamma, tempA[:, 1], label="100")
-        plt.plot(tempGamma, tempA[:, 2], label="200")
+        plt.plot(tempGamma, Util.cumMin(tempA[:, 0]), label="50")
+        plt.plot(tempGamma, Util.cumMin(tempA[:, 1]), label="100")
+        plt.plot(tempGamma, Util.cumMin(tempA[:, 2]), label="200")
         plt.legend()
         plt.title(datasetName)    
         plt.xlabel("gamma")
