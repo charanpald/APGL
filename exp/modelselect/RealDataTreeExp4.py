75989155a5f73f7b08c4b9e3068c68a0c2c62c3d
Charanpal Dhanjal <charanpal@gmail.com>
2012-07-11 13:36:40 +0200

    Test learning rate update.

--- exp/modelselect/RealDataTreeExp4.py
+++ exp/modelselect/RealDataTreeExp4.py
@@ -5,7 +5,6 @@ import logging
 import numpy 
 import sys 
 import multiprocessing 
-import scipy.stats 
 from apgl.util.PathDefaults import PathDefaults 
 from exp.modelselect.ModelSelectUtils import ModelSelectUtils 
 from apgl.util.Sampling import Sampling
@@ -19,6 +18,7 @@ numpy.seterr(all="raise")
 numpy.random.seed(45)
 dataDir = PathDefaults.getDataDir() 
 dataDir += "modelPenalisation/regression/"
+outputDir = PathDefaults.getOutputDir() + "modelPenalisation/regression/CART/"
 
 figInd = 0 
 
@@ -26,7 +26,7 @@ loadMethod = ModelSelectUtils.loadRegressDataset
 datasets = ModelSelectUtils.getRegressionDatasets(True)
 
 #sampleSizes = numpy.array([50, 100, 200])
-sampleSizes = numpy.array([50])
+sampleSizes = numpy.array([200])
 foldsSet = numpy.arange(2, 13, 1)
 alpha = 1.0
 
@@ -40,83 +40,62 @@ sampleMethod = Sampling.crossValidation
 numProcesses = multiprocessing.cpu_count()
 Cvs = numpy.array([1])
 
-outputDir = PathDefaults.getOutputDir() + "modelPenalisation/regression/CART/"
-
 for datasetName, numRealisations in datasets:
     logging.debug("Dataset " + datasetName)
-    betas = numpy.zeros((gammas.shape[0], sampleSizes.shape[0]))
+    
     
     learner = DecisionTreeLearner(criterion="mse", maxDepth=100, minSplit=1, pruneType="CART", processes=numProcesses)
     learner.setChunkSize(3)   
     
     outfileName = outputDir + datasetName + "Beta"
+
+    for m in range(sampleSizes.shape[0]): 
+        sampleSize = sampleSizes[m]
+        logging.debug("Sample size " + str(sampleSize))
     
-    fileLock = FileLock(outfileName + ".npz")
-    if not fileLock.isLocked() and not fileLock.fileExists():
-        #fileLock.lock()    
-    
-        for m in range(sampleSizes.shape[0]): 
-            sampleSize = sampleSizes[m]
-            logging.debug("Sample size " + str(sampleSize))
+        penalties = numpy.zeros((foldsSet.shape[0], numParams))
+        betas = numpy.zeros((gammas.shape[0], sampleSizes.shape[0]))
         
-            meanPenalties = numpy.zeros((foldsSet.shape[0], numParams))
-        
-            numRealisations = 1
+        for j in range(numRealisations):      
+            logging.debug("Realisation: " + str(j))
             
-            k = 0 
-            for folds in foldsSet: 
+            trainX, trainY, testX, testY = loadMethod(dataDir, datasetName, j)
+            
+            trainInds = numpy.arange(sampleSize)
+            trainX = trainX[trainInds,:]
+            trainY = trainY[trainInds]
+                           
+            for k in range(foldsSet.shape[0]): 
+                folds = foldsSet[k]
                 logging.debug("Folds " + str(folds))
-                errors = numpy.zeros(numRealisations)
-                        
-                for j in range(numRealisations):
-                    print("")
-                    logging.debug("Realisation: " + str(j))
-                    trainX, trainY, testX, testY = loadMethod(dataDir, datasetName, j)
-                    
-                    trainInds = numpy.arange(sampleSize)
-                    trainX = trainX[trainInds,:]
-                    trainY = trainY[trainInds]
-                    
-                    idx = sampleMethod(folds, trainX.shape[0])        
-                    
-                    #Now try penalisation
-                    resultsList = learner.parallelPen(trainX, trainY, idx, paramDict, Cvs)
-                    bestLearner, trainErrors, currentPenalties = resultsList[0]
-                    meanPenalties[k, :] += currentPenalties
-    
-                k+= 1
                 
-            numRealisations = float(numRealisations)
-            meanPenalties /=  numRealisations 
-            
-            print("meanPenalties="+str(meanPenalties))
+                idx = sampleMethod(folds, trainX.shape[0])   
+                
+                #Now try penalisation
+                resultsList = learner.parallelPen(trainX, trainY, idx, paramDict, Cvs)
+                bestLearner, trainErrors, currentPenalties = resultsList[0]
+                penalties[k, :] += currentPenalties
             
             for i in range(gammas.shape[0]): 
-                inds = numpy.logical_and(numpy.isfinite(meanPenalties[:, i]), meanPenalties[:, i]>0)
-                tempMeanPenalties = meanPenalties[:, i][inds]
-                tempfoldsSet = numpy.array(foldsSet, numpy.float)[inds]                
+                inds = numpy.logical_and(numpy.isfinite(penalties[:, i]), penalties[:, i]>0)
+                tempPenalties = penalties[:, i][inds]
+                tempfoldsSet = numpy.array(foldsSet, numpy.float)[inds]                            
                 
-                print("tempMeanPenalties=" + str(tempMeanPenalties)) 
-                
-                if tempMeanPenalties.shape[0] > 1: 
+                if tempPenalties.shape[0] > 1: 
                     x = numpy.log((tempfoldsSet-1)/tempfoldsSet*sampleSize)
-                    y = numpy.log(tempMeanPenalties)+numpy.log(tempfoldsSet)    
+                    y = numpy.log(tempPenalties)+numpy.log(tempfoldsSet)   
                 
                     clf = linear_model.LinearRegression()
                     clf.fit(numpy.array([x]).T, y)
                     betas[i, m] = clf.coef_[0]    
+                    
+            betas = -betas        
+            print(betas) 
             
-        print(-betas) 
-        
-        betas2 = learner.learningRate(trainX, trainY, foldsSet, paramDict)
-        print(betas2)
-        
-        resultsList = learner.parallelPen(trainX, trainY, idx, paramDict, Cvs)
-        bestLearner, trainErrors, currentPenalties = resultsList[0]
-        print(currentPenalties)
-        
-        
-        numpy.savez(outfileName, -betas)
-        logging.debug("Saved results as file " + outfileName + ".npz")
-        break 
-        #fileLock.unlock()
+            plt.plot(gammas, betas[:, 0])
+            plt.show()
+            
+            betas2 = learner.learningRate(trainX, trainY, foldsSet, paramDict)
+            print(betas2)
+    
+    break 
