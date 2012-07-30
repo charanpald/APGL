c81137e20b4b6a2c8747f0a3d0b7e8cce9b24ff9
Charanpal Dhanjal <charanpal@gmail.com>
2012-07-30 17:03:41 +0200

    Cut redundent test.

--- exp/viroscopy/model/test/HIVGraphMetricsTest.py
+++ exp/viroscopy/model/test/HIVGraphMetricsTest.py
@@ -12,6 +12,18 @@ from exp.sandbox.GraphMatch import GraphMatch
 class  HIVGraphMetricsTest(unittest.TestCase):
     def setUp(self):
         numpy.random.seed(21)
+        numpy.set_printoptions(linewidth=100, suppress=True, precision=3)
+        
+        
+        numVertices = 10
+        self.graph = HIVGraph(numVertices)
+
+        self.graph.getVertexList().setInfected(1, 0.0)
+        self.graph.getVertexList().setDetected(1, 0.1, 0)
+        self.graph.getVertexList().setInfected(2, 2.0)
+        self.graph.getVertexList().setDetected(2, 2.0, 0)
+        self.graph.getVertexList().setInfected(7, 3.0)
+        self.graph.getVertexList().setDetected(7, 3.0, 0)
 
     def testSummary(self): 
         numVertices = 10
@@ -61,67 +73,8 @@ class  HIVGraphMetricsTest(unittest.TestCase):
         currentTime = 0.9        
         self.assertFalse(HIVGraphMetrics(times).shouldBreak(summary2, graph, epsilon, currentTime))
 
-    
-    def testSummary2(self): 
-        numVertices = 10
-        graph = HIVGraph(numVertices)
-
-        graph.getVertexList().setInfected(1, 0.0)
-        graph.getVertexList().setDetected(1, 0.1, 0)
-        graph.getVertexList().setInfected(2, 2.0)
-        graph.getVertexList().setDetected(2, 2.0, 0)
-        graph.getVertexList().setInfected(7, 3.0)
-        graph.getVertexList().setDetected(7, 3.0, 0)
+  
         
-        times = numpy.array([0, 1.0, 2.9, 4.0, 10.0])
-        
-        metrics = HIVGraphMetrics2(times)
-        summary = metrics.summary(graph)
-        
-        self.assertEquals(summary[0].size, 0)   
-        self.assertEquals(summary[1].size, 1)
-        self.assertEquals(summary[2].size, 2)
-        self.assertEquals(summary[3].size, 3)
-
-    def testShouldBreak2(self): 
-        numVertices = 10
-        graph1 = HIVGraph(numVertices)
-        graph1.getVertexList().setInfected(1, 0.0)
-        graph1.getVertexList().setDetected(1, 0.0, 0)
-        graph1.getVertexList().setInfected(2, 2.0)
-        graph1.getVertexList().setDetected(2, 2.0, 0)
-        graph1.getVertexList().setInfected(7, 3.0) 
-        graph1.getVertexList().setDetected(7, 3.0, 0)
-        
-        graph2 = HIVGraph(numVertices)
-        graph2.getVertexList().setInfected(2, 0.0)
-        graph2.getVertexList().setDetected(2, 0.0, 0)
-        graph2.getVertexList().setInfected(3, 2.0)
-        graph2.getVertexList().setDetected(3, 2.0, 0)
-        graph2.getVertexList().setInfected(8, 3.0)
-        graph2.getVertexList().setDetected(8, 3.0, 0)
-        
-        times = numpy.array([0, 1.0, 3.0, 4.0])
-        metrics = HIVGraphMetrics2(times, GraphMatch(alpha=0.7))
-        summary1 = metrics.summary(graph1)
-        summary2 = metrics.summary(graph2)
-        
-        metrics.distance(summary1, summary2)
- 
-        times = numpy.array([0, 1.0, 3.0, 4.0])      
-        epsilon = 0.05
-        
-        currentTime = 1
-        self.assertFalse(metrics.shouldBreak(summary2, graph1, epsilon, currentTime))
-
-        currentTime = 2        
-        self.assertFalse(metrics.shouldBreak(summary2, graph1, epsilon, currentTime))
-        
-        currentTime = 3        
-        self.assertFalse(metrics.shouldBreak(summary2, graph1, epsilon, currentTime))
-        
-        currentTime = 4        
-        self.assertTrue(metrics.shouldBreak(summary2, graph1, epsilon, currentTime))
 
 if __name__ == '__main__':
     unittest.main()
