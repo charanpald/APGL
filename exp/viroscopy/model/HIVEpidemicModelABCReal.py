a022ed228aea3e61ff141b9678fa9fce9dc54d51
Charanpal Dhanjal <charanpal@gmail.com>
2012-08-17 16:47:55 +0100

    Reduce sample.

--- exp/viroscopy/model/HIVEpidemicModelABCReal.py
+++ exp/viroscopy/model/HIVEpidemicModelABCReal.py
@@ -63,7 +63,7 @@ if len(sys.argv) > 1:
 else: 
     numProcesses = multiprocessing.cpu_count()
 
-posteriorSampleSize = 30
+posteriorSampleSize = 10
 thetaLen = 10
 
 logging.debug("Posterior sample size " + str(posteriorSampleSize))
