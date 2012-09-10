cf627d10f55d23c3db42d05eb0f0c63a97c2091d
Charanpal Dhanjal <charanpal@gmail.com>
2012-09-10 17:09:14 +0200

    Plot results during test period.

--- exp/viroscopy/model/HIVEpidemicStatistics.py
+++ exp/viroscopy/model/HIVEpidemicStatistics.py
@@ -6,11 +6,14 @@ import matplotlib.pyplot as plt
 from apgl.graph.GraphStatistics import GraphStatistics 
 from apgl.util.PathDefaults import PathDefaults
 from apgl.util.Util import Util 
+from apgl.util.Latex import Latex 
 from apgl.predictors.ABCSMC import ABCSMC, loadThetaArray 
 from exp.viroscopy.model.HIVModelUtils import HIVModelUtils
 from exp.viroscopy.model.HIVVertices import HIVVertices
 from exp.sandbox.GraphMatch import GraphMatch
 from exp.viroscopy.model.HIVGraphMetrics2 import HIVGraphMetrics2
+
+assert False, "Must run with -O flag"
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.set_printoptions(suppress=True, precision=4, linewidth=150)
@@ -19,8 +22,12 @@ plotStyles = ['k-', 'kx-', 'k+-', 'k.-', 'k*-']
 
 resultsDir = PathDefaults.getOutputDir() + "viroscopy/toy/"
 startDate, endDate, recordStep, M, targetGraph = HIVModelUtils.toySimulationParams()
+endDate += HIVModelUtils.toyTestPeriod
+
 #resultsDir = PathDefaults.getOutputDir() + "viroscopy/real/"
 #startDate, endDate, recordStep, M, targetGraph = HIVModelUtils.realSimulationParams()
+#endDate += HIVModelUtils.realTestPeriod
+
 
 thetaDir = resultsDir + "theta/" 
 saveResults = False 
@@ -65,22 +72,35 @@ if saveResults:
         paramList.append((i, thetaArray[i, :]))
 
     pool = multiprocessing.Pool(multiprocessing.cpu_count())               
-    #resultIterator = pool.map(saveStats, paramList)  
-    resultIterator = map(saveStats, paramList)  
+    resultIterator = pool.map(saveStats, paramList)  
+    #resultIterator = map(saveStats, paramList)  
+    pool.terminate()
 
     #Now save the statistics on the target graph 
     stats = HIVModelUtils.generateStatistics(targetGraph, startDate, endDate, recordStep)
     resultsFileName = resultsDir + "IdealStats.pkl"
     Util.savePickle(stats, resultsFileName)
 else:
-    for i in range(t+1): 
-        thetaArray = loadThetaArray(N, thetaDir, i)
-        logging.debug(thetaArray.mean(0))
-        #logging.debug(thetaArray.std(0))
+    realTheta, sigmaTheta = HIVModelUtils.toyTheta()
+    thetaArray = loadThetaArray(N, thetaDir, t)
+    print(realTheta)
+    print(thetaArray)    
+    
+    
+    meanTable = numpy.c_[realTheta, thetaArray.mean(0)]
+    stdTable = numpy.c_[sigmaTheta, thetaArray.std(0)]
+    table = Latex.array2DToRows(meanTable, stdTable)
+    rowNames = ["$\\|\\mathcal{I}_0 \\|$", "$\\rho_B$", "$\\alpha$", "$C$", "$\\gamma$", "$\\beta$", "$\\kappa_{max}$", "$\\lambda_H$", "$\\lambda_B$", "$\\sigma_{WM}$",  "$\\sigma_{MW}$","$\\sigma_{MB}$"]
+    table = Latex.addRowNames(rowNames, table)
+    print(table)
+
     
     resultsFileName = resultsDir + "IdealStats.pkl"
     stats = Util.loadPickle(resultsFileName)  
     times, vertexArray, removedGraphStats = stats 
+    
+    times2 = range(int(startDate), int(endDate)+1, recordStep)  
+    times2 = times2[1:]
     
     graphStats = GraphStatistics()
     
@@ -88,66 +108,59 @@ else:
     plotInd = 0 
     
     plt.figure(plotInd)
-    plt.title("Removed")
     plt.plot(times, vertexArray[:, 0], "r")
     plt.xlabel("Time (days)")
-    plt.ylabel("Individuals")
+    plt.ylabel("Removed")
     plotInd += 1
     
     plt.figure(plotInd)
-    plt.title("Males")
     plt.plot(times, vertexArray[:, 1], "r")
     plt.xlabel("Time (days)")
-    plt.ylabel("Individuals")
+    plt.ylabel("Males")
     plotInd += 1
     
     plt.figure(plotInd)
-    plt.title("Females")
     plt.plot(times, vertexArray[:, 2], "r")
     plt.xlabel("Time (days)")
-    plt.ylabel("Individuals")
+    plt.ylabel("Females")
     plotInd += 1
     
     plt.figure(plotInd)
-    plt.title("Hetero")
     plt.plot(times, vertexArray[:, 3], "r")
     plt.xlabel("Time (days)")
-    plt.ylabel("Individuals")
+    plt.ylabel("Hetero")
     plotInd += 1
     
     plt.figure(plotInd)
-    plt.title("Bis")
     plt.plot(times, vertexArray[:, 4], "r")
     plt.xlabel("Time (days)")
-    plt.ylabel("Individuals")
+    plt.ylabel("Bi")
     plotInd += 1
     
     plt.figure(plotInd)
-    plt.title("Random detection")
     plt.plot(times, vertexArray[:, 5], "r")
     plt.xlabel("Time (days)")
-    plt.ylabel("Individuals")
+    plt.ylabel("Random detection")
     plotInd += 1
     
     plt.figure(plotInd)
-    plt.title("Contact Tracing")
     plt.plot(times, vertexArray[:, 6], "r")
     plt.xlabel("Time (days)")
-    plt.ylabel("Individuals")
+    plt.ylabel("Contact Tracing")
     plotInd += 1
     
     plt.figure(plotInd)
-    plt.title("Components")
     plt.plot(times, removedGraphStats[:, graphStats.numComponentsIndex], "r")
     plt.xlabel("Time (days)")
     plt.ylabel("Number of components")
     plotInd += 1
 
     plt.figure(plotInd)
-    plt.title("Graph distance")
     plt.xlabel("Time (days)")
-    plt.ylabel("Distance")
+    plt.ylabel("Graph distance")
     plotInd += 1
+    
+    meanDists = []
 
     for i in range(thetaArray.shape[0]): 
         plotInd = 0
@@ -189,7 +202,12 @@ else:
         plotInd += 1
 
         plt.figure(plotInd)
-        plt.plot(times, dists, plotStyles[0])
+        plt.plot(times2, dists, plotStyles[0])
         plotInd += 1
+        meanDists.append(dists)
+        print(numpy.array(dists).mean())
+    
+    meanDists = numpy.array(meanDists).mean(0)
+    plt.plot(times2, meanDists, "b")  
     
     plt.show()
