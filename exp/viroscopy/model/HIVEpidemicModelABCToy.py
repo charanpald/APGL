74c22c3f0766ba5edd1b980f5ee2dec1c532c072
Charanpal Dhanjal <charanpal@gmail.com>
2012-09-18 16:08:01 +0200

    Reduce population

--- exp/viroscopy/model/HIVEpidemicModelABCToy.py
+++ exp/viroscopy/model/HIVEpidemicModelABCToy.py
@@ -30,7 +30,7 @@ startDate, endDate, recordStep, M, targetGraph = HIVModelUtils.toySimulationPara
 epsilonArray = numpy.ones(10)*0.4
 logging.debug("Total time of simulation is " + str(endDate-startDate))
 
-posteriorSampleSize = 100
+posteriorSampleSize = 50
 breakDist = 0.5
 logging.debug("Posterior sample size " + str(posteriorSampleSize))
 
