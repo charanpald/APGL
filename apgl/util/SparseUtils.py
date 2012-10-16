be2ec8036e13c48104519153fc32deeb2ea37a43
Charanpal Dhanjal <charanpal@gmail.com>
2012-10-16 16:28:33 +0200

    Added resize method

--- apgl/util/SparseUtils.py
+++ apgl/util/SparseUtils.py
@@ -43,3 +43,20 @@ class SparseUtils(object):
         elements = X.data
         return numpy.sqrt((elements**2).sum()) 
         
+    @staticmethod
+    def resize(X, shape): 
+        """
+        Resize a sparse matrix to the given shape, padding with zero if required. 
+        """
+        Y = scipy.sparse.csr_matrix((shape))
+        rows, cols = X.nonzero()
+        
+        for ind in range(rows.shape[0]):
+            i = rows[ind]
+            j = cols[ind]
+            if i < Y.shape[0] and j < Y.shape[1]: 
+                Y[i, j] = X[i, j]
+        
+        return Y 
+        
+        
