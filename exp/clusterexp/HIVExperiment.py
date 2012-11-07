548d81cd428f4ff41434a3262f25db23af98bb7e
Charanpal Dhanjal <charanpal@gmail.com>
2012-11-07 16:56:02 +0100

    remove most args and fix params k1, k2, k3

--- exp/clusterexp/HIVExperiment.py
+++ exp/clusterexp/HIVExperiment.py
@@ -5,13 +5,10 @@ Compare our clustering method and that of Ning et al. on the HIV data.
 import os
 import sys
 import errno
-import itertools
 import logging
 import numpy
 from apgl.graph import *
-from apgl.util.PathDefaults import PathDefaults
 from exp.clusterexp.ClusterExpHelper import ClusterExpHelper
-from apgl.util.Parameter import Parameter
 import argparse
 from exp.viroscopy.HIVGraphReader import HIVGraphReader
 from apgl.util.DateUtils import DateUtils
@@ -27,7 +24,7 @@ from exp.sandbox.GraphIterators import IncreasingSubgraphListIterator
 dataArgs = argparse.Namespace()
 dataArgs.startYear = 1900
 dataArgs.daysInMonth = 30
-dataArgs.monthStep = 3
+dataArgs.monthStep = 1
 dataArgs.minGraphSize = 500
 
 # Arguments related to the algorithm
@@ -38,10 +35,6 @@ defaultAlgoArgs = argparse.Namespace()
 #defaultAlgoArgs.runModularity = True
 #defaultAlgoArgs.runNystrom = True
 #defaultAlgoArgs.runNing = True
-
-#defaultAlgoArgs.k1 = 5
-#defaultAlgoArgs.k2 = 10
-#defaultAlgoArgs.k3 = 15
     
 #=========================================================================
 #=========================================================================
@@ -52,10 +45,6 @@ defaultAlgoArgs = argparse.Namespace()
 # data args parser #
 dataParser = argparse.ArgumentParser(description="", add_help=False)
 dataParser.add_argument("-h", "--help", action="store_true", help="show this help message and exit")
-dataParser.add_argument("--startYear", type=int, help="", default=dataArgs.startYear)
-dataParser.add_argument("--daysInMonth", type=int, help="", default=dataArgs.daysInMonth)
-dataParser.add_argument("--monthStep", type=int, help="", default=dataArgs.monthStep)
-dataParser.add_argument("--minGraphSize", type=int, help="", default=dataArgs.minGraphSize)
 devNull, remainingArgs = dataParser.parse_known_args(namespace=dataArgs)
 if dataArgs.help:
     helpParser  = argparse.ArgumentParser(description="", add_help=False, parents=[dataParser, ClusterExpHelper.newAlgoParser(defaultAlgoArgs)])
@@ -85,8 +74,6 @@ keys = list(vars(dataArgs).keys())
 keys.sort()
 for key in keys:
     logging.info("    " + str(key) + ": " + str(dataArgs.__getattribute__(key)))
-
-
 
 
 #=========================================================================
@@ -129,11 +116,6 @@ for i in dayList:
     if subgraphIndices.shape[0] >= dataArgs.minGraphSize: 
         subgraphIndicesList.append(subgraphIndices)
 
-#subgraphIndicesList2 = []
-#for i in range(5):
-#    subgraphIndicesList2.append(subgraphIndicesList[i])
-#subgraphIndicesList = subgraphIndicesList2
-
 def getIterator():
     return IncreasingSubgraphListIterator(graph, subgraphIndicesList)
     
@@ -146,10 +128,11 @@ logging.info("number of iterations: " + str(numGraphs))
 #=========================================================================
 #=========================================================================
 logging.info("Creating the exp-runner")
-clusterExpHelper = ClusterExpHelper(getIterator, numGraphs, remainingArgs, defaultAlgoArgs)
+clusterExpHelper = ClusterExpHelper(getIterator, numGraphs, remainingArgs, defaultAlgoArgs, dataArgs.extendedDirName)
 clusterExpHelper.printAlgoArgs()
-
-clusterExpHelper.extendResultsDir(dataArgs.extendedDirName)
+clusterExpHelper.algoArgs.k1 = 25
+clusterExpHelper.algoArgs.k2s = [100, 200, 500]
+clusterExpHelper.algoArgs.k3s = [100, 200, 500, 1000]
 #    os.makedirs(resultsDir, exist_ok=True) # for python 3.2
 try:
     os.makedirs(clusterExpHelper.resultsDir)
