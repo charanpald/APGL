88b96b5c2a1913ec830864e7fac679fd207ade0b
Charanpal Dhanjal <charanpal@gmail.com>
2012-11-09 16:59:26 +0100

    Plot labels

--- exp/clusterexp/CitationIterStats.py
+++ exp/clusterexp/CitationIterStats.py
@@ -10,7 +10,7 @@ import logging
 import numpy
 from exp.clusterexp.CitationIterGenerator import CitationIterGenerator 
 from apgl.graph import GraphStatistics 
-from apgl.graph import SparseGraph, GeneralVertexList 
+from apgl.graph import SparseGraph 
 import matplotlib.pyplot as plt 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -22,7 +22,7 @@ subgraphIndicesList = []
 for W in iterator: 
     subgraphIndicesList.append(range(W.shape[0])) 
     
-graph = SparseGraph(GeneralVertexList(W.shape[0]))
+graph = SparseGraph(W.shape[0], W=W)
 
 graphStats = GraphStatistics()
 statsMatrix = graphStats.sequenceScalarStats(graph, subgraphIndicesList, slowStats=False)
@@ -30,11 +30,17 @@ statsMatrix = graphStats.sequenceScalarStats(graph, subgraphIndicesList, slowSta
 
 plt.figure(0)
 plt.plot(numpy.arange(statsMatrix.shape[0]), statsMatrix[:, graphStats.numVerticesIndex])
+plt.xlabel("Graph index")
+plt.ylabel("Num vertices")
 
 plt.figure(1)
 plt.plot(numpy.arange(statsMatrix.shape[0]), statsMatrix[:, graphStats.maxComponentSizeIndex])
+plt.xlabel("Graph index")
+plt.ylabel("Max component size")
 
 plt.figure(2)
 plt.plot(numpy.arange(statsMatrix.shape[0]), statsMatrix[:, graphStats.numComponentsIndex])
+plt.xlabel("Graph index")
+plt.ylabel("Num components")
 
 plt.show()
