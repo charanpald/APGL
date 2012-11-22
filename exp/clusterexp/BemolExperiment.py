1d3e089e30226a501f53caa846d902551a478baa
Romaric Gaudel <romaric.gaudel@univ-lille3.fr>
2012-11-22 14:30:00 +0100

    Add command line option: maxComponents
    also put back default options at beginning of the file
    also add possibility for a verbose results dirname

--- exp/clusterexp/BemolExperiment.py
+++ exp/clusterexp/BemolExperiment.py
@@ -33,6 +33,7 @@ dataArgs.nbPurchasesPerIt = 50 # set to 'None' to take all the purchases
 dataArgs.startingIteration = 0
 dataArgs.endingIteration = None # set to 'None' to have all iterations
 dataArgs.stepSize = 30 #This is the step in the number of weeks 
+dataArgs.maxComponents = None
 
 # Arguments related to the algorithm
 # If one arg is not set, default from ClusterExpHelper.py is used
@@ -43,9 +44,11 @@ defaultAlgoArgs = argparse.Namespace()
 #defaultAlgoArgs.runNystrom = True
 #defaultAlgoArgs.runNing = True
 
-#defaultAlgoArgs.k1 = 5
-#defaultAlgoArgs.k2 = 10
-#defaultAlgoArgs.k3 = 15
+defaultAlgoArgs.k1 = 30
+defaultAlgoArgs.k2s = [30, 60, 100, 200]
+defaultAlgoArgs.k3s = [30, 60, 100, 200, 400]
+
+defaultAlgoArgs.T = 10 
 
 #=========================================================================
 #=========================================================================
@@ -71,11 +74,12 @@ def isIntOrNone(string):
 # data args parser #
 dataParser = argparse.ArgumentParser(description="", add_help=False)
 dataParser.add_argument("-h", "--help", action="store_true", help="show this help message and exit")
-dataParser.add_argument("--nbUser", type=isIntOrNone, help="The graph is based on the $nbUser$ first users", default=dataArgs.nbUser)
-dataParser.add_argument("--nbPurchasesPerIt", type=isIntOrNone, help="Maximum number of purchases per iteration", default=dataArgs.nbPurchasesPerIt)
-dataParser.add_argument("--startingIteration", type=int, help="At which iteration to start clustering algorithms", default=dataArgs.startingIteration)
-dataParser.add_argument("--endingIteration", type=isIntOrNone, help="At which iteration to end clustering algorithms", default=dataArgs.endingIteration)
-dataParser.add_argument("--stepSize", type=int, help="Number of iterations between each clustering", default=dataArgs.stepSize)
+dataParser.add_argument("--nbUser", type=isIntOrNone, help="The graph is based on the $nbUser$ first users (default: %(default)s)", default=dataArgs.nbUser)
+dataParser.add_argument("--nbPurchasesPerIt", type=isIntOrNone, help="Maximum number of purchases per iteration (default: %(default)s)", default=dataArgs.nbPurchasesPerIt)
+dataParser.add_argument("--startingIteration", type=int, help="At which iteration to start clustering algorithms (default: %(default)s)", default=dataArgs.startingIteration)
+dataParser.add_argument("--endingIteration", type=isIntOrNone, help="At which iteration to end clustering algorithms (default: %(default)s)", default=dataArgs.endingIteration)
+dataParser.add_argument("--stepSize", type=int, help="Number of iterations between each clustering (default: %(default)s)", default=dataArgs.stepSize)
+dataParser.add_argument("--maxComponents", type=isIntOrNone, help="Graphs with more than MAXCOMPONENTS components are filtered out (default: %(default)s)", default=dataArgs.maxComponents)
 devNull, remainingArgs = dataParser.parse_known_args(namespace=dataArgs)
 if dataArgs.help:
     helpParser  = argparse.ArgumentParser(description="", add_help=False, parents=[dataParser, ClusterExpHelper.newAlgoParser(defaultAlgoArgs)])
@@ -83,6 +87,7 @@ if dataArgs.help:
     exit()
 
 dataArgs.extendedDirName = "Bemol/"
+#dataArgs.extendedDirName = "Bemol_nbU=" + str(dataArgs.nbUser) + "_nbPurchPerIt=" + str(dataArgs.nbPurchasesPerIt) + "_startIt=" + str(dataArgs.startingIteration) + "_endIt=" + str(dataArgs.endingIteration) + "_maxComponents=" + str(dataArgs.maxComponents) + "/"
 
 # seed #
 numpy.random.seed(21)
@@ -105,11 +110,11 @@ for key in keys:
 #=========================================================================
 #=========================================================================
 dataDir = PathDefaults.getDataDir() + "cluster/"
-maxComponents = 50 
 
 def getIterator():
     bemolIterator = BemolData.getGraphIterator(dataDir, dataArgs.nbUser, dataArgs.nbPurchasesPerIt)
-    bemolIterator = MaxComponentsIterator(bemolIterator, maxComponents)
+    if dataArgs.maxComponents:
+        bemolIterator = MaxComponentsIterator(bemolIterator, dataArgs.maxComponents)
     return itertools.islice(bemolIterator, dataArgs.startingIteration, dataArgs.endingIteration, dataArgs.stepSize)
 
 logging.info("Computing the number of iterations")
@@ -130,11 +135,6 @@ logging.info("Number of iterations: " + str(numGraphs))
 
 logging.info("Creating the exp-runner")
 clusterExpHelper = ClusterExpHelper(getIterator, numGraphs, remainingArgs, defaultAlgoArgs, dataArgs.extendedDirName)
-clusterExpHelper.algoArgs.T = 20 
-clusterExpHelper.algoArgs.k1 = 50
-clusterExpHelper.algoArgs.k2s = [50, 100, 200, 500]
-#clusterExpHelper.algoArgs.k3s = [500, 1000, 2000, 5000]
-clusterExpHelper.algoArgs.k3s = [2000, 5000]
 clusterExpHelper.printAlgoArgs()
 
 #    os.makedirs(resultsDir, exist_ok=True) # for python 3.2
