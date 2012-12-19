99e2b6fd7e7299bc0bab02d151cb9b285ad6fa71
Charanpal Dhanjal <charanpal@gmail.com>
2012-12-19 17:03:11 +0100

    Added argsort

--- apgl/util/Util.py
+++ apgl/util/Util.py
@@ -669,4 +669,11 @@ class Util(object):
             
         return u 
         
+    @staticmethod         
+    def argsort(seq):
+        """
+        Find the indices of a sequence after being sorted. Code taken from 
+        http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
+        """
+        return sorted(range(len(seq)), key = seq.__getitem__)
         
