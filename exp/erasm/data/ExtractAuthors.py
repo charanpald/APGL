91f9cc5e22c1f213ef14902234d38f61845306bf
Charanpal Dhanjal <charanpal@gmail.com>
2013-01-22 18:14:35 +0100

    Output igraph directly

--- exp/erasm/data/ExtractAuthors.py
+++ exp/erasm/data/ExtractAuthors.py
@@ -4,24 +4,27 @@ We will analyse the article metadata file and extract co-authors.
 """
 
 from apgl.util.PathDefaults import PathDefaults 
+import os
 import logging 
 import sys 
 import itertools 
 import igraph 
 import matplotlib.pyplot as plt 
 import json 
+import gc 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-#path = "/local/dhanjalc/dataDump-28-11-12/" 
-path = PathDefaults.getDataDir() + "erasm/"
+path = "/local/dhanjalc/dataDump-28-11-12/" 
+fileName = path + "articleMetadata-28-11-12"
 
-#fileName = path + "articleMetadata-28-11-12"
-fileName = path + "articleMetadata100000"
+if not os.path.exists(fileName): 
+    path = PathDefaults.getDataDir() + "erasm/"
+    fileName = path + "articleMetadata100000"
 
+logging.debug("Loading article metadata from " + fileName)
 
 fileObj = open(fileName, 'r')
-articleMetadataList = []
 vertexIdDict = {} 
 vertexIdSet = set([])
 vertexIdList = []
@@ -29,47 +32,65 @@ edgeSet = set([])
 edgeArray = []
 
 i = 0 
+lineInd = 0 
+emptyAuthors = 0
+
+newEdges = []
+graph = igraph.Graph()
+
+#TODO: Write out file in real time 
 
 for line in fileObj: 
-    if i % 1000 == 0: 
-        print(i)
+    if lineInd % 1000 == 0: 
+        print("Line " + str(lineInd) + " Author " + str(len(vertexIdSet)) + " empty author strings " + str(emptyAuthors)) 
+        graph.add_edges(newEdges)
+        del newEdges 
+        newEdges = []
+        gc.collect()
     
     articleMetaData = json.loads(line)
     
     if "authors" in articleMetaData: 
         authors = articleMetaData["authors"]
+        del articleMetaData
         
         coauthorList = []
         for author in authors: 
-            authorString = author["forename"] + " " + author["surname"]
+            authorString = "".join([author["forename"], " ", author["surname"]])
             authorString = authorString.strip()         
             
             if len(authorString) != 0: 
+                if authorString not in vertexIdSet: 
+                    graph.add_vertex(authorString)                   
+                
                 coauthorList.append(authorString)
-        
-            if len(authorString) != 0 and authorString not in vertexIdSet:  
-                vertexIdDict[authorString] = i
                 vertexIdSet.add(authorString)
-                vertexIdList.append(authorString)
-                i += 1 
-        
-        iterator = itertools.permutations(range(len(coauthorList)), 2)
-        
-        for j, k in iterator: 
-            vId1 = coauthorList[j]
-            vId2 = coauthorList[k]            
+                
+                del authorString 
+            else: 
+                emptyAuthors += 1
             
-            if (vertexIdDict[vId1], vertexIdDict[vId2]) not in edgeSet and (vertexIdDict[vId2], vertexIdDict[vId1]) not in edgeSet: 
-                edgeArray.append([vertexIdDict[vId1], vertexIdDict[vId2]])
-                edgeSet.add((vertexIdDict[vId1], vertexIdDict[vId2]))
+        iterator = itertools.combinations(coauthorList, 2)
+        del coauthorList 
+        
+        for vId1, vId2 in iterator:         
+            newEdges.append([vId1, vId2])
 
-graph = igraph.Graph()
-graph.add_vertices(len(vertexIdSet))
-graph.add_edges(edgeArray) 
+    lineInd += 1 
 
-print(graph.summary())
+print(sys.getsizeof(vertexIdDict))
+print(sys.getsizeof(vertexIdSet))
+print(sys.getsizeof(vertexIdList))
+print(sys.getsizeof(edgeSet))
+print(sys.getsizeof(edgeArray))
+print(sys.getsizeof(newEdges))
+print(sys.getsizeof(graph))
 
-sortedNames = sorted(vertexIdDict.keys(), key=lambda name: name.split()[-1])
+logging.debug(graph.summary())
+
+print(graph.neighbors("David Anderson"))
+
+#sortedNames = sorted(vertexIdDict.keys(), key=lambda name: name.split()[-1])
 #for name in sortedNames: 
 #    print(name)
 
