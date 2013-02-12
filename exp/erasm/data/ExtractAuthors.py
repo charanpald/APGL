9a16ac97460add68539a5717c0ec8bd901278273
Charanpal Dhanjal <charanpal@gmail.com>
2013-02-12 17:29:27 +0100

    Update to save rating matrix

--- exp/erasm/data/ExtractAuthors.py
+++ exp/erasm/data/ExtractAuthors.py
@@ -10,6 +10,9 @@ import sys
 import itertools 
 import json 
 from apgl.util.ProfileUtils import ProfileUtils 
+import numpy
+import scipy 
+import scipy.io 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -89,5 +92,55 @@ def saveAuthors():
     
     logging.debug("Saved edges as " + edgeFileName)
     
+def saveRatingMatrix(): 
+    """
+    Take the coauthor graph above and make vertices indexed from 0 then save 
+    as matrix market format. 
+    """    
+    edgeFileName = PathDefaults.getOutputDir() + "erasm/edges2.txt"
     
-ProfileUtils.profile('saveAuthors()', globals(), locals())
+    logging.debug("Reading edge list")
+    edges = numpy.loadtxt(edgeFileName, delimiter=",", dtype=numpy.int)
+    logging.debug("Total number of edges: " + str(edges.shape[0]))
+    
+    vertexIdDict = {} 
+    vertexIdSet = set([])
+    
+    i = 0 
+        
+    for edge in edges:
+        if edge[0] not in vertexIdSet: 
+            vertexIdDict[edge[0]] = i
+            vertexIdSet.add(edge[0])
+            i += 1 
+         
+        if edge[1] not in vertexIdSet: 
+            vertexIdDict[edge[1]] = i 
+            vertexIdSet.add(edge[1])
+            i += 1 
+
+    n = len(vertexIdDict)    
+    R = scipy.sparse.lil_matrix((n, n))
+    logging.debug("Creating sparse matrix")
+    
+    for edge in edges:
+        R[vertexIdDict[edge[0]], vertexIdDict[edge[1]]] += 1 
+        R[vertexIdDict[edge[1]], vertexIdDict[edge[0]]] += 1 
+        
+    logging.debug("Created matrix " + str(R.shape) + " with " + str(R.getnnz()) + " non zeros")    
+
+    R = R.tocsr()    
+    
+    minCoauthors = 20
+    logging.debug("Removing vertices with <" + str(minCoauthors) + " coauthors")
+    nonzeros = R.nonzero()    
+    inds = numpy.arange(nonzeros[0].shape[0])[numpy.bincount(nonzeros[0]) >= minCoauthors]
+    R = R[inds, :][:, inds]
+    logging.debug("Matrix has shape " + str(R.shape) + " with " + str(R.getnnz()) + " non zeros")    
+        
+    matrixFileName = PathDefaults.getOutputDir() + "erasm/R"
+    scipy.io.mmwrite(matrixFileName, R)
+    logging.debug("Wrote matrix to file " + matrixFileName)
+    
+#saveAuthors()
+saveRatingMatrix()
