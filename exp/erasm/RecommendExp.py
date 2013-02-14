a5343e5c58b91613e2d5d92242784b0193b12e1e
Charanpal Dhanjal <charanpal@gmail.com>
2013-02-14 14:08:00 +0100

    Multiprocessing and test different factorisation methods.

--- exp/erasm/RecommendExp.py
+++ exp/erasm/RecommendExp.py
@@ -1,17 +1,17 @@
 """
-Test some recommendation with the movielen data 
+Test some recommendation with the Mendeley coauthor data 
 """
 
 from apgl.util.PathDefaults import PathDefaults 
+from apgl.util.Sampling import Sampling 
 import nimfa
 import numpy 
 import matplotlib.pyplot as plt 
 from os.path import dirname, abspath, sep
-from warnings import warn
 import scipy.io 
 import logging 
 import sys
-from apgl.util.Sampling import Sampling 
+import multiprocessing 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.set_printoptions(linewidth=1000, threshold=10000000)
@@ -35,17 +35,63 @@ def preprocess(V):
     logging.debug("Finished.")  
     return V, maxs
 
-def readCoauthors(): 
+
+def factorise(args):
+    maxIter = 30
+    R, trainInds, testInds, rank, method = args 
+    
+    logging.debug("Computing factorisation with rank " + str(rank))
+    rowInds, colInds = R.nonzero()
+    
+    trainR = scipy.sparse.csr_matrix(R.shape)
+    for ind in trainInds: 
+        trainR[rowInds[ind], colInds[ind]] = R[rowInds[ind], colInds[ind]]
+        
+    testR = scipy.sparse.csr_matrix(R.shape) 
+    for ind in testInds: 
+        testR[rowInds[ind], colInds[ind]] = R[rowInds[ind], colInds[ind]] 
+    
+    model = nimfa.mf(trainR, method=method, max_iter=maxIter, rank=rank)
+    fit = nimfa.mf_run(model)
+    W = fit.basis()
+    H = fit.coef()
+    
+    predR = W.dot(H)
+    
+    #Compute train error 
+    rowInds, colInds = trainR.nonzero()
+    trainError = 0 
+
+    for i in range(rowInds.shape[0]): 
+        trainError += (trainR[rowInds[i], colInds[i]] - predR[rowInds[i], colInds[i]])**2 
+        
+    trainError /= rowInds.shape[0]
+    
+    #Compute test error
+    rowInds, colInds = testR.nonzero()
+    testError = 0 
+
+    for i in range(rowInds.shape[0]): 
+        testError += (testR[rowInds[i], colInds[i]] - predR[rowInds[i], colInds[i]])**2 
+        
+    testError /= rowInds.shape[0]
+    
+    return trainError, testError 
+    
+
+def recommend(method): 
     """
     Take a list of coauthors and read in the complete graph into a sparse 
     matrix R such that R_ij = k means author i has worked with j, k times. Then 
     do matrix factorisation on the resulting methods. 
     """
-    matrixFileName = PathDefaults.getOutputDir() + "erasm/R"
+    outputDir = PathDefaults.getOutputDir() + "erasm/" 
+    matrixFileName = outputDir + "R"
     
-    numExamples = 100 
+    
+    numExamples = 1000 
     numFolds = 3    
-    ranks = numpy.arange(10, 14, 3)
+    ranks = numpy.arange(10, 25, 3)
     
     trainErrors = numpy.zeros((ranks.shape[0], numFolds))
     testErrors = numpy.zeros((ranks.shape[0], numFolds))
@@ -61,57 +107,48 @@ def readCoauthors():
     randInds = numpy.random.permutation(rowInds.shape[0])
     indexList = Sampling.crossValidation(numFolds, rowInds.shape[0])
     
+    paramList = [] 
     for j, (trnIdx, tstIdx) in enumerate(indexList): 
-        logging.debug("Fold index " + str(j))
         trainInds = randInds[trnIdx]
         testInds = randInds[tstIdx]
         
-        trainR = scipy.sparse.csr_matrix(R.shape)
-        for ind in trainInds: 
-            trainR[rowInds[ind], colInds[ind]] = R[rowInds[ind], colInds[ind]]
-            
-        testR = scipy.sparse.csr_matrix(R.shape) 
-        for ind in testInds: 
-            testR[rowInds[ind], colInds[ind]] = R[rowInds[ind], colInds[ind]]  
-        
         for i, rank in enumerate(ranks):
-            logging.debug("Rank=" + str(rank))
-            model = nimfa.mf(trainR, method = "lsnmf", max_iter = 50, rank = rank, update = 'Euclidean', objective = 'div')
-            logging.debug("Performing factorisation") 
-            fit = nimfa.mf_run(model)
-            logging.debug("Finished")
-            sparse_w, sparse_h = fit.fit.sparseness()
+            paramList.append((R, trainInds, testInds, rank, method))
         
-            W = fit.basis()
-            H = fit.coef()
-            
-            predR = W.dot(H)
-            
-            #Compute train error  
-            for ind in trainInds: 
-                trainErrors[i, j] += (trainR[rowInds[ind], colInds[ind]] - predR[rowInds[ind], colInds[ind]])**2
-          
-            trainErrors[i, j] /= trainInds.shape[0]  
-          
-            #Compute test error 
-            for ind in testInds: 
-                testErrors[i, j] += (testR[rowInds[ind], colInds[ind]] - predR[rowInds[ind], colInds[ind]])**2
-                
-            testErrors[i, j] /= testInds.shape[0]  
-            logging.debug(testErrors[i, j])
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+    results = pool.map(factorise, paramList)
     
+    k = 0 
+    for j in range(len(indexList)): 
+        for i in range(len(ranks)):
+            trainErrors[i, j], testErrors[i, j] = results[k]
+            k += 1 
+            
     meanTrainErrors = trainErrors.mean(1)
     meanTestErrors = testErrors.mean(1)
         
     logging.debug("Train errors = " + str(meanTrainErrors))
     logging.debug("Test errors = " + str(meanTestErrors))
     
-    plt.plot(ranks, meanTrainErrors)
-    plt.plot(ranks, meanTestErrors)
+    errorFileName = outputDir + "results_" + method
+    numpy.savez(errorFileName, meanTrainErrors, meanTestErrors)    
+    
+    """
+    plt.plot(ranks, meanTrainErrors, label="Train Error")
+    plt.plot(ranks, meanTestErrors, label="Test Error")
+    plt.xlabel("Rank")
+    plt.ylabel("MSE")
+    plt.legend()
     plt.show()
-       
-readCoauthors()
+    """
+
+methods = ["icm", "lfnmf", "lsnmf", "nmf", "nsnmf" "pmf", "psnmf", "snmf"]      
+
+for method in methods: 
+    recommend(method)
 
 #TODO: 
-#Try a selection of factorisation methods 
-#Parallelism 
+#Try matrix completion 
+#Profile 
+#Different error methods 
+#Save results 
