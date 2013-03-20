c96edf19a61a29e57f1b723d2cf507471fb67e1e
Charanpal Dhanjal <charanpal@gmail.com>
2013-03-20 17:28:35 +0100

    Make result non negative

--- exp/erasm/GenerateToyData.py
+++ exp/erasm/GenerateToyData.py
@@ -18,11 +18,14 @@ A = numpy.random.rand(numExamples, numExamples)
 A = A.dot(A.T)
 s, U = numpy.linalg.eig(A)
 U = U[:, 0:rank] 
+#Make sure result is non-negative by taking the absolute value of single vectors 
+U = numpy.abs(U)
 
 B = numpy.random.rand(numExamples, numExamples)
 B = B.dot(B.T)
 s, V = numpy.linalg.eig(B)
 V = V[:, 0:rank]
+V = numpy.abs(V)
 
 s = numpy.random.rand(rank)
 
