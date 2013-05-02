7eb5742ef5124404bef68eef4edd027d65659c7c
Charanpal Dhanjal <charanpal@gmail.com>
2013-05-02 21:02:03 +0200

    Use iterators for learnModel and predict

--- exp/sandbox/recommendation/IterativeSoftImpute.py
+++ exp/sandbox/recommendation/IterativeSoftImpute.py
@@ -44,109 +44,124 @@ class IterativeSoftImpute(AbstractMatrixCompleter):
         self.q = 2
         self.logStep = logStep
         
-    def learnModel(self, matrixIterator):
+    def learnModel(self, XIterator):
         """
-        Learn the matrix completion using a sparse matrix X. This is the simple 
-        version of the soft impute algorithm in which we store the entire 
-        matrices, newZ and oldZ. 
+        Learn the matrix completion using an iterator which outputs 
+        a sequence of sparse matrices X. The output of this method is also 
+        an iterator which outputs a sequence of completed matrices. 
         """
-        tol = 10**-6    
-        ZList = []
-        j = 0 
-        
-        for X in matrixIterator: 
-            (n, m) = X.shape
-            
-            if j == 0: 
-                oldU = numpy.zeros((n, 1))
-                oldS = numpy.zeros(1)
-                oldV = numpy.zeros((m, 1))   
-            else: 
-                oldN = oldU.shape[0]
-                oldM = oldV.shape[0]
-                
-                if self.updateAlg == "initial":                 
-                    if n > oldN: 
-                        oldU = Util.extendArray(oldU, (n, oldU.shape[1]))
-                    elif n < oldN: 
-                        oldU = oldU[0:n, :]
-                        
-                    if m > oldM: 
-                        oldV = Util.extendArray(oldV, (m, oldV.shape[1]))
-                    elif m < oldN: 
-                        oldV = oldV[0:m, :]
-                elif self.updateAlg == "svdUpdate":
-                    pass 
-                elif self.updateAlg == "zero": 
-                    oldU = numpy.zeros((n, 1))
-                    oldS = numpy.zeros(1)
-                    oldV = numpy.zeros((m, 1))   
-                else: 
-                    raise ValueError("Unknown SVD update algorithm: " + self.updateAlg)
-            
-            omega = X.nonzero()
-            rowInds = numpy.array(omega[0], numpy.int)
-            colInds = numpy.array(omega[1], numpy.int)
-             
-            gamma = self.eps + 1
-            i = 0
-            
-            while gamma > self.eps:
-                ZOmega = SparseUtilsCython.partialReconstruct2((rowInds, colInds), oldU, oldS, oldV)
-                Y = X - ZOmega
-                Y = Y.tocsc()
-                
-                if self.svdAlg=="propack": 
-                    newU, newS, newV = ExpSU.SparseUtils.svdSparseLowRank(Y, oldU, oldS, oldV, self.k)
-                elif self.svdAlg=="svdUpdate": 
-                    newU, newS, newV = SVDUpdate.addSparseProjected(oldU, oldS, oldV, Y, self.k)
-                elif self.svdAlg=="RSVD": 
-                    newU, newS, newV = SVDUpdate.addSparseRSVD(oldU, oldS, oldV, Y, self.k, self.k, kRand=self.r, q=self.q)
-                else: 
-                    raise ValueError("Unknown SVD algorithm: " + self.svdAlg)
-        
-                #Soft threshold 
-                newS = newS - self.lmbda
-                newS = numpy.clip(newS, 0, numpy.max(newS))
-                
-                normOldZ = (oldS**2).sum()
-                normNewZmOldZ = (oldS**2).sum() + (newS**2).sum() - 2*numpy.trace((oldV.T.dot(newV*newS)).dot(newU.T.dot(oldU*oldS)))
-                
-                #We can get newZ == oldZ in which case we break
-                if normNewZmOldZ < tol: 
-                    gamma = 0
-                elif abs(normOldZ) < tol:
-                    gamma = self.eps + 1 
-                else: 
-                    gamma = normNewZmOldZ/normOldZ
-                
-                oldU = newU.copy() 
-                oldS = newS.copy() 
-                oldV = newV.copy() 
-                
-                logging.debug("Iteration " + str(i) + " gamma="+str(gamma)) 
-                i += 1 
-                
-            logging.debug("Number of iterations for lambda="+str(self.lmbda) + ": " + str(i))
-            
-            ZList.append((newU,newS,newV))
-            j += 1 
-            
-        return ZList
 
-    def predict(self, ZList, indList): 
+        class ZIterator(object): 
+            def __init__(self, XIterator, iterativeSoftImpute): 
+                self.tol = 10**-6    
+                self.j = 0 
+                self.XIterator = XIterator
+                self.iterativeSoftImpute = iterativeSoftImpute
+
+            def __iter__(self): 
+                return self 
+                
+            def next(self): 
+                X = self.XIterator.next() 
+                (n, m) = X.shape
+                
+                if self.j == 0: 
+                    self.oldU = numpy.zeros((n, 1))
+                    self.oldS = numpy.zeros(1)
+                    self.oldV = numpy.zeros((m, 1))   
+                else: 
+                    oldN = self.oldU.shape[0]
+                    oldM = self.oldV.shape[0]
+                    
+                    if self.iterativeSoftImpute.updateAlg == "initial":                 
+                        if n > oldN: 
+                            self.oldU = Util.extendArray(self.oldU, (n, self.oldU.shape[1]))
+                        elif n < oldN: 
+                            self.oldU = self.oldU[0:n, :]
+                            
+                        if m > oldM: 
+                            self.oldV = Util.extendArray(self.oldV, (m, self.oldV.shape[1]))
+                        elif m < oldN: 
+                            self.oldV = self.oldV[0:m, :]
+                    elif self.iterativeSoftImpute.updateAlg == "svdUpdate":
+                        pass 
+                    elif self.iterativeSoftImpute.updateAlg == "zero": 
+                        self.oldU = numpy.zeros((n, 1))
+                        self.oldS = numpy.zeros(1)
+                        self.oldV = numpy.zeros((m, 1))   
+                    else: 
+                        raise ValueError("Unknown SVD update algorithm: " + self.updateAlg)
+                
+                omega = X.nonzero()
+                rowInds = numpy.array(omega[0], numpy.int)
+                colInds = numpy.array(omega[1], numpy.int)
+                 
+                gamma = self.iterativeSoftImpute.eps + 1
+                i = 0
+                
+                while gamma > self.iterativeSoftImpute.eps:
+                    ZOmega = SparseUtilsCython.partialReconstruct2((rowInds, colInds), self.oldU, self.oldS, self.oldV)
+                    Y = X - ZOmega
+                    Y = Y.tocsc()
+                    
+                    if self.iterativeSoftImpute.svdAlg=="propack": 
+                        newU, newS, newV = ExpSU.SparseUtils.svdSparseLowRank(Y, self.oldU, self.oldS, self.oldV, self.iterativeSoftImpute.k)
+                    elif self.iterativeSoftImpute.svdAlg=="svdUpdate": 
+                        newU, newS, newV = SVDUpdate.addSparseProjected(self.oldU, self.oldS, self.oldV, Y, self.iterativeSoftImpute.k)
+                    elif self.iterativeSoftImpute.svdAlg=="RSVD": 
+                        newU, newS, newV = SVDUpdate.addSparseRSVD(self.oldU, self.oldS, self.oldV, Y, self.iterativeSoftImpute.k, self.iterativeSoftImpute.k, kRand=self.iterativeSoftImpute.r, q=self.iterativeSoftImpute.q)
+                    else: 
+                        raise ValueError("Unknown SVD algorithm: " + self.iterativeSoftImpute.svdAlg)
+            
+                    #Soft threshold 
+                    newS = newS - self.iterativeSoftImpute.lmbda
+                    newS = numpy.clip(newS, 0, numpy.max(newS))
+                    
+                    normOldZ = (self.oldS**2).sum()
+                    normNewZmOldZ = (self.oldS**2).sum() + (newS**2).sum() - 2*numpy.trace((self.oldV.T.dot(newV*newS)).dot(newU.T.dot(self.oldU*self.oldS)))
+                    
+                    #We can get newZ == oldZ in which case we break
+                    if normNewZmOldZ < self.tol: 
+                        gamma = 0
+                    elif abs(normOldZ) < self.tol:
+                        gamma = self.iterativeSoftImpute.eps + 1 
+                    else: 
+                        gamma = normNewZmOldZ/normOldZ
+                    
+                    self.oldU = newU.copy() 
+                    self.oldS = newS.copy() 
+                    self.oldV = newV.copy() 
+                    
+                    logging.debug("Iteration " + str(i) + " gamma="+str(gamma)) 
+                    i += 1 
+                    
+                logging.debug("Number of iterations for lambda="+str(self.iterativeSoftImpute.lmbda) + ": " + str(i))
+                
+                self.j += 1
+                return (newU,newS,newV)
+                 
+        return ZIterator(XIterator, self)
+
+    def predict(self, ZIter, indList): 
         """
-        Make a set of predictions for a given set of completed matrices and 
-        index lists. 
-        """ 
-        XhatList = []
-        
-        for i, Z in enumerate(ZList):
-            U, s, V = Z 
-            Xhat = ExpSU.SparseUtils.reconstructLowRank(U, s, V, indList[i])
-            XhatList.append(Xhat)    
-        
-        return XhatList
+        Make a set of predictions for a given iterator of completed matrices and 
+        an index list. 
+        """         
+        class ZTestIter(object):         
+            def __init__(self): 
+                self.i = 0 
+            
+            def __iter__(self): 
+                return self 
+                
+            def next(self): 
+                U, s, V = ZIter.next()
+                Xhat = ExpSU.SparseUtils.reconstructLowRank(U, s, V, indList[self.i])
+                self.i += 1 
+                
+                return Xhat 
+  
+        return ZTestIter()
 
     def setK(self, k):
         Parameter.checkInt(k, 1, float('inf'))
