f6ce5e07458e633b6fc1722390bfdc3cade33322
Charanpal Dhanjal <charanpal@gmail.com>
2013-05-03 13:48:13 +0200

    Fix to get running experiment

--- exp/recommendexp/RecommendExpHelper.py
+++ exp/recommendexp/RecommendExpHelper.py
@@ -10,23 +10,26 @@ from apgl.util.PathDefaults import PathDefaults
 from apgl.util import Util
 from apgl.util.MCEvaluator import MCEvaluator 
 from exp.sandbox.recommendation.IterativeSoftImpute import IterativeSoftImpute 
+from exp.util.SparseUtils import SparseUtils 
 
 class RecommendExpHelper(object):
+    defaultAlgoArgs = argparse.Namespace()
+    defaultAlgoArgs.runSoftImpute = False
+    defaultAlgoArgs.lmbdas = [0.1, 0.2, 0.5, 1.0]        
+    
     def __init__(self, trainXIteratorFunc, testXIteratorFunc, cmdLine=None, defaultAlgoArgs = None, dirName=""):
         """ priority for default args
          - best priority: command-line value
          - middle priority: set-by-function value
          - lower priority: class value
         """
-        self.defaultAlgoArgs = argparse.Namespace()
-        self.defaultAlgoArgs.runSoftImpute = False
-        self.defaultAlgoArgs.lmbdas = [0.1, 0.2, 0.5, 1.0]        
+    
         
         # Parameters to choose which methods to run
         # Obtained merging default parameters from the class with those from the user
         self.algoArgs = RecommendExpHelper.newAlgoParams(defaultAlgoArgs)
         
-        # Variables related to the dataset
+        #Function to return iterators to the training and test matrices  
         self.trainXIteratorFunc = trainXIteratorFunc
         self.testXIteratorFunc = testXIteratorFunc
         
@@ -63,7 +66,7 @@ class RecommendExpHelper(object):
         algoParser = argparse.ArgumentParser(description="", add_help=add_help)
         for method in ["runSoftImpute"]:
             algoParser.add_argument("--" + method, action="store_true", default=defaultAlgoArgs.__getattribute__(method))
-        algoParser.add_argument("--lmbdas", type=float, help="Regularisation parameter (default: %(default)s)", default=defaultAlgoArgs.k1)
+        algoParser.add_argument("--lmbdas", type=float, help="Regularisation parameter (default: %(default)s)", default=defaultAlgoArgs.lmbdas)
 
         return(algoParser)
     
@@ -92,17 +95,18 @@ class RecommendExpHelper(object):
         """
         Save results for a particular recommendation 
         """
-        trainIterator = self.trainXIterator()
-        testIterator = self.testXIterator()
+        testIterator = self.testXIteratorFunc()
         measures = []
-        logging.debug("Computing cluster measures")
+        logging.debug("Computing recommendation errors")
 
-        for predX in ZIter:
+        for Z in ZIter:
             #Util.printIteration(i, self.logStep, len(clusterList))
             testX = next(testIterator)
-
-            #Store modularity, k-way normalised cut, and cluster size 
+            
+            U, s, V = Z 
+            predX = SparseUtils.reconstructLowRank(U, s, V, testX.nonzero())
             currentMeasures = [MCEvaluator.meanSqError(testX, predX)] 
+            print(currentMeasures)
             measures.append(currentMeasures) 
 
         measures = numpy.array(measures)
@@ -116,12 +120,12 @@ class RecommendExpHelper(object):
         """
         if self.algoArgs.runSoftImpute:
             logging.debug("Running exact method")
-            learner = IterativeSoftImpute(self.algoArgs.lmbdas, svdAlg="propack", logStep=self.logStep)
-            trainIterator = self.trainXIterator()
+            learner = IterativeSoftImpute(self.algoArgs.lmbdas[0], svdAlg="propack", logStep=self.logStep)
+            trainIterator = self.trainXIteratorFunc()
             ZIter = learner.learnModel(trainIterator)
             
-            for lmbda in self.algoArgs.lmbdas: 
-                resultsFileName = self.resultsDir + "ResultsSoftImpute_lmbda=" + str(lmbda) + ".npz"
-                self.recordResults(ZIter, resultsFileName)
+
+            resultsFileName = self.resultsDir + "ResultsSoftImpute_lmbda=" + str(self.algoArgs.lmbdas[0]) + ".npz"
+            self.recordResults(ZIter, resultsFileName)
 
         logging.info("All done: see you around!")
