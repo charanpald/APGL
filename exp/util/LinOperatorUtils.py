f46ffd93a2302dfa9b2ef1338a3abebf4b9993c8
Charanpal Dhanjal <charanpal@gmail.com>
2013-05-07 21:27:04 +0200

    Parallel processing for sparse matrix operations and sparse + low rank.

--- exp/util/LinOperatorUtils.py
+++ exp/util/LinOperatorUtils.py
@@ -1,71 +1,110 @@
 import numpy 
 import multiprocessing 
-import scipy.sparse.linalg.LinearOperator
-
+import itertools
+import scipy.sparse.linalg
 
 def dot(args): 
-    X, v = args 
-    return X.dot(v)
+    X, w = args 
+    return X.dot(w)
 
 def dotT(args): 
-    X, v = args 
-    return X.T.dot(v)
+    X, w = args 
+    return X.T.dot(w)
+
+def dotSVD(args): 
+    X, U, s, V, w = args 
+    return X.dot(w) + (U*s).dot(V.T.dot(w)) 
+
+def dotSVDT(args): 
+    X, U, s, V, w = args 
+    return X.T.dot(w) + (V*s).dot(U.T.dot(w))
 
 class LinOperatorUtils(object): 
     """
     Some implementations of linear operators for various structured matrices. 
     """    
-        
     def __init__(self): 
         pass 
     
     @staticmethod 
-    def sparseMatrixOp(X): 
-        def matvec(v): 
-            return X.dot(v)
+    def sparseLowRankOp(X, U, s, V): 
+        def matvec(w): 
+            return X.dot(w) + (U*s).dot(V.T.dot(w)) 
         
-        def rmatvec(v): 
-            return X.T.dot(v)          
-            
-        L = scipy.sparse.linalg.LinearOperator(X.shape, matvec, rmatvec) 
-        return L
+        def rmatvec(w): 
+            return X.T.dot(w) + (V*s).dot(U.T.dot(w))
         
-    def parallelSparseMatrixOp(X): 
-        def matvec(v): 
-            numProcesses = multiprocessing.cpu_count()
-            pool = multiprocessing.Pool(processes=numProcesses) 
+        return scipy.sparse.linalg.LinearOperator(X.shape, matvec, rmatvec, dtype=X.dtype) 
+    
+    @staticmethod 
+    def parallelSparseLowRankOp(X, U, s, V): 
+        numProcesses = multiprocessing.cpu_count()
+        pool = multiprocessing.Pool(processes=numProcesses) 
+        rowInds = numpy.array(numpy.linspace(0, X.shape[0], numProcesses+1), numpy.int) 
+        colInds = numpy.array(numpy.linspace(0, X.shape[1], numProcesses+1), numpy.int)
+        
+        def matvec(w): 
             paramList = [] 
-            
-            inds = numpy.array(numpy.linspace(0, X.shape[0], numProcesses+1))
             for i in range(numProcesses): 
-                paramList.append((X[inds[i]:inds[i+1], :], v))
+                paramList.append((X[rowInds[i]:rowInds[i+1], :], U[rowInds[i]:rowInds[i+1], :], s, V, w))
+                
+            iterator = pool.imap(dotSVD, paramList)
+            #iterator = itertools.imap(dotSVD, paramList)
+            p = numpy.zeros(X.shape[0])
+            
+            for i in range(numProcesses): 
+                p[rowInds[i]:rowInds[i+1]] = iterator.next()
+            
+            return p
+        
+        def rmatvec(w):
+            paramList = [] 
+            for i in range(numProcesses): 
+                paramList.append((X[:, colInds[i]:colInds[i+1]], U, s, V[colInds[i]:colInds[i+1], :], w))
+                
+            iterator = pool.imap(dotSVDT, paramList)
+            p = numpy.zeros(X.shape[1])
+            
+            for i in range(numProcesses): 
+                p[colInds[i]:colInds[i+1]] = iterator.next()
+            
+            return p      
+            
+        return scipy.sparse.linalg.LinearOperator(X.shape, matvec, rmatvec, dtype=X.dtype)   
+        
+        
+    @staticmethod 
+    def parallelSparseOp(X):
+        numProcesses = multiprocessing.cpu_count()
+        pool = multiprocessing.Pool(processes=numProcesses) 
+        rowInds = numpy.array(numpy.linspace(0, X.shape[0], numProcesses+1), numpy.int) 
+        colInds = numpy.array(numpy.linspace(0, X.shape[1], numProcesses+1), numpy.int)
+        
+        def matvec(w): 
+            paramList = [] 
+            for i in range(numProcesses): 
+                paramList.append((X[rowInds[i]:rowInds[i+1], :], w))
                 
             iterator = pool.imap(dot, paramList)
-            u = numpy.zeros(X.shape[0])
+            p = numpy.zeros(X.shape[0])
             
             for i in range(numProcesses): 
-                u[inds[i]:inds[i+1]] = iterator.next()
+                p[rowInds[i]:rowInds[i+1]] = iterator.next()
             
-            return u
+            return p
         
-        def rmatvec(v): 
-            numProcesses = multiprocessing.cpu_count()
-            pool = multiprocessing.Pool(processes=numProcesses) 
+        def rmatvec(w): 
             paramList = [] 
-            
-            inds = numpy.array(numpy.linspace(0, X.shape[0], numProcesses+1))
             for i in range(numProcesses): 
-                paramList.append((X[:, inds[i]:inds[i+1]], v))
+                paramList.append((X[:, colInds[i]:colInds[i+1]], w))
                 
             iterator = pool.imap(dotT, paramList)
-            u = numpy.zeros(X.shape[1])
+            p = numpy.zeros(X.shape[1])
             
             for i in range(numProcesses): 
-                u[inds[i]:inds[i+1]] = iterator.next()
+                p[colInds[i]:colInds[i+1]] = iterator.next()
             
-            return u      
+            return p      
             
-        L = scipy.sparse.linalg.LinearOperator(X.shape, matvec, rmatvec) 
-        return L    
-    
+        return scipy.sparse.linalg.LinearOperator(X.shape, matvec, rmatvec, dtype=X.dtype)   
     
