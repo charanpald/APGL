3d867ca271235345fc636ba6f3321cc20e2cc584
Charanpal Dhanjal <charanpal@gmail.com>
2013-05-14 14:06:03 +0200

    Profile ARPACK

--- exp/util/profile/SparseUtilsProfile.py
+++ exp/util/profile/SparseUtilsProfile.py
@@ -29,6 +29,24 @@ class SparseUtilsProfile(object):
         
         #Memory consumption is dependent on kmax
         print("All done")
+
+    def profileArpackSvd(self):
+        dataDir = PathDefaults.getDataDir() + "erasm/contacts/" 
+        trainFilename = dataDir + "contacts_train"        
+        
+        trainX = scipy.io.mmread(trainFilename)
+        trainX = scipy.sparse.csc_matrix(trainX, dtype=numpy.float32)
+        print(trainX.dtype.char, trainX.dtype)
+        
+        
+        k = 500 
+        U, s, V = SparseUtils.svdArpack(trainX, k, kmax=k*5)
+        
+        print(s)
+        
+        #Memory consumption is dependent on kmax and less than PROPACK 
+        print("All done")
         
 profiler = SparseUtilsProfile()
-profiler.profilePropackSvd()
+#profiler.profilePropackSvd()
+profiler.profileArpackSvd()
