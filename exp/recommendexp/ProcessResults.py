007e494c9deaf3f4989a3746dee0d4166efd756f
Charanpal Dhanjal <charanpal@gmail.com>
2013-05-22 20:47:00 +0100

    Plot errors

--- exp/recommendexp/ProcessResults.py
+++ exp/recommendexp/ProcessResults.py
@@ -1,20 +1,52 @@
 
+import logging 
+import sys 
 import numpy 
 import matplotlib.pyplot as plt 
 from apgl.util.PathDefaults import PathDefaults 
 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 #For now just print some results for a particular dataset 
 #dataset = "MovieLensDataset"
-dataset = "NetflixDataset"
+#dataset = "NetflixDataset"
+dataset = "SyntheticDataset1"
 outputDir = PathDefaults.getOutputDir() + "recommend/" + dataset + "/"
-ks = numpy.array([5, 10, 15, 20, 50, 100, 200, 300])
+ks = numpy.array(2**numpy.arange(3, 8.5, 0.5), numpy.int)
+logging.debug(ks)
 
-for k in ks: 
-    data = numpy.load(outputDir + "ResultsSoftImpute_k=" + str(k) + ".npz")
+plotStyles = ['k-', 'k--', 'k-.', 'r--', 'r-', 'g-', 'b-', 'b--', 'b-.', 'g--', 'g--', 'g-.', 'r-', 'r--', 'r-.']
+i = 0
+
+try: 
+    fileName = outputDir + "ResultsSoftImpute.npz"
+    data = numpy.load(fileName)
     measures = data["arr_0"]
-    print(measures)
-    #plt.plot(numpy.arange(measures.shape[0]), measures[:, 1], label="k="+str(k))
+    metadata = data["arr_1"]
     
-#plt.show()
-#plt.legend() 
+    print(metadata[0,1])
+    
+    plt.figure(0)
+    plt.plot(numpy.arange(measures.shape[0]), measures[:, 1], plotStyles[i],)
+    plt.ylabel("RMSE Test")
+    plt.legend(loc="lower left") 
+    
+    plt.figure(1)
+    plt.plot(numpy.arange(measures.shape[0]), measures[:, 2], plotStyles[i])
+    plt.ylabel("MAE Test")
+    plt.legend(loc="lower left") 
+    
+    plt.figure(2)
+    plt.plot(numpy.arange(measures.shape[0]), measures[:, 0], plotStyles[i])
+    plt.legend() 
+    plt.ylabel("RMSE Train")
+    
+    plt.figure(3)
+    plt.plot(numpy.arange(metadata.shape[0]), metadata[:, 0], plotStyles[i])
+    plt.legend() 
+    plt.ylabel("Rank")
+except: 
+    logging.debug("Missing results : " + str(fileName))
+   
+
+plt.show()
