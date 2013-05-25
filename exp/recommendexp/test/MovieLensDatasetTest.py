5a181b73162e94d53f396cc79a62c3ec62309890
Charanpal Dhanjal <charanpal@gmail.com>
2013-05-25 18:58:52 +0100

    Test iterator generates new matrices

--- exp/recommendexp/test/MovieLensDatasetTest.py
+++ exp/recommendexp/test/MovieLensDatasetTest.py
@@ -3,6 +3,7 @@ import unittest
 import pickle 
 import logging 
 import time 
+import numpy.testing as nptst 
 from datetime import datetime 
 from exp.recommendexp.MovieLensDataset import MovieLensDataset 
 
@@ -11,7 +12,7 @@ class  MovieLensDatasetTest(unittest.TestCase):
     def setUp(self): 
         pass 
 
-    #@unittest.skip("")
+    @unittest.skip("")
     def testIteratorFunc(self):
         iterStartTimeStamp = time.mktime(datetime(2009,01,25).timetuple())
         dataset = MovieLensDataset(iterStartTimeStamp=iterStartTimeStamp)
@@ -30,7 +31,7 @@ class  MovieLensDatasetTest(unittest.TestCase):
         except StopIteration: 
             pass 
       
-    #@unittest.skip("")
+    @unittest.skip("")
     def testIteratorFunc2(self):
         dataset = MovieLensDataset()
 
@@ -42,6 +43,22 @@ class  MovieLensDatasetTest(unittest.TestCase):
             
             print(trainX.shape, trainX.nnz, testX.nnz)
             self.assertEquals(trainX.shape, testX.shape)
+
+    def testIteratorFunc2(self):
+        #Check to see if repeated calls generate new matrices 
+        iterStartTimeStamp = time.mktime(datetime(2005,1,1).timetuple())
+        dataset = MovieLensDataset(iterStartTimeStamp=iterStartTimeStamp)
+        trainIterator = dataset.getTrainIteratorFunc()   
+        
+        X = next(trainIterator)
+        X.data += 1 
+        
+        trainIterator = dataset.getTrainIteratorFunc()  
+        X2 = next(trainIterator)
+        
+        nptst.assert_array_almost_equal(X.data, X2.data+1)
+        
+        
         
     #@unittest.skip("")    
     def testData(self): 
