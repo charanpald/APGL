99bee810d13a894d26dd273e6dca17d9f9b5d436
Charanpal Dhanjal <charanpal@gmail.com>
2013-06-06 10:41:20 +0200

    Bug fix and speedup

--- exp/util/SparseUtilsCython.cpp
+++ exp/util/SparseUtilsCython.cpp
@@ -6,14 +6,16 @@ void partialReconstructValsPQCpp(int* rowInds, int*colInds, double* P, double* Q
 
     */
     int i, j; 
-    int sum; 
+    int p, q; 
+    double sum; 
 
     for(i=0;i<size;i++) {
         sum = 0; 
-
+        p = rowInds[i]*numCols; 
+        q = colInds[i]*numCols;
         for(j=0;j<numCols;j++) { 
             //P[rowInds[i], :]^T Q[colInds[i], :]
-            sum += P[rowInds[i]*numCols + j]*P[colInds[i]*numCols + j];
+            sum += P[p + j]*Q[q + j];
             }
  
         values[i] = sum;         
