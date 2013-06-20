6e595a11b6de3196c0f0a2156e8d80bf3a3b0eed
Charanpal Dhanjal <charanpal@gmail.com>
2013-06-20 20:23:45 +0200

    More testing

--- exp/recommendexp/test/FlixsterDatasetTest.py
+++ exp/recommendexp/test/FlixsterDatasetTest.py
@@ -12,6 +12,7 @@ class FlixsterDatasetTest(unittest.TestCase):
     def setUp(self): 
         pass 
     
+    @unittest.skip("")
     def testIteratorFunc2(self):
         dataset = FlixsterDataset()
 
@@ -23,6 +24,49 @@ class FlixsterDatasetTest(unittest.TestCase):
             
             print(trainX.shape, trainX.nnz, testX.nnz)
             self.assertEquals(trainX.shape, testX.shape)
+
+    @unittest.skip("")
+    def testIteratorFunc3(self):
+        #Check to see if repeated calls generate new matrices 
+        iterStartTimeStamp = time.mktime(datetime(2005,1,1).timetuple())
+        dataset = FlixsterDataset(iterStartTimeStamp=iterStartTimeStamp)
+        trainIterator = dataset.getTrainIteratorFunc()   
+        
+        X = next(trainIterator)
+        X.data += 1 
+        
+        trainIterator = dataset.getTrainIteratorFunc()  
+        X2 = next(trainIterator)
+        
+        nptst.assert_array_almost_equal(X.data, X2.data+1)
+
+    def testData(self): 
+        dataset = FlixsterDataset()
+        custIdDict = pickle.load(open(dataset.custDictFileName))
+        movieIdDict = pickle.load(open(dataset.itemDictFileName))                 
+        dataArr = numpy.load(dataset.ratingFileName)
+        movieInds, custInds, ratings, dates = dataArr["arr_0"], dataArr["arr_1"], dataArr["arr_2"], dataArr["arr_3"]
+        
+        #Check a few entries of the read data 
+        self.assertEquals(custInds[0], 0)
+        self.assertEquals(custInds[1], 0)
+        self.assertEquals(custInds[71], 0)
+        self.assertEquals(custInds[72], 1)
+        
+        self.assertEquals(movieIdDict[81], 0)
+        self.assertEquals(movieIdDict[926], 1)
+        self.assertEquals(movieIdDict[5124], 10)
+        
+        self.assertEquals(ratings[0], 1.5)
+        self.assertEquals(ratings[9], 0.5)
+        self.assertEquals(ratings[10], 1)
+        self.assertEquals(ratings[250], 0.5)
+        self.assertEquals(ratings[550], 4.5)
+        
+        self.assertEquals(time.strftime("%D", time.localtime(dates[0])), "10/10/07")
+        self.assertEquals(time.strftime("%D", time.localtime(dates[4])), "12/29/07")
+        self.assertEquals(time.strftime("%D", time.localtime(dates[5])), "11/13/07")
+        self.assertEquals(time.strftime("%D", time.localtime(dates[7])), "10/10/07")
 
 if __name__ == '__main__':
     unittest.main()
