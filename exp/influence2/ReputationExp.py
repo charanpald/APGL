aa4617be36df7730eb6c65d9f267e0ab468d9997
Charanpal Dhanjal <charanpal@gmail.com>
2013-06-27 17:30:18 +0200

    Updates

--- exp/influence2/ReputationExp.py
+++ exp/influence2/ReputationExp.py
@@ -1,23 +1,34 @@
-
-import ctypes;
+import numpy 
+import ctypes
 try:  
     ctypes.cdll.LoadLibrary("/usr/local/lib/libigraph.so")
 except: 
     pass 
 import igraph 
+from exp.influence2.MaxInfluence import MaxInfluence 
 
 
 #Generate random graph and then rankings 
-n = 1000 
+n = 100 
 m = 5 
 
+k = 100 
 
 graph = igraph.Graph.Barabasi(n, m, directed=True)
+graph.es["p"] = numpy.random.rand(graph.ecount())
 print(graph.summary())
 
 
-scores1 = graph.eigenvector_centrality(directed=True)
-scores2 = graph.betweenness()
-scores3 = graph.pagerank()
+rank1 = MaxInfluence.greedyMethod(graph, k, 5)
 
-print(scores2)
+#scores1 = graph.eigenvector_centrality(directed=True)
+scores = graph.betweenness()
+rank2 = numpy.flipud(numpy.argsort(scores)) 
+
+scores = graph.pagerank()
+rank3 = numpy.flipud(numpy.argsort(scores)) 
+
+
+print(rank1)
+print(rank2)
+print(rank3)
