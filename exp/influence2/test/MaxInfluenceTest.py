ab8272d4fac036c94ad123b7a8a7e5deed1bd7f1
Charanpal Dhanjal <charanpal@gmail.com>
2013-07-01 21:29:36 +0200

    Test on deterministic graphs

--- exp/influence2/test/MaxInfluenceTest.py
+++ exp/influence2/test/MaxInfluenceTest.py
@@ -13,6 +13,7 @@ from exp.influence2.MaxInfluence import MaxInfluence
 class  MaxInfluenceTest(unittest.TestCase):
     def setUp(self): 
         numpy.random.seed(22) 
+        numpy.set_printoptions(suppress=True, precision=3)
     
     def testGreedyMethod(self): 
         graph = igraph.Graph(directed=True)        
@@ -27,8 +28,9 @@ class  MaxInfluenceTest(unittest.TestCase):
         self.assertEquals(set(influenceList), set([0,1,2]))
     
     def testSimulateCascade(self): 
-        graph = igraph.Graph(directed=True)        
-        graph.add_vertices(5)
+        graph = igraph.Graph(directed=True)
+        numVertices = 5        
+        graph.add_vertices(numVertices)
         
         
         graph.add_edges([(0,1), (0,2), (1, 3), (2,3), (2,4)])
@@ -45,8 +47,40 @@ class  MaxInfluenceTest(unittest.TestCase):
         graph.es["p"] = numpy.array([0, 0, 0, 0, 0])
         outputInds = MaxInfluence.simulateCascade(graph, activeVertexInds)
         self.assertEquals(outputInds, set([0])) 
+    
+    def testSimulateCascades(self): 
+        #Test submodularity 
+        n = 20 
+        p = 0.1
+        graph = igraph.Graph.Erdos_Renyi(n, p)
+        graph.es["p"] = numpy.array(numpy.random.rand(graph.ecount()) < 0.5, numpy.bool)
         
-    def testGreedyMethod(self): 
+        numRuns = 1
+        influences = numpy.zeros(n)
+        
+        activeVertexInds = set([0])  
+        lastInfluence = MaxInfluence.simulateCascades(graph, activeVertexInds, numRuns)
+        
+        for i in range(n): 
+            activeVertexInds = set([0, i])  
+            influences[i] = MaxInfluence.simulateCascades(graph, activeVertexInds, numRuns)
+
+        marginalGains = (influences - lastInfluence)
+        
+        activeVertexInds = set([0, 1]) 
+        lastInfluence = MaxInfluence.simulateCascades(graph, activeVertexInds, numRuns)
+        
+        for i in range(n): 
+            activeVertexInds = set([0, 1, i])  
+            influences[i] = MaxInfluence.simulateCascades(graph, activeVertexInds, numRuns)
+            
+
+        marginalGains2 = (influences - lastInfluence)
+        
+        self.assertTrue((marginalGains2 <= marginalGains).all())
+        
+    #@unittest.skip("")
+    def testCelf(self): 
         graph = igraph.Graph(directed=True)        
         graph.add_vertices(5)
         graph.add_edges([(0,1), (0,2), (1, 3), (2,3), (2,4)])
@@ -66,21 +100,21 @@ class  MaxInfluenceTest(unittest.TestCase):
         influenceList = MaxInfluence.celf(graph, k)        
         self.assertEquals(influenceList, [5, 0, 1]) 
         
-        
         #Now try some random graphs 
-        print("Starting random test")
-        numReps = 10         
+        numReps = 20      
+        numRuns = 1
         
         for i in range(numReps): 
-            n = 10 
-            p = 0.1
+            print(i)
+            n = numpy.random.randint(10, 50) 
+            p = numpy.random.rand()
             graph = igraph.Graph.Erdos_Renyi(n, p)
-            graph.es["p"] = numpy.random.rand(graph.ecount())
+            graph.es["p"] = numpy.array(numpy.random.rand(graph.ecount()) < 0.5, numpy.bool)
             
-            k = 4
-            influenceList = MaxInfluence.celf(graph, k, 10000)  
+            k = numpy.random.randint(5, 10)
+            influenceList = MaxInfluence.celf(graph, k, numRuns)  
             
-            influenceList2 = MaxInfluence.greedyMethod(graph, k, 10000)  
+            influenceList2 = MaxInfluence.greedyMethod(graph, k, numRuns)  
             self.assertEquals(influenceList, influenceList2)
 
 if __name__ == '__main__':
