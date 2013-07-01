d69dbb31f3fa148890df960a8a89515378258d76
Charanpal Dhanjal <charanpal@gmail.com>
2013-07-01 17:33:02 +0200

    Continued solution to celf

--- exp/influence2/MaxInfluence.py
+++ exp/influence2/MaxInfluence.py
@@ -1,4 +1,6 @@
 import numpy 
+import heapq 
+import logging
 
 class MaxInfluence(object): 
     def __init__(self):
@@ -11,19 +13,23 @@ class MaxInfluence(object):
         """
         influenceSet = set([])
         influenceList = []        
-        unvisited = set(range(graph.vcount()))        
+        unvisited = set(range(graph.vcount()))  
+        currentInfluence = 0 
                 
         for i in range(k):
-            print(i)
+            logging.debug(i)
             infSums = numpy.zeros(graph.vcount()) 
             for vertexInd in unvisited: 
-                for j in range(numRuns): 
-                    infSums[vertexInd] += len(MaxInfluence.simulateCascade(graph, influenceSet.union([vertexInd]))) 
-                    
-            infSums /= numRuns    
-            influenceSet.add(numpy.argmax(infSums))
-            influenceList.append(numpy.argmax(infSums))
-            unvisited.remove(numpy.argmax(infSums))
+                infSums[vertexInd]  = MaxInfluence.simulateCascades(graph, influenceSet.union([vertexInd]), numRuns, 21)  
+            
+            print(i, infSums)
+            print(i, infSums - currentInfluence)                
+            
+            bestVertexInd = numpy.argmax(infSums)
+            currentInfluence = infSums[bestVertexInd] 
+            influenceSet.add(bestVertexInd)
+            influenceList.append(bestVertexInd)
+            unvisited.remove(bestVertexInd)
     
         return influenceList
         
@@ -46,11 +52,57 @@ class MaxInfluence(object):
             
         return allActiveVertexInds 
             
-                        
+    @staticmethod 
+    def simulateCascades(graph, activeVertexInds, numRuns, seed=21): 
+        numpy.random.seed(seed)        
+        
+        currentInfluence = 0 
+        for j in range(numRuns): 
+            currentInfluence += len(MaxInfluence.simulateCascade(graph, activeVertexInds))
+        currentInfluence /= float(numRuns) 
+        
+        return currentInfluence 
 
-    def celf(self, graph ): 
+    @staticmethod 
+    def celf(graph, k, numRuns=10): 
         """
         Maximising the influence using the CELF algorithm of Leskovec et al. 
         """
-        pass 
+        influenceSet = set([])
+        influenceList = []                
+        negMarginalIncreases = []
+        
+        #For the initial values we compute marginal increases with respect to the empty set 
+        for vertexInd in range(graph.vcount()): 
+            currentInfluence = MaxInfluence.simulateCascades(graph, influenceSet.union([vertexInd]), numRuns, 21)         
+            #Note that we store the negation of the influence since heappop chooses the smallest value 
+            heapq.heappush(negMarginalIncreases, (-currentInfluence, vertexInd))
+                     
+        negLastInfluence, bestVertexInd = heapq.heappop(negMarginalIncreases)
+        influenceSet.add(bestVertexInd)
+        influenceList.append(bestVertexInd)
+                
+        for i in range(1, k):
+            logging.debug(i)
+            valid = numpy.zeros(graph.vcount(), numpy.bool) 
+            negMarginalInfluence, currentBestVertexInd = heapq.heappop(negMarginalIncreases)    
+            
+            while not valid[currentBestVertexInd]: 
+                marginalInfluence = MaxInfluence.simulateCascades(graph, influenceSet.union([currentBestVertexInd]), numRuns, 21) 
+                print(i, currentBestVertexInd, marginalInfluence, negLastInfluence)
+                marginalInfluence += negLastInfluence 
+                
+                #Note that we store the negation of the influence since heappop chooses the smallest value 
+                heapq.heappush(negMarginalIncreases, (-marginalInfluence, currentBestVertexInd)) 
+                valid[currentBestVertexInd] = True
+                
+                negMarginalInfluence, currentBestVertexInd = heapq.heappop(negMarginalIncreases) 
+                totalInfluence = -(negMarginalInfluence + negLastInfluence)
+            
+            negLastInfluence = -totalInfluence 
+            
+            influenceSet.add(currentBestVertexInd)
+            influenceList.append(currentBestVertexInd)
+                
+        return influenceList
         
--- exp/influence2/test/MaxInfluenceTest.py
+++ exp/influence2/test/MaxInfluenceTest.py
@@ -14,7 +14,6 @@ class  MaxInfluenceTest(unittest.TestCase):
     def setUp(self): 
         numpy.random.seed(22) 
     
-    
     def testGreedyMethod(self): 
         graph = igraph.Graph(directed=True)        
         graph.add_vertices(5)
@@ -47,6 +46,42 @@ class  MaxInfluenceTest(unittest.TestCase):
         outputInds = MaxInfluence.simulateCascade(graph, activeVertexInds)
         self.assertEquals(outputInds, set([0])) 
         
+    def testGreedyMethod(self): 
+        graph = igraph.Graph(directed=True)        
+        graph.add_vertices(5)
+        graph.add_edges([(0,1), (0,2), (1, 3), (2,3), (2,4)])
+        graph.es["p"] = numpy.array([0, 1, 1, 0, 1])
+        
+        k = 3
+        influenceList = MaxInfluence.celf(graph, k)
+        self.assertEquals(set(influenceList), set([0,1,2]))
+
+        #2nd example 
+        graph = igraph.Graph(directed=True)        
+        graph.add_vertices(8)
+        graph.add_edges([(0,1), (0,2), (1, 3), (2,3), (2,4), (5,6), (6,7), (5, 4)])
+        graph.es["p"] = numpy.array([0, 1, 1, 0, 1, 1, 1, 1])
+        
+        k = 3
+        influenceList = MaxInfluence.celf(graph, k)        
+        self.assertEquals(influenceList, [5, 0, 1]) 
+        
+        
+        #Now try some random graphs 
+        print("Starting random test")
+        numReps = 10         
+        
+        for i in range(numReps): 
+            n = 10 
+            p = 0.1
+            graph = igraph.Graph.Erdos_Renyi(n, p)
+            graph.es["p"] = numpy.random.rand(graph.ecount())
+            
+            k = 4
+            influenceList = MaxInfluence.celf(graph, k, 10000)  
+            
+            influenceList2 = MaxInfluence.greedyMethod(graph, k, 10000)  
+            self.assertEquals(influenceList, influenceList2)
 
 if __name__ == '__main__':
     unittest.main()
