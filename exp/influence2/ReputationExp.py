885e76f881511977c38852c22c1552367d284664
Charanpal Dhanjal <charanpal@gmail.com>
2013-07-05 14:29:05 +0100

    Read domain names.

--- exp/influence2/ReputationExp.py
+++ exp/influence2/ReputationExp.py
@@ -11,13 +11,15 @@ import array
 
 #Read in graph 
 dataDir = PathDefaults.getDataDir() + "reputation/" 
-dataFileName = dataDir + "dataset_sample2.csv" 
+dataFileName = dataDir + "dataset_sample.csv" 
+domainFileName  = dataDir + "domains.csv" 
 
 dataFile = open(dataFileName)
 dataFile.readline() 
 
 authorInds = array.array("i")
 articleInds = array.array("i")
+domainInds = array.array("i")
 maxVals = 10000
 
 authorIdSet = set([])
@@ -28,10 +30,26 @@ articleIdSet = set([])
 articleIdDict = {}
 j = 0
 
+authorIdDomainDict = {}
+
+domainFile = open(domainFileName)
+domainFile.readline() 
+
+for line in domainFile: 
+    vals = line.split("\t")
+    print(vals)
+    
+    authorId = vals[0].strip()
+    domainInd = int(vals[1])
+    
+    authorIdDomainDict[authorId] = domainInd
+
+
 for i, line in enumerate(dataFile): 
     if i % 1000 == 0: 
         print(i)
     if i == maxVals: 
+        print("Max number of iterations reached")
         break 
     
     vals = line.split("\t")
@@ -55,16 +73,21 @@ for i, line in enumerate(dataFile):
     else: 
         articleInd = articleIdDict[articleId]    
     
+    domainInd = authorIdDomainDict.get(authorId, -1)
+    
     authorInds.append(authorInd)
     articleInds.append(articleInd)
+    domainInds.append(domainInd)
 
 authorInds = numpy.array(authorInds)
 articleInds = numpy.array(articleInds)
+domainInds = numpy.array(domainInds)
 edges = numpy.c_[authorInds, articleInds]
 
 print(numpy.max(authorInds), numpy.max(articleInds))
+print((domainInds!=-1).sum())
 
-
+#Coauthor graph is undirected 
 graph = igraph.Graph()
 graph.add_vertices(numpy.max(authorInds) + numpy.max(articleInds))
 graph.add_edges(edges)
@@ -87,3 +110,7 @@ print(rank3)
 
 
 print(len(graph.components()))
+
+compSizes = [len(x) for x in graph.components()]
+
+print(numpy.max(compSizes))
