f9947eae4562e0cc69a40f59858e14b937b000d5
Charanpal Dhanjal <charanpal@gmail.com>
2013-07-05 18:14:26 +0100

    IdIndexer

---
+++ exp/util/IdIndexer.py
@@ -0,0 +1,27 @@
+import array 
+import numpy 
+
+class IdIndexer(object): 
+    def __init__(self, arrayType="i"): 
+        self.inds = array.array(arrayType)
+        
+        self.idSet = set([])
+        self.idDict = {}
+        self.p = 0 
+        
+    def append(self, id): 
+        if id not in self.idSet: 
+            self.idSet.add(id)
+            self.idDict[id] = self.p
+            ind = self.p 
+            self.p += 1 
+        else: 
+            ind = self.idDict[id]   
+            
+        self.inds.append(ind)
+        
+    def getArray(self): 
+        return numpy.array(self.inds)
+        
+    def getIdDict(self): 
+        return self.idDict
