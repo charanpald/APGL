10fee9f10bb2d4c443f231601651767aa819e7af
Charanpal Dhanjal <charanpal@gmail.com>
2013-07-25 17:28:29 +0200

    Profile cascade simulation

--- exp/influence2/profile/MaxInfluenceProfile.py
+++ exp/influence2/profile/MaxInfluenceProfile.py
@@ -22,5 +22,18 @@ class MaxInfluenceProfile(object):
             
         ProfileUtils.profile('MaxInfluence.celf(graph, k, p=0.5)', globals(), locals())    
 
+    def profileSimulateCascades(self): 
+        n = 500 
+        p = 0.1
+        graph = igraph.Graph.Erdos_Renyi(n, p)
+            
+        k = 50
+        
+        activeVertices = set(numpy.random.randint(0, n, 10))  
+        numRuns = 1000
+        
+        ProfileUtils.profile('MaxInfluence.simulateCascades(graph, activeVertices, numRuns, p=0.5)', globals(), locals())  
+
 profiler = MaxInfluenceProfile()
-profiler.profileCelf()
+#profiler.profileCelf()
+profiler.profileSimulateCascades()
