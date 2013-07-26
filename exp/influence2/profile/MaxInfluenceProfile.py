c3a85d4aa96497279e2a0376e5b7821a1625bd53
Charanpal Dhanjal <charanpal@gmail.com>
2013-07-26 13:28:08 +0200

    Profile celf

--- exp/influence2/profile/MaxInfluenceProfile.py
+++ exp/influence2/profile/MaxInfluenceProfile.py
@@ -2,7 +2,8 @@ import os
 import numpy
 import logging
 import sys
-import igraph 
+import igraph
+import random  
 from apgl.util.ProfileUtils import ProfileUtils
 from exp.influence2.MaxInfluence import MaxInfluence 
 
@@ -11,16 +12,18 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 class MaxInfluenceProfile(object):
     def __init__(self):
         numpy.random.seed(21)        
-        
+        igraph._igraph.set_random_number_generator(random.WichmannHill(21))
         
     def profileCelf(self):
-        n = 500 
+         
+        n = 100 
         p = 0.1
         graph = igraph.Graph.Erdos_Renyi(n, p)
+        print(graph.summary())
             
         k = 50
-            
-        ProfileUtils.profile('MaxInfluence.celf(graph, k, p=0.5)', globals(), locals())    
+        numpy.random.seed(21) 
+        ProfileUtils.profile('MaxInfluence.celf(graph, k, p=0.5, numRuns=100)', globals(), locals())    
 
     def profileSimulateCascades(self): 
         n = 500 
@@ -30,7 +33,7 @@ class MaxInfluenceProfile(object):
         k = 50
         
         activeVertices = set(numpy.random.randint(0, n, 10))  
-        numRuns = 1000
+        numRuns = 100
         
         ProfileUtils.profile('MaxInfluence.simulateCascades(graph, activeVertices, numRuns, p=0.5)', globals(), locals())  
 
