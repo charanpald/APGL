a49e7e9189b90a0bd54e3120c94c521c49a6e2ab
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-06 13:21:03 +0200

    Tweaks.

--- exp/influence2/GraphReader2.py
+++ exp/influence2/GraphReader2.py
@@ -11,13 +11,13 @@ class GraphReader2(object):
     """
     A class to read the similarity graph generated from the Arnetminer dataset 
     """
-    def __init__(self, field): 
+    def __init__(self, field, index): 
         self.field = field
         self.eps = 0.1
         
-        dirName = PathDefaults.getDataDir() + "reputation/" + self.field + "/arnetminer/"
+        dirName = PathDefaults.getDataDir() + "reputation/" + self.field + "/"
         self.coauthorFilename = dirName + "coauthors.csv"
-        self.coauthorMatrixFilename = dirName + "coauthorSimilarity.npy"
+        self.coauthorMatrixFilename = dirName + "coauthorSimilarity" + str(index) + ".npy"
         self.trainExpertsFilename = dirName + "experts_train_matches" + ".csv"
         self.testExpertsFilename = dirName + "experts_test_matches" + ".csv"
         
@@ -49,7 +49,7 @@ class GraphReader2(object):
             logging.debug("Reading training experts list")
             expertsFile = open(self.trainExpertsFilename)
             
-        expertsList = expertsFile.readlines()
+        allExperts = expertsFile.readlines()
         expertsFile.close()
         
         coauthorsFile = open(self.coauthorFilename)
@@ -57,9 +57,13 @@ class GraphReader2(object):
         coauthorsFile.close() 
         
         expertsIdList = []  
+        expertsList = []
         
-        for expert in expertsList: 
+        for expert in allExperts: 
             if expert in coauthors: 
                 expertsIdList.append(coauthors.index(expert))
+                expertsList.append(coauthors.index(expert))
+                
+        logging.debug("Found " + str(len(expertsList)) + " of " + str(len(allExperts)) + " experts")
         
         return expertsList, expertsIdList  
