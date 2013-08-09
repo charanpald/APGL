272bf62dc76c83d7e9d64e787fb2a91aaf0a8271
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-09 16:33:37 +0100

    Tweaks for new setup

--- exp/influence2/ReputationExp3.py
+++ exp/influence2/ReputationExp3.py
@@ -1,8 +1,7 @@
 """
-Use the new dataset from Pierre to perform expert recommendation. 
+Use the DBLP dataset to recommend experts. 
 """
 import numpy 
-import xml.etree.ElementTree as ET
 import logging 
 import sys 
 from apgl.util.Evaluator import Evaluator 
@@ -15,36 +14,55 @@ from apgl.util.Latex import Latex
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.random.seed(21)
 
-#field = "Test" 
 field = "Boosting" 
 #field = "IntelligentAgents"
 #field = "MachineLearning"
 
-
 dataset = ArnetMinerDataset(field)
-reader = GraphReader2(field)
-graph = reader.read()
-trainExpertsList, trainExpertsIdList = reader.readExperts(train=True)
-testExpertsList, testExpertsIdList = reader.readExperts(train=False)
+dataset.vectoriseDocuments()
+dataset.findSimilarDocuments()
 
-print(trainExpertsIdList)
-print(testExpertsIdList)
+graph, authorIndexer, relevantExperts = dataset.coauthorsGraph()
+expertMatches = dataset.matchExperts()
+
+print(graph.summary())
+
+logging.debug(expertMatches)
+
+expertMatchesInds = [] 
+for expert in expertMatches: 
+    expertMatchesInds.append(authorIndexer.translate(expert))
+   
+logging.debug(expertMatchesInds)   
+   
+relevantAuthorInds = [] 
+for author in relevantExperts: 
+    relevantAuthorInds.append(authorIndexer.translate(author))
+    
 
 #First compute graph properties 
 computeInfluence = False
-outputLists = GraphRanker.rankedLists(graph, numRuns=100, computeInfluence=computeInfluence, p=0.05, trainExpertsIdList=trainExpertsIdList)
+outputLists = GraphRanker.rankedLists(graph, numRuns=100, computeInfluence=computeInfluence, p=0.05, trainExpertsIdList=expertMatchesInds)
 itemList = RankAggregator.generateItemList(outputLists)
 methodNames = GraphRanker.getNames(computeInfluence=computeInfluence)
 
 #Then use MC2 rank aggregation 
-outputList, scores = RankAggregator.MC2(outputLists, itemList)
-outputLists.append(outputList)
-methodNames.append("MC2")
+#outputList, scores = RankAggregator.MC2(outputLists, itemList)
+#outputLists.append(outputList)
+#methodNames.append("MC2")
+#print("outputList="+str(outputList))
 
 #The supervised MC2
 #outputList2, scores2 = RankAggregator.supervisedMC22(outputLists, itemList, expertsIdList)
 #outputLists.append(outputList2)
 #methodNames.append("SMC2")
+
+#Process outputLists to only include people from the relevant field  
+newOutputLists = []
+for lst in outputLists: 
+    print(len(lst))
+    lst = lst[lst <= len(relevantAuthorInds)]  
+    newOutputLists.append(lst)
 
 print("\n")
 """
@@ -58,12 +76,12 @@ for ind in outputLists[-1][0:r]:
 print("\n")
 
 ns = numpy.arange(5, 105, 5)
-numMethods = len(outputLists)
+numMethods = len(newOutputLists)
 precisions = numpy.zeros((len(ns), numMethods))
 
 for i, n in enumerate(ns):     
     for j in range(len(outputLists)): 
-        precisions[i, j] = Evaluator.precision(testExpertsIdList, outputLists[j][0:n])
+        precisions[i, j] = Evaluator.precision(expertMatchesInds, newOutputLists[j][0:n])
 
 precisions = numpy.c_[numpy.array(ns), precisions]
 print(Latex.latexTable(Latex.array2DToRows(precisions), colNames=methodNames))
