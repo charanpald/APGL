de502d6dc8dab9399a2b05159ff1260190fb9a6e
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-09 11:04:04 +0100

    Vectorise documents and find those within a particular field.

--- exp/influence2/ArnetMinerDataset.py
+++ exp/influence2/ArnetMinerDataset.py
@@ -4,18 +4,29 @@ import logging
 import difflib 
 import re 
 import sklearn.feature_extraction.text as text 
-import sklearn.cluster 
+import gc
 import scipy.sparse
 import scipy.io
 import string 
+import array
 import pickle 
+import Stemmer
 from apgl.util.Util import Util 
 from apgl.util.PathDefaults import PathDefaults 
-from nltk import wordpunct_tokenize  
-from nltk.stem import WordNetLemmatizer 
-#from nltk.stem.snowball import PorterStemmer
-from nltk.stem.lancaster import LancasterStemmer
-from nltk.stem.porter import PorterStemmer
+from exp.sandbox.RandomisedSVD import RandomisedSVD
+from exp.util.IdIndexer import IdIndexer
+
+
+#Tokenise the documents                 
+class PorterTokeniser(object):
+    def __init__(self):
+        self.stemmer = Stemmer.Stemmer('english')
+        self.minWordLength = 2
+     
+    def __call__(self, doc):
+        doc = doc.lower()
+        tokens =  [self.stemmer.stemWord(t) for t in doc.split()]  
+        return [token for token in tokens if len(token) >= self.minWordLength]
 
 class ArnetMinerDataset(object): 
     """
@@ -33,29 +44,29 @@ class ArnetMinerDataset(object):
         
         baseDir = PathDefaults.getDataDir() + "reputation/"
         resultsDir = baseDir + field + "/"
+        
         self.expertsFileName = resultsDir + "experts.txt"
+        self.docTermMatrixSVDFilename = baseDir + "termDocMatrixSVD.npz"
+        self.authorListFilename = baseDir + "authorList.pkl"
+        self.vectoriserFilename = baseDir + "vectoriser.pkl"        
         
         self.expertMatchesFilename = resultsDir + "experts_matches.csv"
         self.trainExpertMatchesFilename = resultsDir + "experts_train_matches.csv"
         self.testExpertMatchesFilename = resultsDir + "experts_test_matches.csv"
-        self.coauthorsFilenameL1 = resultsDir + "coauthorsL1.csv"
-        self.coauthorsFilenameL2 = resultsDir + "coauthorsL2.csv"
+        self.coauthorsFilename = resultsDir + "coauthors.csv"
         self.similarAuthorsFilename = resultsDir + "similarAuthors.npy"   
-        self.expertSeedMatrixFilename = resultsDir + "seedMatrix.mtx"  
         
-        self.docTermMatrixFilename = baseDir + "termDocMatrix.mtx"
-        self.authorListFilename = baseDir + "authorList.pkl"
-        
+        self.relevantExpertsFilename = resultsDir + "relevantExperts.pkl"          
+
         self.stepSize = 100000    
         self.numLines = 15192085
         self.matchCutoff = 0.95
         self.p = 0.5
-        #self.p = 1        
+        #self.p = 1      
         
-        self.matchExperts()
-        self.splitExperts()   
-        self.writeCoauthors()
-        #self.writeSimilarityGraph()
+        self.similarityCutoff = 0.4 
+        self.k = 100
+        self.numFeatures = None
         
     def matchExperts(self): 
         expertsFile = open(self.expertsFileName)
@@ -131,188 +142,49 @@ class ArnetMinerDataset(object):
             logging.debug("Generated files exist: " + self.trainExpertMatchesFilename + " " + self.testExpertMatchesFilename)
             
     def writeCoauthors(self): 
-        if not os.path.exists(self.coauthorsFilenameL1) and not os.path.exists(self.coauthorsFilenameL2): 
-            logging.debug("Finding coauthors of path length <= 2 from experts")
-            matchedExpertsFile = open(self.trainExpertMatchesFilename)
-            matchedExperts = matchedExpertsFile.readlines()
-            matchedExperts = set([x.strip() for x in matchedExperts])
+        """
+        Using the relevant authors we find all coauthors. 
+        """
+        if not os.path.exists(self.coauthorsFilename): 
+            logging.debug("Finding coauthors of relevant experts")
             
-            inFile = open(self.dataFilename)  
+            relevantExpertsFile = open(self.relevantExpertsFilename, "w") 
+            relevantExperts = pickle.load(relevantExpertsFile)
+            relevantExpertsFile.close()
+            
+            dataFile = open(self.dataFilename)  
+            authorIndexer = IdIndexer()
+            author1Inds = array.array("i")
+            author2Inds = array.array("i")
             expertCoauthors1 = set([])
             
-            for i, line in enumerate(inFile):
+            for i, line in enumerate(dataFile):
                 Util.printIteration(i, self.stepSize, self.numLines)
                 authors = re.findall("#@(.*)", line)  
                                 
                 if len(authors) != 0: 
                     authors = [x.strip() for x in authors[0].split(",")]     
                     for author in authors: 
-                        if author in matchedExperts: 
+                        if author in relevantExperts: 
                             expertCoauthors1 = expertCoauthors1.union(set(authors))
             
             logging.debug("Found " + str(len(expertCoauthors1)) + " coauthors at level 1")
-            inFile.close()
-            
-            coauthorsFile = open(self.coauthorsFilenameL1, "w")
+            dataFile.close()
+                                   
+            #Now just write out the coauthors 
+            coauthorsFile = open(self.coauthorsFilename, "w")
             expertCoauthors = sorted([coauthor + "\n" for coauthor in expertCoauthors1]) 
             coauthorsFile.writelines(expertCoauthors)
             coauthorsFile.close()
-            logging.debug("Wrote coauthors to file " + str(self.coauthorsFilenameL1))
-            
-            #Now find their coauthors
-            inFile = open(self.dataFilename)  
-            expertCoauthors2 = set([])
-            
-            for i, line in enumerate(inFile):
-                Util.printIteration(i, self.stepSize, self.numLines)
-                authors = re.findall("#@(.*)", line)  
-                                
-                if len(authors) != 0: 
-                    authors = [x.strip() for x in authors[0].split(",")]     
-                    for author in authors: 
-                        if author in expertCoauthors1: 
-                            expertCoauthors2 = expertCoauthors2.union(set(authors))
-            
-            logging.debug("Found " + str(len(expertCoauthors2)) + " coauthors at level 2")
-            inFile.close()            
-            
-            #Now just write out the coauthors 
-            coauthorsFile = open(self.coauthorsFilenameL2, "w")
-            expertCoauthors = sorted([coauthor + "\n" for coauthor in expertCoauthors2]) 
-            coauthorsFile.writelines(expertCoauthors)
-            coauthorsFile.close()
-            logging.debug("Wrote coauthors to file " + str(self.coauthorsFilenameL2))
+            logging.debug("Wrote coauthors to file " + str(self.coauthorsFilename))
         else: 
-            logging.debug("Files already generated: " + self.coauthorsFilenameL1 + " " + self.coauthorsFilenameL2)  
+            logging.debug("Files already generated: " + self.coauthorsFilename)  
     
-    def writeSimilarityGraph(self): 
+    def vectoriseDocuments(self):
         """
-        Run through the abstracts and titles and compare them from each author. 
-        """     
-
-        
-        matchedTestExpertsFile = open(self.testExpertMatchesFilename)
-        matchedTestExperts = matchedTestExpertsFile.readlines()
-        matchedTestExperts = set([x.strip() for x in matchedTestExperts])   
-        
-        coauthorsFile = open(self.coauthorsFilenameL2)
-        coauthors = coauthorsFile.readlines()
-        coauthorsList = [x.strip() for x in coauthors]
-        coauthors = set(coauthorsList)
-    
-        matchedExpertsFile = open(self.trainExpertMatchesFilename)
-        matchedExperts = matchedExpertsFile.readlines()
-        matchedExperts = [x.strip() for x in matchedExperts]   
-    
-        expertInds = []
-        for expert in matchedExperts: 
-            expertInds.append(coauthorsList.index(expert))
-        
-        matchedTestExpertsFile = open(self.testExpertMatchesFilename)
-        matchedTestExperts = matchedTestExpertsFile.readlines()
-        matchedTestExperts = set([x.strip() for x in matchedTestExperts]) 
-        
-        testExpertInds = []
-        for expert in matchedTestExperts: 
-            if coauthorsList.count(expert) != 0:
-                testExpertInds.append(coauthorsList.index(expert))
-            else: 
-                logging.debug("Missing test expert: " + expert)
-        
-        titleAbstracts = []
-        for i in range(len(coauthors)): 
-            titleAbstracts.append("")   
-            
-        coauthorMatrix = scipy.sparse.lil_matrix((len(coauthors), len(coauthors)))
-        
-        if not os.path.exists(self.expertSeedMatrixFilename): 
-            inFile = open(self.dataFilename)
-            i = 0     
-                        
-            lastAbstract = ""
-            lastTitle = ""    
-            lastAuthors = []                    
-                        
-            for line in inFile:
-                Util.printIteration(i, self.stepSize, self.numLines)
-                    
-                #Match the fields in the file 
-                emptyLine = line == "\n"
-                title = re.findall("#\*(.*)", line)
-                currentAuthors = re.findall("#@(.*)", line)  
-                abstract = re.findall("#!(.*)", line)
-                
-                if emptyLine:
-                    for author in lastAuthors:
-                        ind = coauthorsList.index(author)
-                        titleAbstracts[ind] += lastTitle + " " + lastAbstract 
-                            
-                        for author2 in lastAuthors: 
-                            ind2 = coauthorsList.index(author2)
-                            coauthorMatrix[ind, ind2] += 1    
-                            
-                    lastAbstract = ""
-                    lastTitle = ""
-                    lastAuthors = []
- 
-                if len(title) != 0 and len(title[0]) != 0: 
-                    lastTitle = title[0]               
-                
-                if len(abstract) != 0 and len(abstract[0]) != 0: 
-                    lastAbstract = abstract[0]
-                           
-                if len(currentAuthors) != 0: 
-                    currentAuthors = currentAuthors[0].split(",")  
-                    currentAuthors = set([x.strip() for x in currentAuthors])
-                    currentAuthors = coauthors.intersection(currentAuthors)   
-
-                    lastAuthors = currentAuthors                     
-
-                i += 1
-                 
-            min_df = 2
-            ngram_max= 2
-            binary = True
-            sublinear = True
-            norm = "l2"
-            numFeatures = 3000
-            #How to combine coauthor and similarity graphs 
-            #l1 norm is useless 
-            
-            vectoriser = sklearn.feature_extraction.text.TfidfVectorizer(min_df=min_df, ngram_range=(1,ngram_max), binary=binary, sublinear_tf=sublinear, norm=norm, max_df=0.95, max_features=numFeatures)
-            X = vectoriser.fit_transform(titleAbstracts)
-            logging.debug("Generated vectoriser")
-            K = numpy.array((X[expertInds, :].dot(X.T)).todense())
-            print(K.shape)
-            
-            numAuthors = 300 
-            similarAuthors = []
-            for i, expertInd in enumerate(expertInds): 
-                similarAuthors.extend(numpy.flipud(numpy.argsort(K[i, :]))[0:numAuthors].tolist()) 
-                
-            
-            similarAuthors = numpy.unique(similarAuthors)
-            matchedExperts = numpy.array(list(matchedExperts))
-            matchedTestExperts = numpy.array(testExpertInds)
-            
-            print(matchedExperts.shape, matchedTestExperts.shape, similarAuthors.shape)
-            print(numpy.intersect1d(similarAuthors, matchedExperts))
-            print(numpy.intersect1d(similarAuthors, matchedTestExperts))
-            
-            numpy.save(self.similarAuthorsFilename, similarAuthors)
-            logging.debug("Saved similar authors as " + self.similarAuthorsFilename)
-            scipy.io.mmwrite(self.expertSeedMatrixFilename, coauthorMatrix)
-            logging.debug("Saved graph matrix as " + self.expertSeedMatrixFilename)
-        else: 
-            logging.debug("File already generated: " + self.similarAuthorsFilename + ".npy")  
-            
-    def findAuthorsInField(self):
+        We want to go through the dataset and vectorise all the title+abstracts.
         """
-        We want to go through the dataset and find all authors who are in the particular 
-        field. We use Gensim for this purpose. The title is concatenated to the abstract 
-        if it exists and this is used as the corpus. 
-        """
-        if not os.path.exists(self.docTermMatrixFilename) or not os.path.exists(self.authorListFilename):
+        if not os.path.exists(self.docTermMatrixSVDFilename) or not os.path.exists(self.authorListFilename):
             #We load all title+abstracts 
             inFile = open(self.dataFilename)  
             authorList = []
@@ -351,32 +223,88 @@ class ArnetMinerDataset(object):
                     currentAuthors = set([x.strip() for x in currentAuthors])
                     lastAuthors = currentAuthors                     
     
-            inFile.close()
-            
-            logging.debug("Finished reading file")            
-            
-            #Tokenise the documents                 
-            class WordNetTokeniser(object):
-                def __init__(self):
-                    self.stemmer = WordNetLemmatizer()
-                    self.minWordLength = 2
-                 
-                def __call__(self, doc):
-                    doc = doc.lower()
-                    tokens =  [self.stemmer.lemmatize(t) for t in doc.split()]  
-                    return [token for token in tokens if len(token) >= self.minWordLength]
-            
-            vectoriser = text.TfidfVectorizer(min_df=2, ngram_range=(1,2), binary=True, sublinear_tf=True, norm="l2", max_df=0.95, stop_words="english", tokenizer=WordNetTokeniser(), max_features=5000)
-            X = vectoriser.fit_transform(documentList)
-            
-            #Save matrix and authors 
-            scipy.io.mmwrite(self.docTermMatrixFilename, X)
-            logging.debug("Wrote to file " + self.docTermMatrixFilename)
+            inFile.close() 
+            logging.debug("Finished reading file")    
             
             authorListFile = open(self.authorListFilename, "w")
             pickle.dump(authorList, authorListFile) 
             authorListFile.close()
-            logging.debug("Wrote to file " + self.authorListFilename)
+            del authorListFile
+            logging.debug("Wrote to file " + self.authorListFilename)            
             
+            vectoriser = text.TfidfVectorizer(min_df=2, ngram_range=(1,2), binary=False, sublinear_tf=True, norm="l2", max_df=0.95, stop_words="english", tokenizer=PorterTokeniser(), max_features=self.numFeatures)
+            X = vectoriser.fit_transform(documentList)
+            logging.debug("Finished vectorising documents")
+            
+            print(X[:, vectoriser.get_feature_names().index("boost")])
+                
+            #Save vectoriser - note that we can't pickle the tokeniser so it needs to be reset when loaded 
+            vectoriser.tokenizer = None 
+            vectoriserFile = open(self.vectoriserFilename, "w")
+            pickle.dump(vectoriser, vectoriserFile)
+            vectoriserFile.close()
+            logging.debug("Wrote vectoriser to file " + self.vectoriserFilename)    
+            del vectoriser  
+            gc.collect()
+                
+            #Take the SVD of X (maybe better to use PROPACK here depending on size of X)
+            logging.debug("Computing the SVD of the document-term matrix")
+            X = X.tocsc()
+            U, s, V = RandomisedSVD.svd(X, self.k)
+            
+            numpy.savez(self.docTermMatrixSVDFilename, U, s, V)
+            logging.debug("Wrote to file " + self.docTermMatrixSVDFilename)
         else: 
-            logging.debug("Files already generated: " + self.docTermMatrixFilename + " " + self.authorListFilename)         
+            logging.debug("Files already generated: " + self.docTermMatrixSVDFilename + " " + self.authorListFilename)   
+            
+    def findSimilarDocuments(self): 
+        """
+        Find all documents within the same field. 
+        """
+        if not os.path.exists(self.relevantExpertsFilename): 
+            #First load all the components 
+            vectoriserFile = open(self.vectoriserFilename)
+            vectoriser = pickle.load(vectoriserFile)
+            vectoriser.tokenizer = PorterTokeniser() 
+            vectoriserFile.close() 
+            
+            authorListFile = open(self.authorListFilename)
+            authorList = pickle.load(authorListFile)
+            authorListFile.close()     
+            
+            data = numpy.load(self.docTermMatrixSVDFilename)    
+            U, s, V = data["arr_0"], data["arr_1"], data["arr_2"] 
+            
+            #Normalised rows of U 
+            normU = numpy.sqrt((U**2).sum(1))
+            invNormU = 1/(normU + numpy.array(normU==0, numpy.int))
+            U = (U.T*invNormU).T
+        
+            #newX = vectoriser.transform(["java"])
+            newX = vectoriser.transform([self.field])
+            if newX.nnz == 0: 
+                raise ValueError("Query term not found") 
+            
+            newU = newX.dot(V*(1/s)).T
+            newU = newU/numpy.linalg.norm(newU)
+            similarities = U.dot(newU).ravel()
+            
+            relevantDocs = numpy.arange(similarities.shape[0])[similarities >= self.similarityCutoff]  
+                    
+            #Now find all authors corresponding to the documents 
+            experts = [] 
+            for docInd in relevantDocs: 
+                experts.extend(authorList[docInd])
+                
+            experts = set(experts)
+            logging.debug("Authors: " + str(experts))
+            logging.debug("Number of authors : " + str(len(experts)))
+            
+            relevantExpertsFile = open(self.relevantExpertsFilename, "w") 
+            pickle.dump(experts, relevantExpertsFile)
+            relevantExpertsFile.close()
+            logging.debug("Saved experts in file " + self.relevantExpertsFilename)
+        else: 
+            logging.debug("File already generated " + self.relevantExpertsFilename)
+        
+        
