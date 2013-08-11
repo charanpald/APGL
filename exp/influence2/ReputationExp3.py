007ea9086a95a9058de622b8dfac2bdbd4f08ad0
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-11 22:46:59 +0100

    Run through params

--- exp/influence2/ReputationExp3.py
+++ exp/influence2/ReputationExp3.py
@@ -4,84 +4,126 @@ Use the DBLP dataset to recommend experts.
 import numpy 
 import logging 
 import sys 
-from apgl.util.Evaluator import Evaluator 
+import sklearn.metrics 
 from exp.influence2.GraphRanker import GraphRanker 
 from exp.influence2.RankAggregator import RankAggregator
 from exp.influence2.ArnetMinerDataset import ArnetMinerDataset
-from exp.influence2.GraphReader2 import GraphReader2
 from apgl.util.Latex import Latex 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.random.seed(21)
 
-field = "Boosting" 
-#field = "IntelligentAgents"
-#field = "MachineLearning"
+#field = "Boosting" 
+#field = "Intelligent Agents"
+field = "Machine Learning"
+#field = "Ontology Alignment"
 
-dataset = ArnetMinerDataset(field)
-dataset.vectoriseDocuments()
-dataset.findSimilarDocuments()
+fields = ["Boosting", "Intelligent Agents", "Machine Learning", "Ontology Alignment"]
 
-graph, authorIndexer, relevantExperts = dataset.coauthorsGraph()
-expertMatches = dataset.matchExperts()
+ks = [100]
+maxRelevantAuthors = [1000, 1500]
+similarityCutoffs = [0.4, 0.5, 0.6]
+bestAveragePrecision = numpy.zeros((len(fields), len(maxRelevantAuthors), len(similarityCutoffs)))
 
-print(graph.summary())
 
-logging.debug(expertMatches)
-
-expertMatchesInds = [] 
-for expert in expertMatches: 
-    expertMatchesInds.append(authorIndexer.translate(expert))
-   
-logging.debug(expertMatchesInds)   
-   
-relevantAuthorInds = [] 
-for author in relevantExperts: 
-    relevantAuthorInds.append(authorIndexer.translate(author))
-    
-
-#First compute graph properties 
-computeInfluence = False
-outputLists = GraphRanker.rankedLists(graph, numRuns=100, computeInfluence=computeInfluence, p=0.05, trainExpertsIdList=expertMatchesInds)
-itemList = RankAggregator.generateItemList(outputLists)
-methodNames = GraphRanker.getNames(computeInfluence=computeInfluence)
-
-#Then use MC2 rank aggregation 
-#outputList, scores = RankAggregator.MC2(outputLists, itemList)
-#outputLists.append(outputList)
-#methodNames.append("MC2")
-#print("outputList="+str(outputList))
-
-#The supervised MC2
-#outputList2, scores2 = RankAggregator.supervisedMC22(outputLists, itemList, expertsIdList)
-#outputLists.append(outputList2)
-#methodNames.append("SMC2")
-
-#Process outputLists to only include people from the relevant field  
-newOutputLists = []
-for lst in outputLists: 
-    print(len(lst))
-    lst = lst[lst <= len(relevantAuthorInds)]  
-    newOutputLists.append(lst)
-
-print("\n")
-"""
-r = 20 
-logging.debug("Top " + str(r) + " authors:")
-for ind in outputLists[-1][0:r]: 
-    key = (key for key,value in reader.authorIndexer.getIdDict().items() if value==ind).next()
-    logging.debug(key)
-"""
-
-print("\n")
-
-ns = numpy.arange(5, 105, 5)
-numMethods = len(newOutputLists)
-precisions = numpy.zeros((len(ns), numMethods))
-
-for i, n in enumerate(ns):     
-    for j in range(len(outputLists)): 
-        precisions[i, j] = Evaluator.precision(expertMatchesInds, newOutputLists[j][0:n])
-
-precisions = numpy.c_[numpy.array(ns), precisions]
-print(Latex.latexTable(Latex.array2DToRows(precisions), colNames=methodNames))
+for r, field in enumerate(fields): 
+    for s, maxRelAuthors in enumerate(maxRelevantAuthors): 
+        for t, similarityCutoff in enumerate(similarityCutoffs): 
+            
+            dataset = ArnetMinerDataset(field)
+            dataset.overwriteRelevantExperts = True
+            dataset.overwriteCoauthors = True
+            dataset.maxRelevantAuthors = maxRelAuthors
+            dataset.similarityCutoff = similarityCutoff
+            dataset.vectoriseDocuments()
+            dataset.findSimilarDocuments()
+            
+            graph, authorIndexer, relevantExperts = dataset.coauthorsGraph()
+            expertMatches, expertsSet = dataset.matchExperts()
+            
+            logging.debug(expertMatches)
+            logging.debug(graph.summary())
+            
+            expertMatchesInds = [] 
+            for expert in expertMatches: 
+                expertMatchesInds.append(authorIndexer.translate(expert))
+               
+            logging.debug(expertMatchesInds)   
+               
+            relevantAuthorInds = [] 
+            for author in relevantExperts: 
+                relevantAuthorInds.append(authorIndexer.translate(author))
+            
+            assert (numpy.array(relevantAuthorInds) < len(relevantAuthorInds)).all()
+            
+            #First compute graph properties 
+            computeInfluence = False
+            outputLists = GraphRanker.rankedLists(graph, numRuns=100, computeInfluence=computeInfluence, p=0.05, trainExpertsIdList=expertMatchesInds)
+            itemList = RankAggregator.generateItemList(outputLists)
+            methodNames = GraphRanker.getNames(computeInfluence=computeInfluence)
+            
+            #Then use MC2 rank aggregation 
+            #outputList, scores = RankAggregator.MC2(outputLists, itemList)
+            #outputLists.append(outputList)
+            #methodNames.append("MC2")
+            #print("outputList="+str(outputList))
+            
+            #The supervised MC2
+            #outputList2, scores2 = RankAggregator.supervisedMC22(outputLists, itemList, expertsIdList)
+            #outputLists.append(outputList2)
+            #methodNames.append("SMC2")
+            
+            #Process outputLists to only include people from the relevant field  
+            newOutputLists = []
+            for lst in outputLists: 
+                lst = lst[lst < len(relevantAuthorInds)]  
+                newOutputLists.append(lst)
+            
+            print("\n")
+            """
+            r = 20 
+            logging.debug("Top " + str(r) + " authors:")
+            for ind in outputLists[-1][0:r]: 
+                key = (key for key,value in reader.authorIndexer.getIdDict().items() if value==ind).next()
+                logging.debug(key)
+            """
+            
+            print("\n")
+            
+            ns = numpy.arange(5, 105, 5)
+            numMethods = len(newOutputLists)
+            
+            precisions = numpy.zeros((len(ns), numMethods))
+            averagePrecisions = numpy.zeros(numMethods)
+            
+            for i, n in enumerate(ns):     
+                for j in range(len(outputLists)): 
+                    predY = -numpy.ones(len(relevantAuthorInds))
+                    predY[expertMatchesInds] = 1
+                    
+                    testY = -numpy.ones(len(relevantAuthorInds))
+                    testY[newOutputLists[j][0:n]] = 1
+                    
+                    precisions[i, j] = sklearn.metrics.precision_score(testY, predY) 
+                    #precisions[i, j] = sklearn.metrics.precision_score(testY, predY, labels=numpy.array([-1, 1, numpy.int]), average="micro")
+            
+            n = 50 
+            
+            for j in range(len(outputLists)): 
+                predY = -numpy.ones(len(relevantAuthorInds))
+                predY[expertMatchesInds] = 1
+                
+                testY = -numpy.ones(len(relevantAuthorInds))
+                testY[newOutputLists[j][0:n]] = 1
+                
+                averagePrecisions[j] = sklearn.metrics.average_precision_score(testY, predY)
+            
+            precisions = numpy.c_[numpy.array(ns), precisions]
+            
+            print(Latex.latexTable(Latex.array2DToRows(precisions), colNames=methodNames))
+            print(Latex.array1DToRow(averagePrecisions))
+            
+            bestAveragePrecision[r,s,t] = numpy.max(averagePrecisions)
+            
+for r in range(bestAveragePrecision.shape[0]): 
+    print(bestAveragePrecision[r, :, :])
