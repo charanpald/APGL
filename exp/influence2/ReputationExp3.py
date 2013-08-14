78aea205af3ba9d5f1d86617754b6895ddb7df29
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-14 16:06:08 +0100

    No need to tune similarity cutoff

--- exp/influence2/ReputationExp3.py
+++ exp/influence2/ReputationExp3.py
@@ -13,64 +13,62 @@ from apgl.util.Latex import Latex
 from apgl.util.Evaluator import Evaluator
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+numpy.set_printoptions(suppress=True, precision=3, linewidth=160)
 numpy.random.seed(21)
 
 averagePrecisionN = 50 
 fields = ["Boosting", "Intelligent Agents", "Machine Learning", "Ontology Alignment"]
 #fields = ["Boosting"]
-ks = [50, 100, 150]
-#ks = [50]
+similarityCutoff = 0.2
+k = 100
 maxRelevantAuthors = [100, 200, 500, 1000]
 #maxRelevantAuthors = [100, 200]
-similarityCutoffs = [0.2, 0.3, 0.4, 0.5]
-#similarityCutoffs = [0.2]
-bestAveragePrecision = numpy.zeros((len(fields), len(maxRelevantAuthors), len(similarityCutoffs)))
+bestAveragePrecision = numpy.zeros((len(fields), len(maxRelevantAuthors)))
 ns = numpy.arange(5, 105, 5)
 
 for r, field in enumerate(fields): 
-    dataset = ArnetMinerDataset(field, k=100)    
+    dataset = ArnetMinerDataset(field, k=k)    
     for s, maxRelAuthors in enumerate(maxRelevantAuthors): 
-        for t, similarityCutoff in enumerate(similarityCutoffs): 
-            dataset.overwriteRelevantExperts = True
-            dataset.overwriteCoauthors = True
-            dataset.maxRelevantAuthors = maxRelAuthors
-            dataset.similarityCutoff = similarityCutoff
-            
-            dataset.vectoriseDocuments()
-            dataset.findSimilarDocuments()
-            
-            graph, authorIndexer, relevantExperts = dataset.coauthorsGraph()
-            expertMatches, expertsSet = dataset.matchExperts()     
+        dataset.overwriteRelevantExperts = True
+        dataset.overwriteCoauthors = True
+        dataset.maxRelevantAuthors = maxRelAuthors
+        dataset.similarityCutoff = similarityCutoff
         
-            expertMatchesInds = authorIndexer.translate(expertMatches) 
-            relevantAuthorInds = authorIndexer.translate(relevantExperts) 
-            assert (numpy.array(relevantAuthorInds) < len(relevantAuthorInds)).all()
-            
-            #First compute graph properties 
-            computeInfluence = False
-            graphRanker = GraphRanker(k=100, numRuns=100, computeInfluence=computeInfluence, p=0.05, trainExpertsIdList=expertMatchesInds)
-            outputLists = graphRanker.vertexRankings(graph, relevantAuthorInds, [relevantAuthorInds])
-            itemList = RankAggregator.generateItemList(outputLists)
-            #methodNames = graphRanker.getNames()
-            
-            numMethods = len(outputLists)
-            precisions = numpy.zeros((len(ns), numMethods))
-            averagePrecisions = numpy.zeros(numMethods)
-            
-            for i, n in enumerate(ns):     
-                for j in range(len(outputLists)): 
-                    precisions[i, j] = Evaluator.precisionFromIndLists(expertMatchesInds, outputLists[j][0:n]) 
-                    
-            for j in range(len(outputLists)):                 
-                averagePrecisions[j] = Evaluator.averagePrecisionFromIndLists(expertMatchesInds, outputLists[j][0:averagePrecisionN]) 
-            
-            precisions = numpy.c_[numpy.array(ns), precisions]
-            logging.debug(Latex.array1DToRow(averagePrecisions*len(expertMatches)))
-            bestAveragePrecision[r,s,t] = numpy.max(averagePrecisions)*len(expertMatches) 
-            logging.debug("Max average precision for " + str((field, maxRelAuthors, similarityCutoff)) + " = " + str(bestAveragePrecision[r,s,t]))
+        dataset.vectoriseDocuments()
+        dataset.findSimilarDocuments()
+        
+        graph, authorIndexer, relevantExperts = dataset.coauthorsGraph()
+        expertMatches, expertsSet = dataset.matchExperts()     
+    
+        expertMatchesInds = authorIndexer.translate(expertMatches) 
+        relevantAuthorInds = authorIndexer.translate(relevantExperts) 
+        assert (numpy.array(relevantAuthorInds) < len(relevantAuthorInds)).all()
+        
+        #First compute graph properties 
+        computeInfluence = False
+        graphRanker = GraphRanker(k=100, numRuns=100, computeInfluence=computeInfluence, p=0.05, trainExpertsIdList=expertMatchesInds)
+        outputLists = graphRanker.vertexRankings(graph, relevantAuthorInds, [relevantAuthorInds])
+        itemList = RankAggregator.generateItemList(outputLists)
+        #methodNames = graphRanker.getNames()
+        
+        numMethods = len(outputLists)
+        precisions = numpy.zeros((len(ns), numMethods))
+        averagePrecisions = numpy.zeros(numMethods)
+        
+        for i, n in enumerate(ns):     
+            for j in range(len(outputLists)): 
+                precisions[i, j] = Evaluator.precisionFromIndLists(expertMatchesInds, outputLists[j][0:n]) 
+                
+        for j in range(len(outputLists)):                 
+            averagePrecisions[j] = Evaluator.averagePrecisionFromLists(expertMatchesInds, outputLists[j][0:averagePrecisionN]) 
+        
+        precisions = numpy.c_[numpy.array(ns), precisions]
+        logging.debug(Latex.array1DToRow(averagePrecisions*len(expertMatches)))
+        bestAveragePrecision[r,s] = numpy.max(averagePrecisions)*len(expertMatches) 
+        logging.debug("Max average precision for " + str((field, maxRelAuthors)) + " = " + str(bestAveragePrecision[r,s]))
 
 for r in range(bestAveragePrecision.shape[0]):
     print("field = " + str(fields[r]))
-    print(bestAveragePrecision[r, :, :])
+    print(bestAveragePrecision[r, :])
 
 logging.debug("All done!")
