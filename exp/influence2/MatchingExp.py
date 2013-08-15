c291656111633dd3761257e2dc752fa70f17bfb4
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-15 18:15:24 +0100

    Run LDA or LSI

--- exp/influence2/MatchingExp.py
+++ exp/influence2/MatchingExp.py
@@ -6,34 +6,45 @@ as possible.
 import numpy 
 import logging 
 import sys 
+import argparse 
 from exp.influence2.ArnetMinerDataset import ArnetMinerDataset
 from apgl.util.PathDefaults import PathDefaults
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--runLDA', action='store_true')
+args = parser.parse_args()
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.random.seed(21)
 
-ks = numpy.array([50, 75, 100, 150, 300])
+ks = numpy.array([100, 150, 200])
 maxRelevantAuthors = numpy.array([100, 200, 500]) 
 fields = ["Boosting", "Intelligent Agents", "Machine Learning", "Ontology Alignment"]
 
 coverage1 = numpy.zeros((len(ks), len(fields), maxRelevantAuthors.shape[0]))
 coverage2 = numpy.zeros((len(ks), len(fields), maxRelevantAuthors.shape[0]))
-
+runLDA = args.runLDA
+print(runLDA)
 
 for m, k in enumerate(ks): 
-    overwriteSVD = True
+    overwriteVectoriser = True
+    overwriteModel = True
+    
     for i, field in enumerate(fields): 
         for j, maxRelAuthors in enumerate(maxRelevantAuthors): 
             logging.debug("k=" + str(k) + " field=" + field + " maxRelAuthors=" + str(maxRelAuthors))
             dataset = ArnetMinerDataset(field)
             dataset.k = k 
-            dataset.overwriteRelevantExperts = True
-            dataset.overwriteCoauthors = True
-            dataset.overwriteSVD = overwriteSVD
+            dataset.overwrite = True
+            dataset.overwriteVectoriser = overwriteVectoriser
+            dataset.overwriteModel = overwriteModel
             dataset.maxRelevantAuthors = maxRelAuthors
+            dataset.dataFilename = dataset.dataDir + "DBLP-citation-1000000.txt"
             
-            dataset.vectoriseDocuments()
-            dataset.findSimilarDocuments()
+            if runLDA:
+                dataset.findSimilarDocumentsLDA()
+            else: 
+                dataset.findSimilarDocumentsLSI()
     
             graph, authorIndexer, relevantExperts = dataset.coauthorsGraph()
             expertMatches, expertsSet = dataset.matchExperts()
@@ -41,7 +52,8 @@ for m, k in enumerate(ks):
             coverage1[m, i, j] = float(len(expertMatches))/len(relevantExperts)
             coverage2[m, i, j] = float(len(expertMatches))/len(expertsSet)
             
-            overwriteSVD = False
+            overwriteVectoriser = False 
+            overwriteModel = False
             
             logging.debug("Coverage 2: " + str((k, field, maxRelAuthors)) + " = " + str(coverage2[m, i, j]))
 
@@ -50,5 +62,8 @@ for m, k in enumerate(ks):
     print(coverage1[m, :, :])
     print(coverage2[m, :, :])
 
-resultsFilename = PathDefaults.getOutputDir() + "MatchingResults.npz"
+if runLDA: 
+    resultsFilename = PathDefaults.getOutputDir() + "MatchingResultsLDA.npz"
+else: 
+    resultsFilename = PathDefaults.getOutputDir() + "MatchingResultsLSI.npz"
 numpy.savez(resultsFilename, coverage1, coverage2)
