2aa0dcf78e38216f0bf77602138a22f32fab59e1
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-22 10:47:40 +0100

    Larger k

--- exp/influence2/ReputationExp3.py
+++ exp/influence2/ReputationExp3.py
@@ -30,7 +30,7 @@ dataset = ArnetMinerDataset(runLSI=runLSI)
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-100000.txt"
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-5000000.txt"
 dataset.dataFilename = dataset.dataDir + "DBLP-citation-7000000.txt"
-dataset.ks = [100, 150, 200, 250, 300]
+dataset.ks = [100, 150, 200, 250, 300, 350]
 dataset.overwriteGraph = True
 dataset.overwriteModel = True
 
