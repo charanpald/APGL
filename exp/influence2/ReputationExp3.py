c7cca552475adacfb86c0a3f9e2c2ee814dc6f9a
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-26 21:40:11 +0100

    5000000 lines

--- exp/influence2/ReputationExp3.py
+++ exp/influence2/ReputationExp3.py
@@ -29,8 +29,8 @@ runLSI = not args.runLDA
 dataset = ArnetMinerDataset(runLSI=runLSI) 
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-100000.txt"
 
-dataset.dataFilename = dataset.dataDir + "DBLP-citation-1000000.txt"
-#dataset.dataFilename = dataset.dataDir + "DBLP-citation-5000000.txt"
+#dataset.dataFilename = dataset.dataDir + "DBLP-citation-1000000.txt"
+dataset.dataFilename = dataset.dataDir + "DBLP-citation-5000000.txt"
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-7000000.txt"
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-Feb21.txt" 
 dataset.ks = [300, 350, 400, 450, 500, 550, 600]
