5d6f3da51d970d1266006b82c7b3a06d7a69b373
Charanpal Dhanjal <charanpal@gmail.com>
2013-08-30 21:24:32 +0100

    Use full dataset

--- exp/influence2/ReputationExp3.py
+++ exp/influence2/ReputationExp3.py
@@ -30,9 +30,9 @@ dataset = ArnetMinerDataset(runLSI=runLSI)
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-100000.txt"
 
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-1000000.txt"
-dataset.dataFilename = dataset.dataDir + "DBLP-citation-5000000.txt"
+#dataset.dataFilename = dataset.dataDir + "DBLP-citation-5000000.txt"
 #dataset.dataFilename = dataset.dataDir + "DBLP-citation-7000000.txt"
-#dataset.dataFilename = dataset.dataDir + "DBLP-citation-Feb21.txt" 
+dataset.dataFilename = dataset.dataDir + "DBLP-citation-Feb21.txt" 
 dataset.ks = [100, 200, 300, 400, 500, 600]
 dataset.minDfs = [0.05, 0.01, 0.005, 0.001]
 dataset.overwriteGraph = True
