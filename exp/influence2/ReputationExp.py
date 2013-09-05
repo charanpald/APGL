d01c7fff020f439bdfb12e87a23a523d7c675632
Charanpal Dhanjal <charanpal@gmail.com>
2013-09-05 14:07:22 +0200

    Figure out coverage of experts

---
+++ exp/influence2/ReputationExp.py
@@ -0,0 +1,34 @@
+
+"""
+Find out which experts exist in the DBLP dataset and how many abstracts 
+they have. 
+"""
+import logging 
+import sys 
+import itertools 
+import numpy 
+from exp.influence2.ArnetMinerDataset import ArnetMinerDataset
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+dataset = ArnetMinerDataset() 
+#dataset.dataFilename = dataset.dataDir + "DBLP-citation-100000.txt" 
+
+authorList, documentList, citationList = dataset.readAuthorsAndDocuments()
+
+authorSet = set(itertools.chain.from_iterable(authorList))
+
+print("Found all authors")
+expertMatchesDict = {} 
+
+for field in dataset.fields: 
+    expertMatchesDict[field] = 0    
+    
+    for expert in dataset.expertsDict[field]: 
+        if expert in authorSet: 
+            expertMatchesDict[field] += 1
+    
+    expertMatchesDict[field] /= float(len(dataset.expertsDict[field]))
+
+print(expertMatchesDict)
+
