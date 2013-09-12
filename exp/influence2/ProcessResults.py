7afc614cec5675b2b95473295706433fcde9bdb5
Charanpal Dhanjal <charanpal@gmail.com>
2013-09-12 17:46:08 +0200

    Include train precision, rank aggregation, and tidy up

--- exp/influence2/ProcessResults.py
+++ exp/influence2/ProcessResults.py
@@ -5,6 +5,7 @@ matplotlib.use("GTK3Agg")
 import matplotlib.pyplot as plt 
 from exp.influence2.ArnetMinerDataset import ArnetMinerDataset
 from exp.influence2.GraphRanker import GraphRanker
+from exp.influence2.RankAggregator import RankAggregator
 from apgl.util.Latex import Latex 
 from apgl.util.Util import Util 
 from apgl.util.Evaluator import Evaluator 
@@ -15,14 +16,16 @@ dataset = ArnetMinerDataset(runLSI=ranLSI)
 #dataset.fields = ["Intelligent Agents"]
 
 ns = numpy.arange(5, 55, 5)
-bestAveragePrecisions = numpy.zeros(len(dataset.fields))
+bestaverageTestPrecisions = numpy.zeros(len(dataset.fields))
 
 computeInfluence = True
-graphRanker = GraphRanker(k=100, numRuns=100, computeInfluence=computeInfluence, p=0.05, inputRanking=[])
+graphRanker = GraphRanker(k=100, numRuns=100, computeInfluence=computeInfluence, p=0.05, inputRanking=[1, 2])
 methodNames = graphRanker.getNames()
+methodNames.append("MC2")
 
-numMethods = 7
-averagePrecisions = numpy.zeros((len(dataset.fields), len(ns), numMethods))
+numMethods = len(methodNames) 
+averageTrainPrecisions = numpy.zeros((len(dataset.fields), len(ns), numMethods))
+averageTestPrecisions = numpy.zeros((len(dataset.fields), len(ns), numMethods))
 
 coverages = numpy.load(dataset.coverageFilename)
 print("==== Coverages ====")
@@ -39,34 +42,71 @@ for s, field in enumerate(dataset.fields):
         outputLists, trainExpertMatchesInds, testExpertMatchesInds = Util.loadPickle(outputFilename)
         graph, authorIndexer = Util.loadPickle(dataset.getCoauthorsFilename(field))
         
-        numMethods = len(outputLists)
-        precisions = numpy.zeros((len(ns), numMethods))
+        trainPrecisions = numpy.zeros((len(ns), numMethods))
+        testPrecisions = numpy.zeros((len(ns), numMethods))
         
-        f1Scores = numpy.zeros(numMethods)
+        #Remove training experts from the output lists 
+        testOutputLists = [] 
+        for outputList in outputLists:
+            newOutputList = []
+            for item in outputList: 
+                if item not in trainExpertMatchesInds: 
+                    newOutputList.append(item)
+            testOutputLists.append(newOutputList)
         
-        for i, n in enumerate(ns):     
-            for j in range(len(outputLists)): 
-                newOutputList = []
-                for item in outputLists[j][0:n]: 
-                    if item not in trainExpertMatchesInds: 
-                        newOutputList.append(item)
                 
-                precisions[i, j] = Evaluator.precisionFromIndLists(testExpertMatchesInds, newOutputList) 
-                averagePrecisions[s, i, j] = Evaluator.averagePrecisionFromLists(testExpertMatchesInds, newOutputList, n) 
+        for i, n in enumerate(ns):     
+            for j, outputList in enumerate(outputLists): 
+                testOutputList = testOutputLists[j]                
+                
+                trainPrecisions[i, j] = Evaluator.precisionFromIndLists(trainExpertMatchesInds, outputList[0:n]) 
+                testPrecisions[i, j] = Evaluator.precisionFromIndLists(testExpertMatchesInds, testOutputList[0:n]) 
+                averageTrainPrecisions[s, i, j] = Evaluator.averagePrecisionFromLists(trainExpertMatchesInds, outputList[0:n], n)
+                averageTestPrecisions[s, i, j] = Evaluator.averagePrecisionFromLists(testExpertMatchesInds, testOutputList[0:n], n) 
+
+        #Now look at rank aggregations
+        relevantItems = set([])
+        for outputList in testOutputLists: 
+            relevantItems = relevantItems.union(outputList)
+        relevantItems = list(relevantItems)
+
+        newOutputList = []
+        #newOutputList.append(testOutputLists[0])
+        newOutputList.append(testOutputLists[1])
+        newOutputList.append(testOutputLists[2])
+        newOutputList.append(testOutputLists[3])
+        #newOutputList.append(testOutputLists[4])
+        #newOutputList.append(testOutputLists[5])
+        #newOutputList.append(testOutputLists[6])
+
+        rankAggregate = RankAggregator.MC2(newOutputList, relevantItems)[0]
+        j = len(outputLists)
         
+        for i, n in enumerate(ns):
+            testPrecisions[i, j] = Evaluator.precisionFromIndLists(testExpertMatchesInds, rankAggregate) 
+            averageTestPrecisions[s, i, j] = Evaluator.averagePrecisionFromLists(testExpertMatchesInds, rankAggregate, n) 
         
         print(authorIndexer.reverseTranslate(trainExpertMatchesInds))
         print(authorIndexer.reverseTranslate(testExpertMatchesInds))
-        print(authorIndexer.reverseTranslate(outputLists[0][0:10]))
+        print(authorIndexer.reverseTranslate(testOutputLists[0][0:10]))
         
-        print(precisions)
-        print(averagePrecisions[s, :, :] )
+        print(trainPrecisions)
+        print(averageTrainPrecisions[s, :, :])
+        print(testPrecisions)
+        print(averageTestPrecisions[s, :, :])
     except IOError as e: 
         print(e)
 
-meanAveragePrecisions = numpy.mean(averagePrecisions, 0)
-meanAveragePrecisions = numpy.c_[numpy.array(ns), meanAveragePrecisions]
+meanAverageTrainPrecisions = numpy.mean(averageTrainPrecisions, 0)
+meanAverageTrainPrecisions = numpy.c_[numpy.array(ns), meanAverageTrainPrecisions]
+
+meanAverageTestPrecisions = numpy.mean(averageTestPrecisions, 0)
+meanAverageTestPrecisions = numpy.c_[numpy.array(ns), meanAverageTestPrecisions]
+
 print("==== Summary ====")
 print(Latex.listToRow(methodNames))
-print(Latex.array2DToRows(meanAveragePrecisions))
+print(Latex.array2DToRows(meanAverageTrainPrecisions))
+
+print(Latex.listToRow(methodNames))
+print(Latex.array2DToRows(meanAverageTestPrecisions))
 
