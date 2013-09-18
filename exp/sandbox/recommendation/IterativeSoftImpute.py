be7c5fa8791042335ec194e20403a357ba864899
Charanpal Dhanjal <charanpal@gmail.com>
2013-09-18 17:26:32 +0200

    New svd update

--- exp/sandbox/recommendation/IterativeSoftImpute.py
+++ exp/sandbox/recommendation/IterativeSoftImpute.py
@@ -210,6 +210,12 @@ class IterativeSoftImpute(AbstractMatrixCompleter):
                             newU, newS, newV = RandomisedSVD.svd(L, self.iterativeSoftImpute.k, p=self.iterativeSoftImpute.p, q=self.iterativeSoftImpute.q)
                         else: 
                             newU, newS, newV = RandomisedSVD.svd(L, self.iterativeSoftImpute.k, p=self.iterativeSoftImpute.p, q=self.iterativeSoftImpute.qu, omega=self.oldV)
+                    elif self.iterativeSoftImpute.svdAlg=="rsvdUpdate2":
+                        L = LinOperatorUtils.sparseLowRankOp(Y, self.oldU, self.oldS, self.oldV, parallel=True)
+                        if self.j == 0: 
+                            newU, newS, newV = RandomisedSVD.svd(L, self.iterativeSoftImpute.k, p=self.iterativeSoftImpute.p, q=self.iterativeSoftImpute.q)
+                        else: 
+                            newU, newS, newV = RandomisedSVD.updateSvd(Y, self.U, self.oldS, self.V, E, self.iterativeSoftImpute.k, p=self.iterativeSoftImpute.p)
                     else:
                         raise ValueError("Unknown SVD algorithm: " + self.iterativeSoftImpute.svdAlg)
                         
