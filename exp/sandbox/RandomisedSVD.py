f874b9c9109f802a8a52bfa98656dfef6dac3fa0
Charanpal Dhanjal <charanpal@gmail.com>
2013-09-18 17:26:21 +0200

    Implement SVD update, and use rsvd from sppy

--- exp/sandbox/RandomisedSVD.py
+++ exp/sandbox/RandomisedSVD.py
@@ -1,8 +1,9 @@
 import numpy 
 import scipy.sparse 
 import gc 
-from exp.util.GeneralLinearOperator import GeneralLinearOperator
+from sppy.linalg.GeneralLinearOperator import GeneralLinearOperator
 from apgl.util.Parameter import Parameter 
+import sppy.linalg 
 
 class RandomisedSVD(object): 
     """
@@ -14,14 +15,14 @@ class RandomisedSVD(object):
         pass
     
     @staticmethod
-    def svd(X, k, p=10, q=2, omega=None): 
+    def svd(A, k, p=10, q=2, omega=None): 
         """
-        Compute the SVD of a sparse or dense matrix X, finding the first k 
+        Compute the SVD of a sparse or dense matrix A, finding the first k 
         singular vectors/values, using exponent q. Returns the left and right singular 
         vectors, and the singular values. The resulting matrix can be approximated 
-        using X ~ U s V.T. 
+        using A ~ U s V.T. 
         
-        :param X: A matrix or GeneralLinearOperator 
+        :param A: A matrix or GeneralLinearOperator 
         
         :param k: The number of singular values and random projections
         
@@ -31,69 +32,49 @@ class RandomisedSVD(object):
         
         :param omega: An initial matrix to perform random projections onto with at least k columns 
         """
+        return sppy.linalg.rsvd(A, k, p, q, omega)
+      
+    @staticmethod 
+    def updateSvd(A, U, s, V, E, k, p): 
+        """
+        Given a matrix A whose approximate SVD is U s V.T, compute the SVD 
+        of the new matrix A + E, using previous info. A and E are sparse 
+        matrices. The rank of the approximation is p, and k is an oversampling
+        parameter. 
+        """
         Parameter.checkInt(k, 1, float("inf"))
-        Parameter.checkInt(p, 0, float("inf"))
-        Parameter.checkInt(q, 0, float("inf"))        
-
-        if scipy.sparse.isspmatrix(X) or type(X) == numpy.ndarray: 
-            L = GeneralLinearOperator.asLinearOperator(X) 
+        Parameter.checkInt(p, 0, float("inf"))     
+                    
+        if isinstance(E, GeneralLinearOperator): 
+            M = E 
         else: 
-            L = X
+            M = GeneralLinearOperator.asLinearOperator(E) 
+            
+        N = GeneralLinearOperator.asLinearOperator(A + E)
         
-        n = L.shape[1]
-        if omega == None: 
-            omega = numpy.random.randn(n, k+p)
-        else: 
-            omega = numpy.c_[omega, numpy.random.randn(n, p+k - omega.shape[1])]
+        n = A.shape[1]
+        omega = numpy.random.randn(n, p)
         
-        Y = L.matmat(omega)
-        del omega 
-
-        for i in range(q):
-            Y = L.rmatmat(Y)
-            gc.collect() 
-            Y = L.matmat(Y)
-            gc.collect() 
+        Y = U*s + M.matmat(V)
+        Y = numpy.c_[Y, N.matmat(omega)]
         
         Q, R = numpy.linalg.qr(Y)
-        
+        del omega 
+            
         del Y 
         del R 
         gc.collect() 
         
-        B = L.rmatmat(Q).T
+        B = N.rmatmat(Q).T 
         U, s, V = numpy.linalg.svd(B, full_matrices=False)
         del B 
         V = V.T
         U = Q.dot(U)
-
+    
         U = U[:, 0:k]
         s = s[0:k]
         V = V[:, 0:k]        
         
         return U, s, V 
-        
-    def svd2(A, lmbda): 
-        """
-        Compute the randomised SVD with error estimate up to a tolerance of 
-        lmbda, i.e. ||A - QQ^TA||_2 <= lmbda. 
-        """ 
-        Parameter.checkFloat(lmbda, 0.0, float("inf"))
-        r = 10 
-
-        m, n = A.shape        
-        Omega = numpy.random.randn(n, k)
-        Y = A.dot(Omega)
-        
-        Q = numpy.zeros((m, 0))
-        
-        i = 0 
-
-        while numpy.max((Y**2).sum(0)) > eps/(10*numpy.sqrt(2/numpy.pi)): 
-            Y[:, i] = Y[:, i] - Q.dot(Q.T).dot(Y[:, i])
-            q = Y[:, i]
-            Q = numpy.c_[Q, q]
-            
-            omega = numpy.random.randn(n) 
         
         
