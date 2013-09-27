ed9ad1f8988bade097db56c6094e8ca9125e5e08
Charanpal Dhanjal <charanpal@gmail.com>
2013-09-27 17:28:39 +0200

    Optional testing of each param

--- exp/viroscopy/model/RunEpidemicModel4.py
+++ exp/viroscopy/model/RunEpidemicModel4.py
@@ -29,7 +29,7 @@ def runModel(meanTheta):
     undirected = True
 
     logging.debug("MeanTheta=" + str(meanTheta))
-    numReps = 5
+    numReps = 10
     numInfectedIndices = [] 
     numRemovedIndices = []
     numEdges = []
@@ -68,11 +68,11 @@ def runModel(meanTheta):
     numEdges = numpy.array(numEdges)
     numEdges = numpy.mean(numEdges, 0)
     
-    return times, numInfectedIndices, numRemovedIndices, numEdges
+    return times, numInfectedIndices, numRemovedIndices, numEdges, vertexArray[:, 6]
 
 def plotResults(meanThetas, labels, k): 
     for i in range(meanThetas.shape[0]): 
-        times, numInfectedIndices, numRemovedIndices, numEdges = runModel(meanThetas[i, :])
+        times, numInfectedIndices, numRemovedIndices, numEdges, contactTraced = runModel(meanThetas[i, :])
 
         plt.figure(k+0)
         plt.plot(times, numInfectedIndices, label=labels[i])
@@ -87,73 +87,96 @@ def plotResults(meanThetas, labels, k):
         plt.legend()
         
         plt.figure(k+2)
+        plt.plot(times, contactTraced, label=labels[i])
+        plt.xlabel("Time")
+        plt.ylabel("Contact Traced")
+        plt.legend()
+        
+        plt.figure(k+3)
         plt.plot(times, numEdges, label=labels[i])
         plt.xlabel("Time")
         plt.ylabel("Edges")
         plt.legend()
 
 numPlots = 3
+i = 0
+
+plotInitialInfects = False 
+plotAlpha = False 
+plotGamma = False 
+plotBeta = False 
+plotLambda = False 
+plotSigma = True
  
 #Number of initial infects 
-i = 0
-meanThetas = [[ 40,   0.9,    0.1,    0.00,    100,        0.1,      0.001]]
-meanThetas.append([ 50,   0.9,    0.1,    0.0,    100,        0.1,      0.001])
-meanThetas.append([ 60,   0.9,    0.1,    0.0,    100,        0.1,      0.001])
-meanThetas = numpy.array(meanThetas)
-labels = ["I_0=40", "I_0=50", "I_0=60"]
-plotResults(meanThetas, labels, i)
+if plotInitialInfects: 
+    meanThetas = [[ 40,   0.9,    0.1,    0.00,    100,        0.1,      0.001]]
+    meanThetas.append([ 50,   0.9,    0.1,    0.0,    100,        0.1,      0.001])
+    meanThetas.append([ 60,   0.9,    0.1,    0.0,    100,        0.1,      0.001])
+    meanThetas = numpy.array(meanThetas)
+    labels = ["I_0=40", "I_0=50", "I_0=60"]
+    plotResults(meanThetas, labels, i)
+    i += numPlots
 
 
 #alpha
-i += numPlots
-meanThetas = [[ 50,   0.6,    0.1,    0.0,    100,        0.1,      0.001]]
-meanThetas.append([ 50,   0.7,    0.1,    0.0,    100,        0.1,      0.001])
-meanThetas.append([ 50,   0.8,    0.1,    0.0,    100,        0.1,      0.001])
-meanThetas.append([ 50,   0.9,    0.1,    0.0,    100,        0.1,      0.001])
-meanThetas.append([ 50,   1.0,    0.1,    0.0,    100,        0.1,      0.001])
-meanThetas = numpy.array(meanThetas)
-labels = ["alpha=0.6", "alpha=0.7", "alpha=0.8", "alpha=0.9", "alpha=1.0"]
-plotResults(meanThetas, labels, i)
-plt.show()
+if plotAlpha: 
+    meanThetas = [[ 50,   0.6,    0.1,    0.0,    100,        0.1,      0.001]]
+    meanThetas.append([ 50,   0.7,    0.1,    0.0,    100,        0.1,      0.001])
+    meanThetas.append([ 50,   0.8,    0.1,    0.0,    100,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.1,    0.0,    100,        0.1,      0.001])
+    meanThetas.append([ 50,   1.0,    0.1,    0.0,    100,        0.1,      0.001])
+    meanThetas = numpy.array(meanThetas)
+    labels = ["alpha=0.6", "alpha=0.7", "alpha=0.8", "alpha=0.9", "alpha=1.0"]
+    plotResults(meanThetas, labels, i)
+    i += numPlots
+
 
 #gamma - random detection rate 
-i += numPlots
-meanThetas = [[ 50,   0.9,    0.05,    0.01,    100,        0.1,      0.001]]
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.001])
-meanThetas.append([ 50,   0.9,    0.15,    0.01,    100,        0.1,      0.001])
-meanThetas.append([ 50,   0.9,    0.2,    0.01,    100,        0.1,      0.001])
-meanThetas = numpy.array(meanThetas)
-labels = ["gamma=0.05", "gamma=0.1", "gamma=0.15", "gamma=0.2"]
-plotResults(meanThetas, labels, i)
+if plotGamma: 
+    meanThetas = [[ 50,   0.9,    0.00,    0.01,    100,        0.1,      0.001]]
+    meanThetas.append([ 50,   0.9,    0.05,    0.01,    100,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.10,    0.01,    100,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.20,    0.01,    100,        0.1,      0.001])
+    meanThetas = numpy.array(meanThetas)
+    labels = ["gamma=0.00", "gamma=0.05", "gamma=0.10", "gamma=0.2"]
+    plotResults(meanThetas, labels, i)
+    i += numPlots
 
 #beta - contact detection rate 
-i += numPlots
-meanThetas = [[ 50,   0.9,    0.1,    0.005,    100,        0.1,      0.001]]
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.001])
-meanThetas.append([ 50,   0.9,    0.1,    0.015,    100,        0.1,      0.001])
-meanThetas.append([ 50,   0.9,    0.1,    0.02,    100,        0.1,      0.001])
-meanThetas = numpy.array(meanThetas)
-labels = ["beta=0.005", "beta=0.01", "beta=0.015", "beta=0.02"]
-plotResults(meanThetas, labels, i)
+if plotBeta: 
+    meanThetas = [[ 50,   0.9,    0.01,    0.01,    1000,        0.1,      0.001]]
+    meanThetas.append([ 50,   0.9,    0.01,    0.02,    1000,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.01,    0.04,    1000,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.01,    0.08,    1000,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.01,    0.16,    1000,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.01,    0.32,    1000,        0.1,      0.001])
+    meanThetas = numpy.array(meanThetas)
+    labels = ["beta=0.01", "beta=0.02", "beta=0.04", "beta=0.08", "beta=0.16", "beta=0.32"]
+    plotResults(meanThetas, labels, i)
+    i += numPlots
 
 #lambda - contact rate 
-i += numPlots
-meanThetas = [[ 50,   0.9,    0.1,    0.01,    100,        0.05,      0.001]]
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.001])
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.15,      0.001])
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.2,      0.001])
-meanThetas = numpy.array(meanThetas)
-labels = ["lambda=0.05", "lambda=0.1", "lambda=0.15", "lambda=0.2"]
-plotResults(meanThetas, labels, i)
+if plotLambda: 
+    meanThetas = [[ 50,   0.9,    0.1,    0.01,    100,        0.05,      0.001]]
+    meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.001])
+    meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.15,      0.001])
+    meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.2,      0.001])
+    meanThetas = numpy.array(meanThetas)
+    labels = ["lambda=0.05", "lambda=0.1", "lambda=0.15", "lambda=0.2"]
+    plotResults(meanThetas, labels, i)
+    i += numPlots
 
 #sigma - infection rate 
-i += numPlots
-meanThetas = [[ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.001]]
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.002])
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.004])
-meanThetas.append([ 50,   0.9,    0.1,    0.01,    100,        0.1,      0.008])
-meanThetas = numpy.array(meanThetas)
-labels = ["lambda=0.001", "lambda=0.002", "lambda=0.004", "lambda=0.008"]
-plotResults(meanThetas, labels, i)
+if plotSigma: 
+    meanThetas = [[ 50,   0.7,    0.0,    0.0,    100,        0.1,      0.0]]
+    meanThetas.append([ 50,   0.7,    0.0,    0.0,    100,        0.1,      0.004])
+    meanThetas.append([ 50,   0.7,    0.0,    0.0,    100,        0.1,      0.016])
+    meanThetas.append([ 50,   0.7,    0.0,    0.0,    100,        0.1,      0.064])
+    meanThetas.append([ 50,   0.7,    0.0,    0.0,    100,        0.1,      0.256])
+    meanThetas = numpy.array(meanThetas)
+    labels = ["sigma=0.0", "sigma=0.004", "sigma=0.016", "sigma=0.064", "sigma=0.256"]
+    plotResults(meanThetas, labels, i)
+    i += numPlots
 
 plt.show()
